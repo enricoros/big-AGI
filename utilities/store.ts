4babee363ae0503aa74257f22bab71345300a475
Enrico Ros <enrico.ros@gmail.com>
2023-03-22 01:20:17 -0700

    Migrated the history state

--- utilities/store.ts
+++ utilities/store.ts
@@ -1,11 +1,12 @@
-import create from 'zustand';
+import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 import { GptChatModel } from '../components/Settings';
 import { SystemPurpose } from '../pages';
 
 
-// single store for now - we may branch later
+/// Settings Store
+
 interface SettingsState {
   // apiKey: string;
   // setApiKey: (apiKey: string) => void;
@@ -16,7 +17,6 @@ interface SettingsState {
   systemPurpose: SystemPurpose;
   setSystemPurpose: (purpose: SystemPurpose) => void;
 }
-
 
 export const useSettingsStore = create<SettingsState>()(
   persist((set) => ({
@@ -30,5 +30,48 @@ export const useSettingsStore = create<SettingsState>()(
     }),
     {
       name: 'app-settings',
+    }),
+);
+
+
+/// Composer Store
+
+export interface HistoryItem {
+  date: number,
+  text: string,
+  count: number,
+}
+
+interface ComposerState {
+  history: HistoryItem[];
+  appendMessageToHistory: (text: string) => void;
+}
+
+export const useComposerStore = create<ComposerState>()(
+  persist((set, get) => ({
+      history: [],
+
+      appendMessageToHistory: (text: string) => {
+        const date = Date.now();
+        const history = [...(get().history || [])];
+
+        // take the item from the array, matching by text
+        let item = history.find((item) => item.text === text);
+        if (item) {
+          history.splice(history.indexOf(item), 1);
+          item.date = date;
+          item.count++;
+        } else
+          item = { date, text, count: 1 };
+
+        // prepend the item to the history array
+        history.unshift(item);
+
+        // update the store (limiting max items)
+        set({ history: history.slice(0, 20) });
+      },
+    }),
+    {
+      name: 'app-composer',
     }),
 );
