2288186b8f1b49e6c292d3881119938b1143d9d5
Enrico Ros <enrico.ros@gmail.com>
2023-03-29 16:32:53 -0700

    Advanced Settings

--- components/SettingsModal.tsx
+++ components/SettingsModal.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { shallow } from 'zustand/shallow';
 
-import { Box, Button, Input, Modal, ModalClose, ModalDialog, Typography } from '@mui/joy';
+import { Box, Button, Grid, Input, Modal, ModalClose, ModalDialog, Slider, Stack, Typography } from '@mui/joy';
 
 import { Link } from './util/Link';
 import { useSettingsStore } from '@/lib/store';
@@ -19,8 +19,14 @@ export const isValidOpenAIApiKey = (apiKey?: string) =>
  * @param {() => void} onClose Call this to close the dialog from outside
  */
 export function SettingsModal({ open, onClose }: { open: boolean, onClose: () => void; }) {
-  const { apiKey, setApiKey } = useSettingsStore(state => ({
+  // state
+  const [showAdvanced, setShowAdvanced] = React.useState(false);
+
+  // global state
+  const { apiKey, setApiKey, modelTemperature, setModelTemperature, modelMaxTokens, setModelMaxTokens } = useSettingsStore(state => ({
     apiKey: state.apiKey, setApiKey: state.setApiKey,
+    modelTemperature: state.modelTemperature, setModelTemperature: state.setModelTemperature,
+    modelMaxTokens: state.modelMaxTokens, setModelMaxTokens: state.setModelMaxTokens,
   }), shallow);
 
   const handleApiKeyChange = (e: React.ChangeEvent) =>
@@ -29,16 +35,21 @@ export function SettingsModal({ open, onClose }: { open: boolean, onClose: () =>
   const handleApiKeyDown = (e: React.KeyboardEvent) =>
     (e.key === 'Enter') && onClose();
 
+  const handleTemperatureChange = (event: Event, newValue: number | number[]) => setModelTemperature(newValue as number);
+
+  const handleMaxTokensChange = (event: Event, newValue: number | number[]) => setModelMaxTokens(newValue as number);
+
   const needsApiKey = !!process.env.REQUIRE_USER_API_KEYS;
   const isValidKey = isValidOpenAIApiKey(apiKey);
 
   return (
     <Modal open={open} onClose={onClose}>
-      <ModalDialog sx={{ minWidth: '40vw' }}>
+      <ModalDialog sx={{ minWidth: '35vw' }}>
         <ModalClose />
         <Typography level='h5'>Settings</Typography>
 
-        <Box sx={{ mt: 3, minWidth: 300 }}>
+        <Box sx={{ mt: 3, minWidth: 320 }}>
+
 
           <Typography level='body1' sx={{ mb: 1 }}>
             Personal <Link href='https://platform.openai.com/account/api-keys'>OpenAI API Key</Link> {needsApiKey ? '(required)' : '(not required)'}
@@ -53,7 +64,53 @@ export function SettingsModal({ open, onClose }: { open: boolean, onClose: () =>
             </Typography>
           )}
 
-          <Button variant='solid' color={isValidKey ? 'primary' : 'neutral'} sx={{ mt: 3 }} onClick={onClose}>
+
+          <Stack direction='row' sx={{ mt: 3, gap: 2, alignItems: 'center' }}>
+            <Typography>
+              Advanced AI settings
+            </Typography>
+            <Button variant='plain' color='neutral' onClick={() => setShowAdvanced(!showAdvanced)}>
+              {showAdvanced ? 'hide' : 'show'}
+            </Button>
+          </Stack>
+          {showAdvanced && (
+            <Grid container spacing={1} sx={{ alignItems: 'center', mb: 1 }}>
+              <Grid xs={6} md={5} xl={4}>
+                <Typography level='body2' sx={{ textAlign: 'right', mr: 1 }}>
+                  Temperature
+                </Typography>
+              </Grid>
+              <Grid xs={6} md={7} xl={8}>
+                <Slider
+                  aria-label='Model Temperature' color='neutral'
+                  min={0} max={1} step={0.1} defaultValue={0.5}
+                  value={modelTemperature} onChange={handleTemperatureChange}
+                  valueLabelDisplay='auto'
+                  sx={{ py: 1, mt: 1.1 }}
+                />
+              </Grid>
+
+              <Grid xs={6} md={5} xl={4}>
+                <Typography level='body2' sx={{ textAlign: 'right', mr: 1 }}>
+                  Max. tokens
+                </Typography>
+              </Grid>
+              <Grid xs={6} md={7} xl={8}>
+                <Slider
+                  aria-label='Model Temperature' color='neutral'
+                  min={512} max={8192} step={512} defaultValue={2048}
+                  value={modelMaxTokens} onChange={handleMaxTokensChange}
+                  valueLabelDisplay='auto'
+                  sx={{ py: 1, mt: 1.1 }}
+                />
+              </Grid>
+            </Grid>
+          )}
+          <Typography level='body2' sx={{ mb: 1 }}>
+            No need to change unless you know what they are
+          </Typography>
+
+          <Button variant='solid' color={isValidKey ? 'primary' : 'neutral'} sx={{ mt: 4 }} onClick={onClose}>
             Close
           </Button>
 
