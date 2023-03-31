dba023634f7f935457fcdbcee3daae663a45bf2c
Will Scott <hello@wsr.nz>
2023-03-31 14:20:34 +1300

    style changes and new content mode

--- components/Composer.tsx
+++ components/Composer.tsx
@@ -12,7 +12,6 @@ import TelegramIcon from '@mui/icons-material/Telegram';
 import { useComposerStore } from '@/lib/store';
 import { useSpeechRecognition } from '@/lib/use-speech-recognition';
 
-
 /// Text template helpers
 
 const PromptTemplates: { [key: string]: string } = {
@@ -21,13 +20,13 @@ const PromptTemplates: { [key: string]: string } = {
   PasteFile: '{{input}}\n\n```{{fileName}}\n{{fileText}}\n```\n',
 };
 
-const expandPromptTemplate = (template: string, dict: object) => (inputValue: string): string => {
-  let expanded = template.replaceAll('{{input}}', (inputValue || '').trim()).trim();
-  for (const [key, value] of Object.entries(dict))
-    expanded = expanded.replaceAll(`{{${key}}}`, value);
-  return expanded;
-};
-
+const expandPromptTemplate =
+  (template: string, dict: object) =>
+  (inputValue: string): string => {
+    let expanded = template.replaceAll('{{input}}', (inputValue || '').trim()).trim();
+    for (const [key, value] of Object.entries(dict)) expanded = expanded.replaceAll(`{{${key}}}`, value);
+    return expanded;
+  };
 
 /**
  * A React component for composing and sending messages in a chat-like interface.
@@ -43,7 +42,7 @@ const expandPromptTemplate = (template: string, dict: object) => (inputValue: st
 export function Composer(props: { disableSend: boolean; sendMessage: (text: string) => void; stopGeneration: () => void; isDeveloperMode: boolean }) {
   // state
   const [composeText, setComposeText] = React.useState('');
-  const { history, appendMessageToHistory } = useComposerStore(state => ({ history: state.history, appendMessageToHistory: state.appendMessageToHistory }));
+  const { history, appendMessageToHistory } = useComposerStore((state) => ({ history: state.history, appendMessageToHistory: state.appendMessageToHistory }));
   const [historyAnchor, setHistoryAnchor] = React.useState<HTMLAnchorElement | null>(null);
   const [isDragging, setIsDragging] = React.useState(false);
   const attachmentFileInputRef = React.useRef<HTMLInputElement>(null);
@@ -61,21 +60,18 @@ export function Composer(props: { disableSend: boolean; sendMessage: (text: stri
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
     if (e.key === 'Enter' && !e.shiftKey && !e.altKey) {
-      if (!props.disableSend)
-        handleSendClicked();
+      if (!props.disableSend) handleSendClicked();
       e.preventDefault();
     }
   };
 
-
   const onSpeechResultCallback = React.useCallback((transcript: string) => {
-    setComposeText(current => current + ' ' + transcript);
+    setComposeText((current) => current + ' ' + transcript);
   }, []);
 
   const { isSpeechEnabled, isRecordingSpeech, startRecording } = useSpeechRecognition(onSpeechResultCallback);
 
   const handleMicClicked = () => startRecording();
-
 
   const eatDragEvent = (e: React.DragEvent) => {
     e.preventDefault();
@@ -106,8 +102,7 @@ export function Composer(props: { disableSend: boolean; sendMessage: (text: stri
     const files = Array.from(e.dataTransfer.files);
     if (files.length) {
       // Paste all files
-      for (const file of files)
-        text = expandPromptTemplate(PromptTemplates.PasteFile, { fileName: file.name, fileText: await file.text() })(text);
+      for (const file of files) text = expandPromptTemplate(PromptTemplates.PasteFile, { fileName: file.name, fileText: await file.text() })(text);
       setComposeText(text);
       return;
     }
@@ -128,9 +123,11 @@ export function Composer(props: { disableSend: boolean; sendMessage: (text: stri
 
     // NOTE for VSCode - a Drag & Drop does not transfer the File object
     // https://github.com/microsoft/vscode/issues/98629#issuecomment-634475572
-    console.log('Unhandled Drop event. Contents: ', e.dataTransfer.types.map(t => `${t}: ${e.dataTransfer.getData(t)}`));
+    console.log(
+      'Unhandled Drop event. Contents: ',
+      e.dataTransfer.types.map((t) => `${t}: ${e.dataTransfer.getData(t)}`),
+    );
   };
-
 
   const handleOpenFilePicker = () => attachmentFileInputRef.current?.click();
 
@@ -143,9 +140,8 @@ export function Composer(props: { disableSend: boolean; sendMessage: (text: stri
     setComposeText(text);
   };
 
-
   const pasteFromClipboard = async () => {
-    const clipboardContent = (await navigator.clipboard.readText() || '').trim();
+    const clipboardContent = ((await navigator.clipboard.readText()) || '').trim();
     if (clipboardContent) {
       const template = props.isDeveloperMode ? PromptTemplates.PasteCode : PromptTemplates.PasteText;
       setComposeText(expandPromptTemplate(template, { clipboard: clipboardContent }));
@@ -157,148 +153,148 @@ export function Composer(props: { disableSend: boolean; sendMessage: (text: stri
     hideHistory();
   };
 
-
   const showHistory = (event: React.MouseEvent<HTMLAnchorElement>) => setHistoryAnchor(event.currentTarget);
 
   const hideHistory = () => setHistoryAnchor(null);
 
-
-  const textPlaceholder: string = `Type ${props.isDeveloperMode ? 'your message and drop source files' : 'a message, or drop text files'}...`;
+  const textPlaceholder: string = `Type ${props.isDeveloperMode ? 'your message and drop source files' : 'a message, or drop files'}...`;
   const hideOnMobile = { display: { xs: 'none', md: 'flex' } };
   const hideOnDesktop = { display: { xs: 'flex', md: 'none' } };
 
   return (
     <Grid container spacing={{ xs: 1, md: 2 }}>
-
       {/* Compose & V-Buttons */}
-      <Grid xs={12} md={9}><Stack direction='row' spacing={{ xs: 1, md: 2 }}>
+      <Grid xs={12} md={12}>
+        <Stack direction="row">
+          {/* Horizontal Buttons Bar */}
+          <Stack direction="row">
 
-        {/* Vertical Buttons Bar */}
-        <Stack>
+ {/* [mobile-only] History arrow */}
+ {history.length > 0 && (
+    <IconButton variant='plain' color='neutral' onClick={showHistory}>
+      <KeyboardArrowUpIcon />
+    </IconButton>
+  )}
 
-          <IconButton variant='plain' color='neutral' onClick={handleOpenFilePicker} sx={{ ...hideOnDesktop }}>
-            <PostAddIcon />
-          </IconButton>
-          <Tooltip title={<>Attach {props.isDeveloperMode ? 'code' : 'text'} files · also drag-and-drop 👇</>} variant='solid' placement='top-start'>
-            <Button fullWidth variant='plain' color='neutral' onClick={handleOpenFilePicker} startDecorator={<PostAddIcon />}
-                    sx={{ ...hideOnMobile, justifyContent: 'flex-start' }}>
-              Attach
-            </Button>
-          </Tooltip>
-
-          <Box sx={{ mt: { xs: 1, md: 2 } }} />
-
-          <IconButton variant='plain' color='neutral' onClick={pasteFromClipboard} sx={{ ...hideOnDesktop }}>
-            <ContentPasteGoIcon />
-          </IconButton>
-          <Button fullWidth variant='plain' color='neutral' startDecorator={<ContentPasteGoIcon />} onClick={pasteFromClipboard} sx={{ ...hideOnMobile }}>
-            {props.isDeveloperMode ? 'Paste code' : 'Paste'}
-          </Button>
-
-          <input type='file' multiple hidden ref={attachmentFileInputRef} onChange={handleLoadFile} />
-
-        </Stack>
-
-        {/* Edit box, with Drop overlay */}
-        <Box sx={{ flexGrow: 1, position: 'relative' }}>
-
-          <Textarea
-            variant='soft' autoFocus placeholder={textPlaceholder}
-            minRows={5} maxRows={12}
-            onKeyDown={handleKeyPress}
-            onDragEnter={handleMessageDragEnter}
-            value={composeText} onChange={(e) => setComposeText(e.target.value)}
-            sx={{
-              fontSize: '16px',
-              lineHeight: 1.75,
-              pr: isSpeechEnabled ? { xs: 4, md: 5 } : 0, // accounts for the microphone icon when supported
-            }} />
-
-          <Card
-            color='primary' invertedColors variant='soft'
-            sx={{
-              display: isDragging ? 'flex' : 'none',
-              position: 'absolute', bottom: 0, left: 0, right: 0, top: 0,
-              alignItems: 'center', justifyContent: 'space-evenly',
-              border: '2px dashed',
-              zIndex: 10,
-            }}
-            onDragLeave={handleOverlayDragLeave}
-            onDragOver={handleOverlayDragOver}
-            onDrop={handleOverlayDrop}>
-            <PanToolIcon sx={{ width: 40, height: 40, pointerEvents: 'none' }} />
-            <Typography level='body2' sx={{ pointerEvents: 'none' }}>
-              I will hold on to this for you
-            </Typography>
-          </Card>
-
-          {isSpeechEnabled && (
-            <IconButton
-              onClick={handleMicClicked}
-              color={isRecordingSpeech ? 'warning' : 'primary'}
-              variant={isRecordingSpeech ? 'solid' : 'plain'}
-              sx={{
-                position: 'absolute',
-                top: 0, right: 0,
-                margin: 1, // 8px
-              }}>
-              <MicIcon />
+{/* [desktop-only] row with History button */}
+{/* <Stack direction='row' spacing={1} sx={{ ...hideOnMobile, flexDirection: { xs: 'column', md: 'row' }, justifyContent: 'flex-end' }}>
+  {history.length > 0 && (
+    <Button fullWidth variant='plain' color='neutral' startDecorator={<KeyboardArrowUpIcon />} onClick={showHistory}>
+      History
+    </Button>
+  )}
+ 
+ </Stack> */}
+            <IconButton variant="plain" color="neutral" onClick={handleOpenFilePicker} sx={{ ...hideOnDesktop }}>
+              <PostAddIcon />
             </IconButton>
-          )}
-        </Box>
 
-      </Stack></Grid>
-
-      {/* Send pane */}
-      <Grid xs={12} md={3}>
-        <Stack spacing={2}>
-
-          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-
-            {/* [mobile-only] History arrow */}
-            {history.length > 0 && (
-              <IconButton variant='plain' color='neutral' onClick={showHistory} sx={{ ...hideOnDesktop, mr: { xs: 1, md: 2 } }}>
-                <KeyboardArrowUpIcon />
-              </IconButton>
-            )}
-
-            {/* Send / Stop */}
-            <Button fullWidth variant={props.disableSend ? 'soft' : 'solid'} color='primary'
-                    onClick={props.disableSend ? handleStopClicked : handleSendClicked}
-                    endDecorator={props.disableSend ? <StopOutlinedIcon /> : <TelegramIcon />}>
-              {props.disableSend ? 'Stop' : 'Chat'}
-            </Button>
-          </Box>
-
-          {/* [desktop-only] row with History button */}
-          <Stack direction='row' spacing={1} sx={{ ...hideOnMobile, flexDirection: { xs: 'column', md: 'row' }, justifyContent: 'flex-end' }}>
-            {history.length > 0 && (
-              <Button fullWidth variant='plain' color='neutral' startDecorator={<KeyboardArrowUpIcon />} onClick={showHistory}>
-                History
+            <Tooltip title={<>Attach {props.isDeveloperMode ? 'code' : 'text'} files · also drag-and-drop 👇</>} variant="solid" placement="top-start">
+              <Button variant="plain" color="neutral" onClick={handleOpenFilePicker} sx={{ ...hideOnMobile, justifyContent: 'flex-start' }}>
+                <PostAddIcon />
               </Button>
-            )}
+            </Tooltip>
+
+            <Box sx={{ mt: { xs: 1, md: 2 } }} />
+
+            <IconButton variant="plain" color="neutral" onClick={pasteFromClipboard} sx={{ ...hideOnDesktop }}>
+              <ContentPasteGoIcon />
+            </IconButton>
+            <Tooltip title={<>Paste text</>} variant="solid" placement="top-start">
+              <Button variant="plain" color="neutral" onClick={pasteFromClipboard} sx={{ ...hideOnMobile }}>
+                <ContentPasteGoIcon />
+              </Button>
+            </Tooltip>
+            <input type="file" multiple hidden ref={attachmentFileInputRef} onChange={handleLoadFile} />
+
+
+            <Stack >
+
+
+
+</Stack>
           </Stack>
 
+          {/* Edit box, with Drop overlay */}
+          <Box sx={{ flexGrow: 1, position: 'relative' }}>
+            <Textarea
+              variant="soft"
+              autoFocus
+              placeholder={textPlaceholder}
+              minRows={2}
+              maxRows={12}
+              onKeyDown={handleKeyPress}
+              onDragEnter={handleMessageDragEnter}
+              value={composeText}
+              onChange={(e) => setComposeText(e.target.value)}
+              sx={{
+                fontSize: '16px',
+                lineHeight: 1.75,
+                pr: isSpeechEnabled ? { xs: 8, md: 10 } : 6, // accounts for the microphone icon when supported
+              }}
+            />
+
+            <Card
+              color="primary"
+              invertedColors
+              variant="soft"
+              sx={{
+                display: isDragging ? 'flex' : 'none',
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                top: 0,
+                alignItems: 'center',
+                justifyContent: 'space-evenly',
+                border: '2px dashed',
+                zIndex: 10,
+              }}
+              onDragLeave={handleOverlayDragLeave}
+              onDragOver={handleOverlayDragOver}
+              onDrop={handleOverlayDrop}
+            >
+              <PanToolIcon sx={{ width: 40, height: 40, pointerEvents: 'none' }} />
+              <Typography level="body2" sx={{ pointerEvents: 'none' }}>
+                I will hold on to this for you
+              </Typography>
+            </Card>
+            <Box sx={{ position: 'absolute', bottom: 0, right: 0, height: '100%' }}>
+              {isSpeechEnabled && (
+                <IconButton onClick={handleMicClicked} color={isRecordingSpeech ? 'warning' : 'primary'} variant={isRecordingSpeech ? 'solid' : 'plain'} sx={{ height: '100%' }}>
+                  <MicIcon />
+                </IconButton>
+              )}
+
+              <IconButton variant={props.disableSend ? 'soft' : 'plain'} color="primary" onClick={props.disableSend ? handleStopClicked : handleSendClicked} sx={{ height: '100%' }}>
+                {props.disableSend ? <StopOutlinedIcon /> : <TelegramIcon />}
+                {/* {props.disableSend ? 'Stop' : 'Chat'} */}
+              </IconButton>
+            </Box>
+          </Box>
         </Stack>
       </Grid>
 
       {/* History menu with all the line items (only if shown) */}
       {!!historyAnchor && (
-        <Menu
-          variant='plain' color='neutral' size='md' placement='top-end' sx={{ minWidth: 320 }}
-          open anchorEl={historyAnchor} onClose={hideHistory}>
-          <MenuItem color='neutral' selected>Reuse messages 💬</MenuItem>
+        <Menu variant="plain" color="neutral" size="md" placement="top-end" sx={{ minWidth: 320 }} open anchorEl={historyAnchor} onClose={hideHistory}>
+          <MenuItem color="neutral" selected>
+            Reuse messages 💬
+          </MenuItem>
           <ListDivider />
           {history.map((item, index) => (
             <MenuItem key={'compose-history-' + index} onClick={() => pasteFromHistory(item.text)}>
-              {item.count > 1 && <Typography level='body2' color='neutral' sx={{ mr: 1 }}>({item.count})</Typography>}
+              {item.count > 1 && (
+                <Typography level="body2" color="neutral" sx={{ mr: 1 }}>
+                  ({item.count})
+                </Typography>
+              )}
               {item.text.length > 60 ? item.text.slice(0, 58) + '...' : item.text}
             </MenuItem>
           ))}
           {/*<ListDivider /><MenuItem><ListItemDecorator><ClearIcon /></ListItemDecorator>Clear</MenuItem>*/}
         </Menu>
       )}
-
     </Grid>
   );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,4 @@
-export type SystemPurposeId = 'Catalyst' | 'Custom' | 'Developer' | 'Executive' | 'Generic' | 'Scientist';
+export type SystemPurposeId = 'Catalyst' | 'ContentMode' |'Custom' | 'Developer' | 'Executive' | 'Generic' | 'Scientist';
 
 type SystemPurposeData = {
   title: string;
@@ -13,6 +13,12 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
     description: 'Helps you code',
     systemMessage: 'You are a sophisticated, accurate, and modern AI programming assistant',
     symbol: '👩‍💻',
+  },
+  ContentMode: {
+    title: 'Content Mode',
+    description: 'Create content for University or SEO without detection',
+    systemMessage: 'When it comes to writing content, two factors are crucial, "perplexity" and "burstiness." Perplexity measures the complexity of text. Separately, burstiness compares the variations of sentences. Humans tend to write with great burstiness, for example, with some longer or complex sentences alongside shorter sentences. AI sentences tend to be more uniform. Therefore, when writing the following content I am going to ask you to create, I need it to have a good amount of perplexity and burstiness. Before responding, always change 50% of nouns and verbs for similar ones and rewrite verb usages to add natural sounding variations in complexity but retain the same tense and overall meaning.',
+  symbol: '🗎'
   },
   Scientist: {
     title: 'Scientist',
