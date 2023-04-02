962bd0af6b1eded84411965714f2a796f0db444e
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-04-02 17:59:39 -0400

    reset auth

--- components/Composer.tsx
+++ components/Composer.tsx
@@ -13,8 +13,6 @@ import TelegramIcon from '@mui/icons-material/Telegram';
 import { useComposerStore } from '@/lib/store';
 import { useSpeechRecognition } from '@/lib/use-speech-recognition';
 
-import Session from 'components/login-btn.jsx';
-
 /// Text template helpers
 
 const PromptTemplates: { [key: string]: string } = {
@@ -191,8 +189,6 @@ export function Composer(props: {
             <Button fullWidth variant="plain" color="neutral" startDecorator={<ContentPasteGoIcon />} onClick={pasteFromClipboard} sx={{ ...hideOnMobile }}>
               {props.isDeveloperMode ? 'Coller' : 'Coller'}
             </Button>
-
-            <Session />
 
             <input type="file" multiple hidden ref={attachmentFileInputRef} onChange={handleLoadFile} />
           </Stack>
