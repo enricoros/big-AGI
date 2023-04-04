eeee96c72455d7b0fb984586794264c7cfda10f0
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-04-03 21:28:00 -0400

    add basicauth

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: ['/', '/index'],
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+  const url = req.nextUrl;
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === 'subvention' && pwd === '123') {
+      return NextResponse.next();
+    }
+  }
+  url.pathname = '/api/auth';
+
+  return NextResponse.rewrite(url);
+}
