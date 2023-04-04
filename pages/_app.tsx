e31bc6a932fcee7aa663481c7f1bd778947052c3
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-04-04 00:54:02 -0400

    add auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,10 @@ import { CssBaseline, CssVarsProvider } from '@mui/joy';
 
 import { createEmotionCache, theme } from '@/lib/theme';
 
+import type { LayoutProps } from '@vercel/examples-ui/layout';
+import { getLayout } from '@vercel/examples-ui';
+import '@vercel/examples-ui/globals.css';
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
@@ -15,19 +19,22 @@ export interface MyAppProps extends AppProps {
 }
 
 export default function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
+  const Layout = getLayout<LayoutProps>(Component);
   return (
     <>
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <meta name="viewport" content="initial-scale=1, width=device-width" />
-        </Head>
-        <CssVarsProvider defaultMode="light" theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </CssVarsProvider>
-      </CacheProvider>
-      <VercelAnalytics debug={false} />
+      <Layout title="Password Protected" path="edge-middleware/basic-auth-password">
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          <CssVarsProvider defaultMode="light" theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </CssVarsProvider>
+        </CacheProvider>
+        <VercelAnalytics debug={false} />
+      </Layout>
     </>
   );
 }
