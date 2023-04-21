9f07c6200ecce769d87d25559d6f8090c57b935a
Enrico Ros <enrico.ros@gmail.com>
2023-04-21 00:59:46 -0700

    Icons.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,8 +17,14 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
     <Html lang='en' className={bodyFontClassName}>
       <Head>
         <link rel='shortcut icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
         <link rel='manifest' href='/manifest.json' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='black' />
+
+        {/* Style Sheets (injected and server-side) */}
         <meta name='emotion-insertion-point' content='' />
         {emotionStyleTags}
       </Head>
