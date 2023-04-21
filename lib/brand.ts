d7ef63ae41d1d4557b0d328128dafd0fbfc23816
Enrico Ros <enrico.ros@gmail.com>
2023-04-21 01:32:41 -0700

    Improve Shareability

--- lib/brand.ts
+++ lib/brand.ts
@@ -13,13 +13,17 @@ export const Brand = {
     Common: 'big-AGI',
   },
   Meta: {
+    SiteName: 'big-AGI',
+    Title: 'big-AGI: Personal AGI App',
     Description: 'big-AGI is a free, open-source project to build a general artificial intelligence (AGI) that can solve any problem.',
     Keywords: 'artificial general intelligence, agi, openai, gpt-4, ai personas, code execution, pdf import, voice i/o, ai chat, artificial intelligence',
     ThemeColor: '#434356',
+    TwitterSite: '@enricoros',
   },
   URIs: {
     // Slug: 'big-agi',
-    // Deployment: 'https://big-agi.com',
+    Home: 'https://big-agi.com',
+    CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
     OpenRepo: 'https://github.com/enricoros/big-agi',
   },
 };
