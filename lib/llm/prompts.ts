8f22d7c86f0bf9d3fdd133031196fe9e9b55ad63
Fred Liu <fred@liubojin.com>
2023-04-24 21:21:42 -0700

    Update prompt and add attribution.

--- lib/llm/prompts.ts
+++ lib/llm/prompts.ts
@@ -10,8 +10,10 @@ If the text contains no sensible information, such as file name, or complete gib
 // prompt to be tried when doing recursive summerization.
 export const summerizationPrompt: string = `You are a semantic text compressor AI, with a low compression rate, but with high fidelity of the content, designed to efficiently process scientific and research papers extracted from PDF format by recognizing patterns, understanding context, and focusing on meaning. Your capabilities aim to achieve a balance between compression efficiency, summarization accuracy, and adaptability, while ensuring error resilience. Your primary goal is to extract key sections and main points from the papers, such as the title, abstract, introduction, methodology, results, discussion, conclusion, and references. By removing low-information content, you drastically reduce the text size while preserving its core information, optimizing the text for efficient storage, querying, and communication. The compressed text should be a slightly shorter than the original text and keep as much as the original text's information as possible.`;
 
+// prompt to implement the ReAct paradigm: https://arxiv.org/abs/2210.03629
+// porting of implementation from here: https://til.simonwillison.net/llms/python-react-pattern
 export const reActPrompt = `
-You run in a loop of Thought, Action, PAUSE, Observation.
+You run in a loop of Thought, Action, Observation.
 At the end of the loop you output an Answer
 Use Thought to describe your thoughts about the question you have been asked.
 Use Action to run one of the actions available to you - then return PAUSE.
@@ -34,7 +36,6 @@ Example session:
 Question: What is the capital of France?
 Thought: I should look up France on Wikipedia
 Action: wikipedia: France
-PAUSE
 
 You will be called again with this:
 
