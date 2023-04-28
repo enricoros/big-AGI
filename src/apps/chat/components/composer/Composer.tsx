b2ac2a3c9e764dd127448b365cfe2bd6275b6a58
Enrico Ros <enrico.ros@gmail.com>
2023-04-28 00:20:22 -0700

    Improve ReAct mode looks

--- src/apps/chat/components/composer/Composer.tsx
+++ src/apps/chat/components/composer/Composer.tsx
@@ -11,6 +11,7 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import MicIcon from '@mui/icons-material/Mic';
 import PanToolIcon from '@mui/icons-material/PanTool';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import PsychologyIcon from '@mui/icons-material/Psychology';
 import StopOutlinedIcon from '@mui/icons-material/StopOutlined';
 import TelegramIcon from '@mui/icons-material/Telegram';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
@@ -30,7 +31,6 @@ import { TokenBadge } from './TokenBadge';
 import { TokenProgressbar } from './TokenProgressbar';
 import { hideOnDesktop, hideOnMobile } from '@/common/theme';
 import { isValidProdiaApiKey, requireUserKeyProdia } from '@/modules/prodia/prodia.client';
-
 
 
 /// Text template helpers
@@ -399,6 +399,8 @@ export function Composer(props: {
     ? 'Tell me what you need, add drop source files...'
     : isProdiaConfigured ? 'Type, /imagine, or drop text files...' : 'Type a message, or drop text files...';
 
+  const isReAct = sendModeId === 'react';
+
   return (
     <Box sx={props.sx}>
       <Grid container spacing={{ xs: 1, md: 2 }}>
@@ -451,10 +453,12 @@ export function Composer(props: {
             <Box sx={{ position: 'relative' }}>
 
               <Textarea
-                variant='outlined' autoFocus placeholder={textPlaceholder}
+                variant='outlined' color={isReAct ? 'info' : 'neutral'}
+                autoFocus
                 minRows={4} maxRows={12}
                 onKeyDown={handleKeyPress}
                 onDragEnter={handleMessageDragEnter}
+                placeholder={textPlaceholder}
                 value={composeText} onChange={(e) => setComposeText(e.target.value)}
                 slotProps={{
                   textarea: {
@@ -515,11 +519,11 @@ export function Composer(props: {
 
               {/* Send / Stop */}
               {assistantTyping
-                ? <Button fullWidth variant='soft' color='primary' disabled={!props.conversationId} onClick={handleStopClicked} endDecorator={<StopOutlinedIcon />}>
+                ? <Button fullWidth variant='soft' color={isReAct ? 'info' : 'primary'} disabled={!props.conversationId} onClick={handleStopClicked} endDecorator={<StopOutlinedIcon />}>
                   Stop
                 </Button>
-                : <Button fullWidth variant='solid' color={sendModeId === 'react' ? 'warning' : 'primary'} disabled={!props.conversationId} onClick={handleSendClicked} onDoubleClick={handleShowSendMode} endDecorator={<TelegramIcon />}>
-                  {sendModeId === 'react' ? 'ReAct' : 'Chat'}
+                : <Button fullWidth variant='solid' color={isReAct ? 'info' : 'primary'} disabled={!props.conversationId} onClick={handleSendClicked} onDoubleClick={handleShowSendMode} endDecorator={isReAct ? <PsychologyIcon /> : <TelegramIcon />}>
+                  {isReAct ? 'ReAct' : 'Chat'}
                 </Button>}
             </Box>
 
