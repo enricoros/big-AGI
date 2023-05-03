cf51ffe467bed9631cdfdfa7b5c87da75b3445e2
Enrico Ros <enrico.ros@gmail.com>
2023-05-02 23:28:19 -0700

    Cleanup

--- src/common/llm-util/react.ts
+++ src/common/llm-util/react.ts
@@ -84,7 +84,7 @@ export class Agent {
   async step(S: State, modelId: ChatModelId, log: (...data: any[]) => void = console.log) {
     log('→ reAct [...' + (S.messages.length + 1) + ']: ' + S.nextPrompt);
     const result = await this.chat(S, S.nextPrompt, modelId);
-    log(result);
+    log(`← ${result}`);
     const actions = result
       .split('\n')
       .map((a: string) => actionRe.exec(a))
@@ -95,11 +95,10 @@ export class Agent {
       if (!(action in knownActions)) {
         throw new Error(`Unknown action: ${action}: ${actionInput}`);
       }
-      log(`→ running ${action} "${actionInput}"`);
-      const observation = await knownActions[action](actionInput);
-      log(`Observation: ${observation}`);
-      S.nextPrompt = `Observation: ${observation}`;
-      S.lastObservation = observation;
+      log(`⚡ ${action} "${actionInput}"`);
+      S.lastObservation = await knownActions[action](actionInput);
+      S.nextPrompt = `Observation: ${S.lastObservation}`;
+      log(S.nextPrompt);
     } else {
       log('↙ done');
       // log(`Result: ${result}`);
