e39271b5cfae0ac8b3f4d006137717a5f3461a71
Enrico Ros <enrico.ros@gmail.com>
2023-05-11 03:34:45 -0700

    Skel for Model Setup

--- src/common/layouts/AppLayout.tsx
+++ src/common/layouts/AppLayout.tsx
@@ -3,15 +3,19 @@ import { shallow } from 'zustand/shallow';
 
 import { Box, Container, useTheme } from '@mui/joy';
 
-import { useSettingsStore } from '@/common/state/store-settings';
-
+import { ModelingModal } from '@/modules/models/ModelingModal';
 import { SettingsModal } from '../../apps/settings/SettingsModal';
 
-import { ApplicationBar } from '../components/appbar/ApplicationBar';
+import { useSettingsStore } from '@/common/state/store-settings';
+
+import { ApplicationBar } from './appbar/ApplicationBar';
 import { NoSSR } from '../components/NoSSR';
 
 
-export function AppLayout(props: { children: React.ReactNode, noAppBar?: boolean, noSettings?: boolean }) {
+export function AppLayout(props: {
+  noAppBar?: boolean, noSettings?: boolean, noModeling?: boolean,
+  children: React.ReactNode,
+}) {
   // external state
   const theme = useTheme();
   const { centerMode } = useSettingsStore(state => ({ centerMode: state.centerMode }), shallow);
@@ -26,8 +30,8 @@ export function AppLayout(props: { children: React.ReactNode, noAppBar?: boolean
         sx={{
           boxShadow: {
             xs: 'none',
-            md: centerMode === 'narrow' ? theme.vars.shadow.md : 'none',
-            xl: centerMode !== 'full' ? theme.vars.shadow.lg : 'none',
+            md: centerMode === 'narrow' ? theme.shadow.md : 'none',
+            xl: centerMode !== 'full' ? theme.shadow.lg : 'none',
           },
         }}>
 
@@ -45,6 +49,8 @@ export function AppLayout(props: { children: React.ReactNode, noAppBar?: boolean
       </Container>
 
       {!props.noSettings && <SettingsModal />}
+
+      {!props.noModeling && <ModelingModal />}
 
     </NoSSR>
   );
---
+++ src/modules/models/Modeling.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/joy';
+import * as React from 'react';
+
+export function Modeling() {
+  return <>
+    <Box>
+      <Typography>
+        Test
+      </Typography>
+    </Box>
+  </>;
+}
