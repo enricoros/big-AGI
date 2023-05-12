1ce9a7c9b5b33b6f51c4d05bef6a651a91f99368
Enrico Ros <enrico.ros@gmail.com>
2023-05-12 04:12:00 -0700

    ConfigureSources: use Store

--- src/modules/models/ModelConfigurator.tsx
+++ src/modules/models/ModelConfigurator.tsx
@@ -1,41 +1,19 @@
 import * as React from 'react';
 
 import { Divider } from '@mui/joy';
+
 import { AddVendor } from '@/modules/models/AddVendor';
-import { DModelSource, DModelSourceId } from '@/modules/models/store-models';
 import { ConfigureSources } from '@/modules/models/ConfigureSources';
 
 
 export function ModelConfigurator() {
-  const [modelSources, setModelSources] = React.useState<DModelSource[]>([]);
-  const [selectedSourceId, setSelectedSourceId] = React.useState<DModelSourceId | null>(null);
-
-  console.log('modelSources', modelSources);
-
-  const handleAddSource = (newSource: DModelSource) => {
-    setModelSources([...modelSources, newSource]);
-    setSelectedSourceId(newSource.sourceId);
-  };
-
-  const handleDeleteSourceId = (sourceId: DModelSourceId) => {
-    setModelSources(modelSources.filter(source => source.sourceId !== sourceId));
-    setSelectedSourceId(null);
-  };
-
   return <>
 
-    {/* Instantiating Vendors */}
     <AddVendor />
 
     <Divider />
 
-    {/* Configuration for this vendor */}
-    <ConfigureSources
-      llmSources={modelSources}
-      selectedSourceId={selectedSourceId}
-      setSelectedSourceId={setSelectedSourceId}
-      onDeleteSourceId={handleDeleteSourceId}
-    />
+    <ConfigureSources />
 
     {/*<Divider />*/}
 
