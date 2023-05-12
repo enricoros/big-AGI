d24e9751dbc9ca723a464f0f79ecefa889db2a73
Enrico Ros <enrico.ros@gmail.com>
2023-05-12 03:18:57 -0700

    Add shared Key Input component

---
+++ src/common/components/FormInputKey.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+
+import { FormControl, FormHelperText, FormLabel, IconButton, Input } from '@mui/joy';
+import KeyIcon from '@mui/icons-material/Key';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
+
+export function FormInputKey(props: {
+  label?: string, description?: string | React.JSX.Element,
+  value: string, onChange: (value: string) => void,
+  required: boolean, validate?: (value: string) => boolean,
+  placeholder?: string, isVisible?: boolean,
+  noFormControl?: boolean,
+}) {
+
+  // internal state is only whether the text is visible or not - the actual value is stored in the parent
+  const [isVisible, setIsVisible] = React.useState(!!props.isVisible);
+
+  const handleChange = (e: React.ChangeEvent) => props.onChange((e.target as HTMLInputElement).value);
+
+  const endDecorator = React.useMemo(() => !!props.value && (
+    <IconButton variant='plain' color='neutral' onClick={() => setIsVisible(!isVisible)}>
+      {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+    </IconButton>
+  ), [props.value, isVisible]);
+
+  const isError = (/*props.required ||*/ !!props.value) && !!props.validate && !props.validate(props.value);
+
+  const Wrapper = props.noFormControl ? React.Fragment : FormControl;
+
+  return <Wrapper>
+
+    {!!props.label && <FormLabel>{props.label}</FormLabel>}
+
+    <Input
+      variant={props.required ? 'outlined' : 'outlined' /* 'soft */}
+      value={props.value} onChange={handleChange}
+      placeholder={props.required ? props.placeholder ? 'required: ' + props.placeholder : 'required' : props.placeholder || '...'}
+      type={isVisible ? 'text' : 'password'}
+      error={isError}
+      startDecorator={<KeyIcon />}
+      endDecorator={endDecorator}
+    />
+
+    {props.description && <FormHelperText sx={{ display: 'block' }}>{props.description}</FormHelperText>}
+
+  </Wrapper>;
+}
