e74e443eabd9b6f4aa26e4ae40eab5085a3f9a71
Enrico Ros <enrico.ros@gmail.com>
2023-05-13 13:30:48 -0700

    Default Source Creation

--- src/modules/llms/configurator/Configurator.tsx
+++ src/modules/llms/configurator/Configurator.tsx
@@ -4,19 +4,23 @@ import { shallow } from 'zustand/shallow';
 import { Divider } from '@mui/joy';
 
 import { GoodModal } from '~/common/components/GoodModal';
-import { useSettingsStore } from '~/common/state/store-settings';
 import { useUIStore } from '~/common/state/store-ui';
 
 import { LLMList } from './LLMList';
 import { EditSources } from './EditSources';
-import { DModelSourceId, useModelsStore } from '../store-models';
+import { DModelSource, DModelSourceId, useModelsStore } from '../store-models';
 import { configureVendorSource } from '~/modules/llms/vendors-registry';
+
+
+function SourceConfiguration(props: { source: DModelSource }) {
+  return configureVendorSource(props.source.vendorId, props.source.sourceId);
+}
 
 
 export function Configurator() {
 
   // local state
-  const [selectedSourceId, setSelectedSourceId] = React.useState<DModelSourceId | null>(null);
+  const [_selectedSourceId, setSelectedSourceId] = React.useState<DModelSourceId | null>(null);
 
   // external state
   const { modelingOpen, openModeling, closeModeling } = useUIStore();
@@ -25,26 +29,26 @@ export function Configurator() {
     llmCount: state.llms.length,
   }), shallow);
 
-  const { OLD_SKOOL_DEL_apiKey } = useSettingsStore(state => ({ OLD_SKOOL_DEL_apiKey: state.apiKey }), shallow);
+  // auto-select the first source - note: we could use a useEffect() here, but this is more efficient
+  // also note that state-persistence is unneeded
+  const selectedSourceId = _selectedSourceId ?? modelSources[0]?.sourceId ?? null;
 
   const activeSource = modelSources.find(source => source.sourceId === selectedSourceId);
-  const vendorConfigComponent = (activeSource && selectedSourceId) ? configureVendorSource(activeSource.vendorId, selectedSourceId) : null;
-
 
   // show the Configuration Dialog at startup if the API key is required but not set
   React.useEffect(() => {
-    // if (!hasServerKeyOpenAI && !isValidOpenAIApiKey(apiKey))
-    openModeling();
-  }, [OLD_SKOOL_DEL_apiKey, openModeling]);
+    if (!activeSource)
+      openModeling();
+  }, [activeSource, openModeling]);
 
   return (
     <GoodModal title='Configure AI Models' open={modelingOpen} onClose={closeModeling}>
 
       <EditSources selectedSourceId={selectedSourceId} setSelectedSourceId={setSelectedSourceId} />
 
-      {!!vendorConfigComponent && <Divider />}
+      {!!activeSource && <Divider />}
 
-      {vendorConfigComponent}
+      {!!activeSource && <SourceConfiguration source={activeSource} />}
 
       {!!llmCount && <Divider />}
 
