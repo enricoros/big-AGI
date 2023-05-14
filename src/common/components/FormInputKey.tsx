081cd226841a5e9d7528a521479ab2429d5dacd3
Enrico Ros <enrico.ros@gmail.com>
2023-05-14 01:59:01 -0700

    Show LocalAI models

--- src/common/components/FormInputKey.tsx
+++ src/common/components/FormInputKey.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 
-import { FormControl, FormHelperText, FormLabel, IconButton, Input } from '@mui/joy';
+import { Box, FormControl, FormHelperText, FormLabel, IconButton, Input } from '@mui/joy';
 import KeyIcon from '@mui/icons-material/Key';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 
 export function FormInputKey(props: {
-  label?: string, description?: string | React.JSX.Element,
+  label?: string, rightLabel?: string | React.JSX.Element,
+  description?: string | React.JSX.Element,
   value: string, onChange: (value: string) => void,
   placeholder?: string, isVisible?: boolean,
   required: boolean, isError?: boolean,
   noFormControl?: boolean,
+  noKey?: boolean,
 }) {
 
   // internal state is only whether the text is visible or not - the actual value is stored in the parent
@@ -19,25 +21,30 @@ export function FormInputKey(props: {
 
   const handleChange = (e: React.ChangeEvent) => props.onChange((e.target as HTMLInputElement).value);
 
-  const endDecorator = React.useMemo(() => !!props.value && (
+  const endDecorator = React.useMemo(() => !!props.value && !props.noKey && (
     <IconButton variant='plain' color='neutral' onClick={() => setIsVisible(!isVisible)}>
       {isVisible ? <VisibilityIcon /> : <VisibilityOffIcon />}
     </IconButton>
-  ), [props.value, isVisible]);
+  ), [props.value, props.noKey, isVisible]);
 
   const Wrapper = props.noFormControl ? React.Fragment : FormControl;
 
   return <Wrapper>
 
-    {!!props.label && <FormLabel>{props.label}</FormLabel>}
+    {!!props.label && <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1, alignItems: 'baseline', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+      <FormLabel>{props.label}</FormLabel>
+      {!!props.rightLabel && <FormHelperText sx={{ display: 'block' }}>
+        {props.rightLabel}
+      </FormHelperText>}
+    </Box>}
 
     <Input
       variant={props.required ? 'outlined' : 'outlined' /* 'soft */}
       value={props.value} onChange={handleChange}
       placeholder={props.required ? props.placeholder ? 'required: ' + props.placeholder : 'required' : props.placeholder || '...'}
-      type={isVisible ? 'text' : 'password'}
+      type={(isVisible || !!props.noKey) ? 'text' : 'password'}
       error={props.isError}
-      startDecorator={<KeyIcon />}
+      startDecorator={!props.noKey && <KeyIcon />}
       endDecorator={endDecorator}
     />
 
--- src/modules/llms/localai/LocalAISetup.tsx
+++ src/modules/llms/localai/LocalAISetup.tsx
@@ -1,32 +1,84 @@
 import * as React from 'react';
+import { z } from 'zod';
 
-import { FormControl, FormHelperText, FormLabel, Input } from '@mui/joy';
+import { Box, Button } from '@mui/joy';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
+import { api } from '~/modules/trpc/trpc.client';
+
+import { settingsGap } from '~/common/theme';
 import { Link } from '~/common/components/Link';
 
-import { DModelSourceId } from '../llm.types';
+import { DLLM, DModelSource, DModelSourceId } from '../llm.types';
 import { normalizeSetup, SourceSetupLocalAI } from './vendor';
-import { useSourceSetup } from '../llm.store';
+import { useModelsStore, useSourceSetup } from '../llm.store';
+import { FormInputKey } from '~/common/components/FormInputKey';
+
+
+const urlSchema = z.string().url().startsWith('http');
 
 
 export function LocalAISetup(props: { sourceId: DModelSourceId }) {
 
   // external state
-  const { normSetup: { hostUrl }, updateSetup } = useSourceSetup<SourceSetupLocalAI>(props.sourceId, normalizeSetup);
+  const { normSetup: { hostUrl }, updateSetup, sourceLLMs, source } = useSourceSetup<SourceSetupLocalAI>(props.sourceId, normalizeSetup);
 
-  return (
+  // validate if url is a well formed proper url with zod
+  const { success: isValidHost } = urlSchema.safeParse(hostUrl);
+  const shallFetchSucceed = isValidHost;
 
-    <FormControl>
-      <FormLabel>Server URL</FormLabel>
-      <Input
-        value={hostUrl}
-        onChange={event => updateSetup({ hostUrl: event.target.value })}
-        placeholder='e.g., http://localhost:8080'
-      />
-      <FormHelperText sx={{ display: 'block' }}>
-        <Link level='body2' href='https://github.com/go-skynet/LocalAI' target='_blank'>Learn more</Link> about LocalAI
-      </FormHelperText>
-    </FormControl>
+  // fetch models
+  const { isFetching, refetch } = api.openai.listModels.useQuery({ oaiKey: '', oaiHost: hostUrl, oaiOrg: '', heliKey: '' }, {
+    enabled: !sourceLLMs.length && shallFetchSucceed,
+    onSuccess: models => {
+      const llms = source ? models.map(model => localAIToDLLM(model, source)) : [];
+      useModelsStore.getState().addLLMs(llms);
+    },
+    staleTime: Infinity,
+  });
 
-  );
+  return <Box sx={{ display: 'flex', flexDirection: 'column', gap: settingsGap }}>
+
+    <FormInputKey
+      required noKey
+      label='Server URL' rightLabel={<Link level='body2' href='https://github.com/go-skynet/LocalAI' target='_blank'>Learn more</Link>}
+      placeholder='e.g., http://127.0.0.1:8080'
+      value={hostUrl} onChange={value => updateSetup({ hostUrl: value })}
+    />
+
+    <Button
+      variant='solid' color='neutral'
+      disabled={!shallFetchSucceed || isFetching}
+      endDecorator={<FileDownloadIcon />}
+      onClick={() => refetch()}
+      sx={{ minWidth: 120, ml: 'auto' }}
+    >
+      Models
+    </Button>
+
+  </Box>;
+}
+
+
+function localAIToDLLM(model: { id: string, object: 'model' }, source: DModelSource): DLLM {
+  let label = model.id
+    .replace('ggml-', '')
+    .replace('.bin', '')
+    .replaceAll('-', ' ');
+  // capitalize first letter of each word
+  if (label.length)
+    label = label.charAt(0).toUpperCase() + label.slice(1);
+  // shall we do some heuristics
+  const contextTokens = 4096; // FIXME
+  return {
+    id: `${source.id}-${model.id}`,
+    label,
+    created: 0,
+    description: 'Local model',
+    tags: ['stream', 'chat'],
+    contextTokens,
+    sId: source.id,
+    _source: source,
+    settings: {},
+  };
 }
