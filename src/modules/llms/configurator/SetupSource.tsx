2ef75ec94b0254c791aabd27b4d9301b5c5aa9e3
Enrico Ros <enrico.ros@gmail.com>
2023-05-13 17:29:45 -0700

    Move class

---
+++ src/modules/llms/configurator/SetupSource.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+import { DModelSource } from '../store-models';
+import { findVendor } from '../vendors-registry';
+
+
+export function SetupSource(props: { source: DModelSource }) {
+  const vendor = findVendor(props.source.vendorId);
+  return vendor?.createSetupComponent(props.source.sourceId) ?? null;
+}
