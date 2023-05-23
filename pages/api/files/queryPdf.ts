628bb2d98d77cc419e37eae3c9daa9e538ec1cec
Michael Yang <michaelyang@Michaels-MBP.attlocal.net>
2023-05-23 00:25:24 -0700

    change gpt version

--- pages/api/files/queryPdf.ts
+++ pages/api/files/queryPdf.ts
@@ -32,7 +32,7 @@ export const queryDocuments = async (query: string, chatHistory?: string) => {
     // concurrent request limit to prevent rate limiting
     maxConcurrency: 10,
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-4', //change this to gpt-4 if you have access
+    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
   });
 
   let pineconeIndexName = process.env.PINECONE_INDEX_NAME || '';
