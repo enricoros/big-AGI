65356260b43bae9dc96e0bbb8c28363b20e7bcb5
Enrico Ros <enrico.ros@gmail.com>
2023-06-19 10:27:38 -0700

    Selection of Fast/Chat Models

--- src/modules/llms/configurator/LLMOptions.tsx
+++ src/modules/llms/configurator/LLMOptions.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { shallow } from 'zustand/shallow';
 
-import { Button, Divider, FormControl, FormLabel, Input, Switch, Typography } from '@mui/joy';
+import { Button, ButtonGroup, Divider, FormControl, FormLabel, Input, Switch, Typography } from '@mui/joy';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
@@ -21,10 +21,14 @@ export function LLMOptions(props: { id: DLLMId }) {
 
   // external state
   const closeLLMOptions = useUIStateStore(state => state.closeLLMOptions);
-  const { llm, removeLLM, updateLLM } = useModelsStore(state => ({
+  const { llm, removeLLM, updateLLM, isChatLLM, setChatLLMId, isFastLLM, setFastLLMId } = useModelsStore(state => ({
     llm: state.llms.find(llm => llm.id === props.id),
     removeLLM: state.removeLLM,
     updateLLM: state.updateLLM,
+    isChatLLM: state.chatLLMId === props.id,
+    isFastLLM: state.fastLLMId === props.id,
+    setChatLLMId: state.setChatLLMId,
+    setFastLLMId: state.setFastLLMId,
   }), shallow);
 
   if (!llm)
@@ -61,6 +65,16 @@ export function LLMOptions(props: { id: DLLMId }) {
           Name
         </FormLabel>
         <Input variant='outlined' value={llm.label} onChange={handleLlmLabelSet} />
+      </FormControl>
+
+      <FormControl orientation='horizontal' sx={{ flexWrap: 'wrap', alignItems: 'center' }}>
+        <FormLabel sx={{ minWidth: 80 }}>
+          Defaults
+        </FormLabel>
+        <ButtonGroup orientation='horizontal' size='sm' variant='outlined'>
+          <Button variant={isChatLLM ? 'solid' : undefined} onClick={() => setChatLLMId(isChatLLM ? null : props.id)}>Chat Model</Button>
+          <Button variant={isFastLLM ? 'solid' : undefined} onClick={() => setFastLLMId(isFastLLM ? null : props.id)}>Fast Model</Button>
+        </ButtonGroup>
       </FormControl>
 
       <FormControl orientation='horizontal' sx={{ flexWrap: 'wrap', alignItems: 'center' }}>
