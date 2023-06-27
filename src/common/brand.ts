b1accbeb3063c403700de03f059d6867a721e992
Enrico Ros <enrico.ros@gmail.com>
2023-06-27 00:35:45 -0700

    Some Meta Improvements

--- src/common/brand.ts
+++ src/common/brand.ts
@@ -2,29 +2,26 @@
  * Application Identity (Brand)
  *
  * Also note that the 'Brand' is used in the following places:
- *  - README.md             all over
- *  - package.json          app-slug and version
- *  - public/manifest.json  name, short_name, description, theme_color, background_color
+ *  - README.md               all over
+ *  - package.json            app-slug and version
+ *  - [public/manifest.json]  name, short_name, description, theme_color, background_color
  */
 export const Brand = {
-  // Name: 'big-AGI',
-  // UpperName: 'BIG-AGI',
   Title: {
     Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
-    SiteName: 'big-AGI',
-    Title: 'big-AGI: Personal AGI App',
-    Description: 'big-AGI is a free, open-source project to build a general artificial intelligence (AGI) that can solve any problem.',
-    Keywords: 'artificial general intelligence, agi, openai, gpt-4, ai personas, code execution, pdf import, voice i/o, ai chat, artificial intelligence',
+    Description: 'Leading open-source AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
+    SiteName: 'big-AGI | Harnessing AI for You',
     ThemeColor: '#434356',
-    TwitterSite: '@bigagienergy',
+    TwitterSite: '@enricoros',
   },
   URIs: {
-    // Slug: 'big-agi',
-    Home: 'https://www.big-agi.com',
+    Home: 'https://big-agi.com',
+    // App: 'https://get.big-agi.com',
     CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
     OpenRepo: 'https://github.com/enricoros/big-agi',
     SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
+    // Twitter: 'https://www.twitter.com/enricoros',
   },
 };
