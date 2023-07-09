eae89b9030ad509ea5095eac169c239538ba35ac
Liam Bilich <liam@cogniteam.com>
2023-07-09 15:05:18 +0300

    Refactored purpose-selector store to handle system-purposes.json file, added try-catch block to handle file import error, and updated default state and toggleHiddenPurposeId action.

--- src/apps/chat/components/purpose-selector/store-purposes.ts
+++ src/apps/chat/components/purpose-selector/store-purposes.ts
@@ -1,38 +1,45 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import systemPurposesJson from './../../../../../customization/system-purposes.json';
+
+let systemPurposesJson: any
+
+try {
+    systemPurposesJson = require('./../../../../../customization/system-purposes.json');
+} catch (error) {
+    systemPurposesJson = { Settings: { hiddenPurposeIDs: ['Designer'] } };
+}
 
 interface PurposeStore {
 
-  // state
-  hiddenPurposeIDs: string[];
+    // state
+    hiddenPurposeIDs: string[];
 
-  // actions
-  toggleHiddenPurposeId: (purposeId: string) => void;
+    // actions
+    toggleHiddenPurposeId: (purposeId: string) => void;
 
 }
 
 
 export const usePurposeStore = create<PurposeStore>()(
-  persist(
-    (set) => ({
+    persist(
+        (set) => ({
 
-      // default state
-      hiddenPurposeIDs: systemPurposesJson.Settings.hiddenPurposeIDs || ['Designer'],
+            // default state
+            hiddenPurposeIDs: systemPurposesJson.Settings.hiddenPurposeIDs,
 
-      toggleHiddenPurposeId: (purposeId: string) => {
-        set(state => {
-          const hiddenPurposeIDs = state.hiddenPurposeIDs.includes(purposeId)
-            ? state.hiddenPurposeIDs.filter((id) => id !== purposeId)
-            : [...state.hiddenPurposeIDs, purposeId];
-          return {
-            hiddenPurposeIDs,
-          };
-        });
-      },
+            toggleHiddenPurposeId: (purposeId: string) => {
+                set(state => {
+                    const hiddenPurposeIDs = state.hiddenPurposeIDs.includes(purposeId)
+                        ? state.hiddenPurposeIDs.filter((id) => id !== purposeId)
+                        : [...state.hiddenPurposeIDs, purposeId];
+                    return {
+                        hiddenPurposeIDs,
+                    };
+                });
+            },
 
-    }),
-    {
-      name: 'app-purpose',
-    }),
+        }),
+        {
+            name: 'app-purpose',
+        }),
 );
