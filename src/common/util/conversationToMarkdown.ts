ecaf8418191c84a071c2841d2ca0c8b783bddc59
Enrico Ros <enrico.ros@gmail.com>
2023-07-08 23:32:35 -0700

    Improve short model naming

--- src/common/util/conversationToMarkdown.ts
+++ src/common/util/conversationToMarkdown.ts
@@ -6,7 +6,9 @@ export function prettyBaseModel(model: string | undefined): string {
   if (!model) return '';
   if (model.includes('gpt-4-32k')) return 'gpt-4-32k';
   if (model.includes('gpt-4')) return 'gpt-4';
+  if (model.includes('gpt-3.5-turbo-16k')) return '3.5 Turbo 16k';
   if (model.includes('gpt-3.5-turbo')) return '3.5 Turbo';
+  if (model.endsWith('.bin')) return model.slice(0, -4);
   return model;
 }
 
