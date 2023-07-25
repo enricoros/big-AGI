f645e5641ef677813a78020dd72173df7b5e98fd
Ross Cohen <rosscohen1990@gmail.com>
2023-07-25 02:42:14 -0400

    Update prodia.models.ts

--- src/modules/prodia/prodia.models.ts
+++ src/modules/prodia/prodia.models.ts
@@ -19,8 +19,6 @@ export const HARDCODED_MODELS: { models: ProdiaModelDescription[] } = {
     { id: 'openjourney_V4.ckpt [ca2f377f]', label: 'Openjourney v4' },
     { id: 'dreamlike-diffusion-1.0.safetensors [5c9fd6e0]', label: 'Dreamlike Diffusion' },
     { id: 'dreamlike-diffusion-2.0.safetensors [fdcf65e7]', label: 'Dreamlike Diffusion 2' },
-    { id: 'portrait+1.0.safetensors [1400e684]', label: 'Portrait' },
-    { id: 'riffusion-model-v1.ckpt [3aafa6fe]', label: 'Riffusion' },
     { id: 'timeless-1.0.ckpt [7c4971d4]', label: 'Timeless' },
     { id: 'dreamshaper_5BakedVae.safetensors [a3fbf318]', label: 'Dreamshaper 5' },
     { id: 'revAnimated_v122.safetensors [3f4fefd9]', label: 'ReV Animated V1.2.2' },
@@ -30,6 +28,16 @@ export const HARDCODED_MODELS: { models: ProdiaModelDescription[] } = {
     { id: 'dreamshaper_6BakedVae.safetensors [114c8abb]', label: 'Dreamshaper 6' },
     { id: 'AOM3A3_orangemixs.safetensors [9600da17]', label: 'Abyss Orange v3' },
     { id: 'shoninsBeautiful_v10.safetensors [25d8c546]', label: 'Shonin Beautiful People' },
+    // New models added below
+    { id: 'dreamshaper_7.safetensors [5cf5ae06]', label: 'Dreamshaper 7' },
+    { id: 'EimisAnimeDiffusion_V1.ckpt [4f828a15]', label: 'Eimis Anime Diffusion' },
+    { id: 'lyriel_v16.safetensors [68fceea2]', label: 'Lyriel v16' },
+    { id: 'meinamix_meinaV11.safetensors [b56ce717]', label: 'meinamix meinaV11' },
+    { id: 'portraitplus_V1.0.safetensors [1400e684]', label: 'portraitplus V1.0' },
+    { id: 'Realistic_Vision_V1.4-pruned-fp16.safetensors [8d21810b]', label: 'Realistic Vision v1.4' },
+    { id: 'Realistic_Vision_V4.0.safetensors [29a7afaa]', label: 'Realistic Vision V4.0' },
+    { id: 'Realistic_Vision_V2.0.safetensors [79587710]', label: 'Realistic Vision V2.0' },
+    { id: 'redshift_diffusion-V10.safetensors [1400e684]', label: 'redshift diffusion V10' },
   ]
     // sort by priority
     .sort((a, b) => (b.priority ?? 0) - (a.priority ?? 0)),
