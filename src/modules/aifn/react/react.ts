2a50347fc3c8726d0bf6f6146e9ffa5f9326852b
Enrico Ros <enrico.ros@gmail.com>
2023-07-24 19:56:51 -0700

    Fold this

--- src/modules/aifn/react/react.ts
+++ src/modules/aifn/react/react.ts
@@ -1,9 +1,56 @@
-import { DLLMId } from '~/modules/llms/llm.types';
-import { OpenAI } from '~/modules/llms/openai/openai.types';
-import { callApiSearchGoogle } from '~/modules/google/search.client';
-import { callChatGenerate } from '~/modules/llms/llm.client';
+/*
+ * porting of implementation from here: https://til.simonwillison.net/llms/python-react-pattern
+ */
 
-import { currentDate, reActPrompt } from '../prompts';
+import { DLLMId } from '~/modules/llms/llm.types';
+import { callApiSearchGoogle } from '~/modules/google/search.client';
+import { callChatGenerate, VChatMessageIn } from '~/modules/llms/llm.client';
+
+
+// prompt to implement the ReAct paradigm: https://arxiv.org/abs/2210.03629
+const reActPrompt: string =
+  `You are a Question Answering AI with reasoning ability.
+You will receive a Question from the User.
+In order to answer any Question, you run in a loop of Thought, Action, PAUSE, Observation.
+If from the Thought or Observation you can derive the answer to the Question, you MUST also output an "Answer: ", followed by the answer and the answer ONLY, without explanation of the steps used to arrive at the answer.
+You will use "Thought: " to describe your thoughts about the question being asked.
+You will use "Action: " to run one of the actions available to you - then return PAUSE. NEVER continue generating "Observation: " or "Answer: " in the same response that contains PAUSE.
+"Observation" will be presented to you as the result of previous "Action".
+If the "Observation" you received is not related to the question asked, or you cannot derive the answer from the observation, change the Action to be performed and try again.
+
+ALWAYS assume today as {{currentDate}} when dealing with questions regarding dates.
+Never mention your knowledge cutoff date
+
+Your available "Actions" are:
+
+google:
+e.g. google: Django
+Returns google custom search results
+ALWAYS look up on google when the question is related to live events or factual information, such as sports, news, or weather.
+
+calculate:
+e.g. calculate: 4 * 7 / 3
+Runs a calculation and returns the number - uses Python so be sure to use floating point syntax if necessary
+
+wikipedia:
+e.g. wikipedia: Django
+Returns a summary from searching Wikipedia
+
+ONLY look things up on Wikipedia when explicitly asked to do so.
+
+Example session:
+
+Question: What is the capital of France?
+Thought: I should look up France on Wikipedia
+Action: wikipedia: France
+
+You will be called again with the following, along with all previous messages between the User and You:
+
+Observation: France is a country. The capital is Paris.
+
+You then output:
+Answer: The capital of France is Paris
+`;
 
 
 export const CmdRunReact: string[] = ['/react'];
@@ -20,7 +67,7 @@ const actionRe = /^Action: (\w+): (.*)$/;
  *   - loop() is a function that will update the state (in place)
  */
 interface State {
-  messages: OpenAI.Wire.ChatCompletion.RequestMessage[];
+  messages: VChatMessageIn[];
   nextPrompt: string;
   lastObservation: string;
   result: string | undefined;
@@ -51,7 +98,7 @@ export class Agent {
 
   initialize(question: string): State {
     return {
-      messages: [{ role: 'system', content: reActPrompt.replaceAll('{{currentDate}}', currentDate()) }],
+      messages: [{ role: 'system', content: reActPrompt.replaceAll('{{currentDate}}', new Date().toISOString().slice(0, 10)) }],
       nextPrompt: question,
       lastObservation: '',
       result: undefined,
