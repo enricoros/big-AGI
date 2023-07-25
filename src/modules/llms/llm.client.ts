7c568c5ce62845187a322e1aedcaffc1ebc9a5bb
Enrico Ros <enrico.ros@gmail.com>
2023-07-24 21:49:10 -0700

    Four model providers all working well

--- src/modules/llms/llm.client.ts
+++ src/modules/llms/llm.client.ts
@@ -5,7 +5,7 @@ import type { DMessage } from '~/common/state/store-chats';
 import type { ChatStreamSchema } from './openai/openai.router';
 import type { DLLM, DLLMId, ModelVendor } from './llm.types';
 import type { OpenAI } from './openai/openai.types';
-import type { SourceSetupOpenAI } from './openai/openai.vendor';
+import { ModelVendorOpenAI, SourceSetupOpenAI } from './openai/openai.vendor';
 import { findVendorById } from './vendor.registry';
 import { useModelsStore } from './store-llms';
 
@@ -131,7 +131,7 @@ async function vendorStreamChat(vendor: ModelVendor, llm: DLLM, messages: VChatM
     body: JSON.stringify({
       // map all to OpenAI, apart from Anthropic
       vendorId: vendor.id === 'anthropic' ? 'anthropic' : 'openai',
-      access: sourceSetup as any,
+      access: ModelVendorOpenAI.normalizeSetup(sourceSetup as any),
       model: {
         id: llmRef,
         temperature: llmTemperature,
