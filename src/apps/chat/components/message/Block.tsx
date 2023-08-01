2f15225987594b26cc48e0eddf1be3077feda646
Enrico Ros <enrico.ros@gmail.com>
2023-08-01 07:10:44 -0700

    Support multiple /imagine generations (.. xN, .. [N])

--- src/apps/chat/components/message/Block.tsx
+++ src/apps/chat/components/message/Block.tsx
@@ -28,7 +28,7 @@ export const parseBlocks = (forceText: boolean, text: string): Block[] => {
   if (forceText)
     return [{ type: 'text', content: text }];
 
-  if (text.startsWith('https://images.prodia.xyz/') && text.endsWith('.png') && text.length > 60 && text.length < 70)
+  if (text.startsWith('https://images.prodia.xyz/') && text.endsWith('.png') && text.length > 60)
     return [{ type: 'image', url: text.trim() }];
 
   if (text.startsWith('<!DOCTYPE html') || text.startsWith('<head>\n'))
