46486353f23adf43e77ca5e108bf7b9b1d849b49
Enrico Ros <enrico.ros@gmail.com>
2023-08-17 01:42:21 -0700

    Vast improvements to the Speech Reco hook
    - Fix initial delay, up to ~8s on desktop
    - Much improve this state
    - More improvements and cleanups

--- src/common/components/useSpeechRecognition.ts
+++ src/common/components/useSpeechRecognition.ts
@@ -23,9 +23,11 @@ export function maySpeechRecognitionWork() {
  */
 export const useSpeechRecognition = (onResultCallback: (result: SpeechResult) => void, softStopTimeout: number, useShortcutCtrlKey?: string) => {
   // enablers
-  const [recognition, setRecognition] = React.useState<ISpeechRecognition | null>(null);
+  const refRecognition = React.useRef<ISpeechRecognition | null>(null);
 
   // session
+  const [isSpeechEnabled, setIsSpeechEnabled] = React.useState<boolean>(false);
+  const refStarted = React.useRef<boolean>(false);
   const [isRecordingAudio, setIsRecordingAudio] = React.useState<boolean>(false);
   const [isRecordingSpeech, setIsRecordingSpeech] = React.useState<boolean>(false);
   const [isSpeechError, setIsSpeechError] = React.useState<boolean>(false);
@@ -33,153 +35,176 @@ export const useSpeechRecognition = (onResultCallback: (result: SpeechResult) =>
   // external state (will update this function when changed)
   const preferredLanguage = useUIPreferencesStore(state => state.preferredLanguage);
 
+  // create the Recognition engine
   React.useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window === 'undefined') return;
 
-      // do not re-initialize, just update the language (if we're here there's a high chance the language has changed)
-      if (recognition) {
-        recognition.lang = preferredLanguage;
-        return;
-      }
-
-      // disable speech recognition on iPhones and Safari browsers - because of sub-par quality
-      if (isSafariOriPhone()) {
-        console.log('Speech recognition is disabled on iPhones and Safari browsers.');
-        return;
-      }
-
-      const webSpeechAPI = getSpeechRecognition();
-      if (webSpeechAPI) {
-
-        // local memory within a session
-        const speechResult: SpeechResult = {
-          transcript: '',
-          interimTranscript: '',
-          done: false,
-        };
-
-        const instance = new webSpeechAPI();
-        instance.lang = preferredLanguage;
-        instance.interimResults = isChromeOnDesktopWindows() && softStopTimeout > 0;
-        instance.maxAlternatives = 1;
-        instance.continuous = true;
-
-        // soft inactivity timer
-        let inactivityTimeoutId: any | null = null;
-
-        const clearInactivityTimeout = () => {
-          if (inactivityTimeoutId) {
-            clearTimeout(inactivityTimeoutId);
-            inactivityTimeoutId = null;
-          }
-        };
-
-        const reloadInactivityTimeout = (timeoutMs: number) => {
-          clearInactivityTimeout();
-          inactivityTimeoutId = setTimeout(() => {
-            inactivityTimeoutId = null;
-            instance.stop();
-          }, timeoutMs);
-        };
-
-        instance.onaudiostart = () => setIsRecordingAudio(true);
-
-        instance.onaudioend = () => setIsRecordingAudio(false);
-
-        instance.onspeechstart = () => setIsRecordingSpeech(true);
-
-        instance.onspeechend = () => setIsRecordingSpeech(false);
-
-        instance.onstart = () => {
-          speechResult.transcript = '';
-          speechResult.interimTranscript = 'Listening...';
-          speechResult.done = false;
-          onResultCallback(speechResult);
-          if (instance.interimResults)
-            reloadInactivityTimeout(2 * softStopTimeout);
-        };
-
-        instance.onend = () => {
-          clearInactivityTimeout();
-          speechResult.interimTranscript = '';
-          speechResult.done = true;
-          onResultCallback(speechResult);
-        };
-
-        instance.onerror = event => {
-          if (event.error !== 'no-speech') {
-            console.error('Error occurred during speech recognition:', event.error);
-            setIsSpeechError(true);
-          }
-        };
-
-        instance.onresult = (event: ISpeechRecognitionEvent) => {
-          if (!event?.results?.length) return;
-
-          // coalesce all the final pieces into a cohesive string
-          speechResult.transcript = '';
-          speechResult.interimTranscript = '';
-          for (let result of event.results) {
-            let chunk = result[0]?.transcript?.trim();
-            if (!chunk)
-              continue;
-
-            // [EN] spoken punctuation marks -> actual characters
-            chunk = chunk
-              .replaceAll(' comma', ',')
-              .replaceAll(' exclamation mark', '!')
-              .replaceAll(' period', '.')
-              .replaceAll(' question mark', '?');
-
-            // capitalize
-            if (chunk.length >= 2 && (result.isFinal || !speechResult.interimTranscript))
-              chunk = chunk.charAt(0).toUpperCase() + chunk.slice(1);
-
-            // add ending
-            if (result.isFinal && !chunk.endsWith('.') && !chunk.endsWith('!') && !chunk.endsWith('?') && !chunk.endsWith(':') && !chunk.endsWith(';') && !chunk.endsWith(','))
-              chunk += '.';
-
-            if (result.isFinal)
-              speechResult.transcript += chunk + ' ';
-            else
-              speechResult.interimTranscript += chunk + ' ';
-          }
-
-          // update the UI
-          onResultCallback(speechResult);
-
-          // auto-stop
-          if (instance.interimResults)
-            reloadInactivityTimeout(softStopTimeout);
-        };
-
-
-        setRecognition(instance);
-      }
+    // do not re-initialize, just update the language (if we're here there's a high chance the language has changed)
+    if (refRecognition.current) {
+      refRecognition.current.lang = preferredLanguage;
+      return;
     }
-  }, [onResultCallback, preferredLanguage, recognition, softStopTimeout]);
+
+    // skip speech recognition on iPhones and Safari browsers - because of sub-par quality
+    if (isSafariOriPhone()) {
+      console.log('Speech recognition is disabled on iPhones and Safari browsers.');
+      return;
+    }
+
+    const webSpeechAPI = getSpeechRecognition();
+    if (!webSpeechAPI)
+      return;
+
+    // local memory within a session
+    const speechResult: SpeechResult = {
+      transcript: '',
+      interimTranscript: '',
+      done: false,
+    };
+
+    const instance = new webSpeechAPI();
+    instance.lang = preferredLanguage;
+    instance.interimResults = isChromeOnDesktopWindows() && softStopTimeout > 0;
+    instance.maxAlternatives = 1;
+    instance.continuous = true;
+
+    // soft inactivity timer
+    let inactivityTimeoutId: any | null = null;
+
+    const clearInactivityTimeout = () => {
+      if (inactivityTimeoutId) {
+        clearTimeout(inactivityTimeoutId);
+        inactivityTimeoutId = null;
+      }
+    };
+
+    const reloadInactivityTimeout = (timeoutMs: number) => {
+      clearInactivityTimeout();
+      inactivityTimeoutId = setTimeout(() => {
+        inactivityTimeoutId = null;
+        instance.stop();
+      }, timeoutMs);
+    };
+
+    instance.onaudiostart = () => setIsRecordingAudio(true);
+
+    instance.onaudioend = () => setIsRecordingAudio(false);
+
+    instance.onspeechstart = () => setIsRecordingSpeech(true);
+
+    instance.onspeechend = () => setIsRecordingSpeech(false);
+
+    instance.onstart = () => {
+      refStarted.current = true;
+      speechResult.transcript = '';
+      speechResult.interimTranscript = 'Listening...';
+      speechResult.done = false;
+      onResultCallback(speechResult);
+      // let the system handle the first stop (as long as possible)
+      // if (instance.interimResults)
+      //   reloadInactivityTimeout(2 * softStopTimeout);
+    };
+
+    instance.onend = () => {
+      refStarted.current = false;
+      clearInactivityTimeout();
+      speechResult.interimTranscript = '';
+      speechResult.done = true;
+      onResultCallback(speechResult);
+    };
+
+    instance.onerror = event => {
+      if (event.error !== 'no-speech') {
+        console.error('Error occurred during speech recognition:', event.error);
+        setIsSpeechError(true);
+      }
+    };
+
+    instance.onresult = (event: ISpeechRecognitionEvent) => {
+      if (!event?.results?.length) return;
+
+      // coalesce all the final pieces into a cohesive string
+      speechResult.transcript = '';
+      speechResult.interimTranscript = '';
+      for (let result of event.results) {
+        let chunk = result[0]?.transcript?.trim();
+        if (!chunk)
+          continue;
+
+        // [EN] spoken punctuation marks -> actual characters
+        chunk = chunk
+          .replaceAll(' comma', ',')
+          .replaceAll(' exclamation mark', '!')
+          .replaceAll(' period', '.')
+          .replaceAll(' question mark', '?');
+
+        // capitalize
+        if (chunk.length >= 2 && (result.isFinal || !speechResult.interimTranscript))
+          chunk = chunk.charAt(0).toUpperCase() + chunk.slice(1);
+
+        // add ending
+        if (result.isFinal && !chunk.endsWith('.') && !chunk.endsWith('!') && !chunk.endsWith('?') && !chunk.endsWith(':') && !chunk.endsWith(';') && !chunk.endsWith(','))
+          chunk += '.';
+
+        if (result.isFinal)
+          speechResult.transcript += chunk + ' ';
+        else
+          speechResult.interimTranscript += chunk + ' ';
+      }
+
+      // update the UI
+      onResultCallback(speechResult);
+
+      // auto-stop
+      if (instance.interimResults)
+        reloadInactivityTimeout(softStopTimeout);
+    };
+
+    // save the instance
+    refRecognition.current = instance;
+    refStarted.current = false;
+    setIsSpeechEnabled(true);
+
+  }, [onResultCallback, preferredLanguage, softStopTimeout]);
 
 
-  const toggleRecording = React.useCallback(() => {
-    if (!recognition)
-      return console.error('Speech recognition is not supported or not initialized.');
+  // ACTIONS: start/stop recording
+
+  const startRecording = React.useCallback(() => {
+    if (!refRecognition.current)
+      return console.error('startRecording: Speech recognition is not supported or not initialized.');
+    if (refStarted.current)
+      return console.error('startRecording: Start recording called while already recording.');
 
     setIsSpeechError(false);
-    if (!isRecordingAudio) {
-      try {
-        recognition.start();
-      } catch (error: any) {
-        setIsSpeechError(true);
-        console.log('Speech recognition error - clicking too quickly?:', error?.message);
-      }
-    } else
-      recognition.stop();
-  }, [recognition, isRecordingAudio]);
+    try {
+      refRecognition.current.start();
+    } catch (error: any) {
+      setIsSpeechError(true);
+      console.log('Speech recognition error - clicking too quickly?', error?.message);
+    }
+  }, []);
+
+  const stopRecording = React.useCallback(() => {
+    if (!refRecognition.current)
+      return console.error('stopRecording: Speech recognition is not supported or not initialized.');
+    if (!refStarted.current)
+      return console.error('stopRecording: Stop recording called while not recording.');
+
+    refRecognition.current.stop();
+  }, []);
+
+  const toggleRecording = React.useCallback(() => {
+    if (refStarted.current)
+      stopRecording();
+    else
+      startRecording();
+  }, [startRecording, stopRecording]);
 
   React.useEffect(() => {
     if (!useShortcutCtrlKey) return;
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (useShortcutCtrlKey && event.ctrlKey && event.key === useShortcutCtrlKey)
+      if (event.ctrlKey && event.key === useShortcutCtrlKey)
         toggleRecording();
     };
     window.addEventListener('keydown', handleKeyDown);
@@ -187,10 +212,12 @@ export const useSpeechRecognition = (onResultCallback: (result: SpeechResult) =>
   }, [toggleRecording, useShortcutCtrlKey]);
 
   return {
-    isSpeechEnabled: !!recognition,
+    isSpeechEnabled,
     isSpeechError,
     isRecordingAudio,
     isRecordingSpeech,
+    startRecording,
+    stopRecording,
     toggleRecording,
   };
 };
