7b107df84e29e00aa1ad29c675c79e90901b6f8d
Enrico Ros <enrico.ros@gmail.com>
2023-08-17 08:35:32 -0700

    fix Chat Message errors displayed as objects

--- src/common/components/InlineError.tsx
+++ src/common/components/InlineError.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 
 import { Alert, Typography } from '@mui/joy';
 
-export function InlineError(props: { error: any, severity?: 'warning' | 'danger' }) {
+export function InlineError(props: { error: React.JSX.Element | null | any, severity?: 'warning' | 'danger' }) {
   return <Alert variant='soft' color={props.severity || 'warning'} sx={{ mt: 1 }}>
     <Typography level='body-sm' color={props.severity || 'warning'}>
-      {props.error?.message || props.error?.toString() || 'Unknown error'}
+      {props.error?.message || props.error || 'Unknown error'}
     </Typography>
   </Alert>;
 }
