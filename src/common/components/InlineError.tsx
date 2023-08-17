c3493d4112e1206195339494d18cc5b732444e29
Enrico Ros <enrico.ros@gmail.com>
2023-08-16 21:36:08 -0700

    Fix Error

--- src/common/components/InlineError.tsx
+++ src/common/components/InlineError.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
-import { Alert, ColorPaletteProp, Typography } from '@mui/joy';
+import { Alert, Typography } from '@mui/joy';
 
-export function InlineError(props: { error: any, severity?: 'warning' | 'error' }) {
-  return <Alert variant='soft' color={(props.severity as ColorPaletteProp) || 'warning'} sx={{ mt: 1 }}>
-    <Typography level='body-sm' color={(props.severity as ColorPaletteProp) || 'warning'}>
+export function InlineError(props: { error: any, severity?: 'warning' | 'danger' }) {
+  return <Alert variant='soft' color={props.severity || 'warning'} sx={{ mt: 1 }}>
+    <Typography level='body-sm' color={props.severity || 'warning'}>
       {props.error?.message || props.error?.toString() || 'Unknown error'}
     </Typography>
   </Alert>;
