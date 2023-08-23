bcbe65ef1fb83772f72113308f8754da49447a28
Enrico Ros <enrico.ros@gmail.com>
2023-08-23 08:58:34 -0700

    Dynamic Code Highligher/Type Inferrer import. Large performance gains.

--- src/apps/chat/components/message/codePrism.tsx
+++ src/apps/chat/components/message/codePrism.tsx
@@ -1,6 +1,6 @@
 import Prism from 'prismjs';
 
-// per-language plugins
+// per-language JS plugins
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-java';
