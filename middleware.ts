58439719320753289658807d5de48ce9ed1a76e6
Peter B Smith <hello@peterbsmith.com>
2023-09-05 04:06:35 -0400

    fix re-route in production

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,9 @@ function noopmiddleware() {}
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default HAS_AUTH ? authMiddleware({}) : noopmiddleware;
+export default HAS_AUTH ? authMiddleware({
+  publicRoutes: ['/']
+}) : noopmiddleware;
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
