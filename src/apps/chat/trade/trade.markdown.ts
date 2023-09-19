c1d0093d48d23cd05285afb4fc04d70a4c4fee33
Enrico Ros <enrico.ros@gmail.com>
2023-09-19 08:39:34 -0700

    3.5 Turbo Instruct models - not supported for Chat (only /completions)

--- src/apps/chat/trade/trade.markdown.ts
+++ src/apps/chat/trade/trade.markdown.ts
@@ -6,6 +6,7 @@ export function prettyBaseModel(model: string | undefined): string {
   if (!model) return '';
   if (model.includes('gpt-4-32k')) return 'gpt-4-32k';
   if (model.includes('gpt-4')) return 'gpt-4';
+  if (model.includes('gpt-3.5-turbo-instruct')) return '3.5 Turbo Instruct';
   if (model.includes('gpt-3.5-turbo-16k')) return '3.5 Turbo 16k';
   if (model.includes('gpt-3.5-turbo')) return '3.5 Turbo';
   if (model.endsWith('.bin')) return model.slice(0, -4);
