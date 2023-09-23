2f92c81bee6368ec02d1621e357f9f1cf1b37e3e
Enrico Ros <enrico.ros@gmail.com>
2023-09-22 21:15:16 -0700

    Llms: small move

--- src/modules/aifn/summarize/summerize.ts
+++ src/modules/aifn/summarize/summerize.ts
@@ -1,5 +1,5 @@
-import { DLLMId } from '~/modules/llms/store-llms';
-import { callChatGenerate, findLLMOrThrow } from '~/modules/llms/llm.client';
+import { DLLMId, findLLMOrThrow } from '~/modules/llms/store-llms';
+import { callChatGenerate } from '~/modules/llms/llm.client';
 
 
 // prompt to be tried when doing recursive summerization.
