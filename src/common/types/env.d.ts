6022aeee50fa1b9c26c12bd3766d163ccf87c6fa
Enrico Ros <enrico.ros@gmail.com>
2023-10-16 14:58:10 -0700

    Server-Side-Storage (SSS): use Prisma

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -5,6 +5,10 @@ declare namespace NodeJS {
   // available to the server-side
   interface ProcessEnv {
 
+    // Postgres, for optional storage via Prisma
+    POSTGRES_PRISMA_URL?: string;
+    POSTGRES_URL_NON_POOLING?: string;
+
     // LLM: OpenAI
     OPENAI_API_KEY: string;
     OPENAI_API_ORG_ID: string;
