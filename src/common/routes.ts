a5c89a3edd522a540ec156cf01b466a845228c2d
Enrico Ros <enrico.ros@gmail.com>
2023-10-17 04:45:45 -0700

    Routes: navigate to chat

--- src/common/routes.ts
+++ src/common/routes.ts
@@ -4,7 +4,13 @@
 // We will centralize them here, for UI and routing purposes.
 //
 
+import { NextRouter } from 'next/router';
+
+const APP_CHAT = '/';
 const APP_SHARING = '/shared/:sharedId';
 
 
 export const getSharingRelativePath = (sharedId: string) => APP_SHARING.replace(':sharedId', sharedId);
+
+export const navigateToChat = async (next: NextRouter['push'] | NextRouter['replace']) =>
+  next(APP_CHAT).then(() => null);
