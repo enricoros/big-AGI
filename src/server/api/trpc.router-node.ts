ae9f7c0571129cc05ab9b709362606e17c0d074d
Enrico Ros <enrico.ros@gmail.com>
2023-10-17 20:54:51 -0700

    Browse: page loading service, using remote Puppeteer
    also: moved to tRPC (node)

--- src/server/api/trpc.router-node.ts
+++ src/server/api/trpc.router-node.ts
@@ -1,11 +1,13 @@
 import { createTRPCRouter } from './trpc.server';
 
+import { browseRouter } from '~/modules/browse/browse.router';
 import { tradeRouter } from '~/modules/trade/server/trade.router';
 
 /**
  * Secondary rooter, and will be sitting on an NodeJS Runtime.
  */
 export const appRouterNode = createTRPCRouter({
+  browse: browseRouter,
   trade: tradeRouter,
 });
 
