dae030f1c0b755257820833c895f985c67c6f235
Enrico Ros <enrico.ros@gmail.com>
2023-11-06 13:19:23 -0800

    OpenAI: new models: improve appearance/defaults

--- src/common/util/modelUtils.ts
+++ src/common/util/modelUtils.ts
@@ -1,7 +1,7 @@
 export function prettyBaseModel(model: string | undefined): string {
   if (!model) return '';
+  if (model.includes('gpt-4-vision-preview')) return 'GPT-4 Vision';
   if (model.includes('gpt-4-1106-preview')) return 'GPT-4 Turbo';
-  if (model.includes('gpt-4-1106-vision-preview')) return 'GPT-4 Vision';
   if (model.includes('gpt-4-32k')) return 'gpt-4-32k';
   if (model.includes('gpt-4')) return 'gpt-4';
   if (model.includes('gpt-3.5-turbo-instruct')) return '3.5 Turbo Instruct';
