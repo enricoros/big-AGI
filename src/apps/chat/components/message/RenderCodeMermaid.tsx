65fc40796b7361953c59815a5565b5fb32c304ee
Enrico Ros <enrico.ros@gmail.com>
2023-11-15 03:35:43 -0800

    Mermaid: switch to CDN operation, to speed up development again
    We are loading Mermaid from the CDN (and spending all the work to dynamically load it
    and strong type it), because the Mermaid dependencies (npm i mermaid) are too heavy
    and would slow down development for everyone.
    
    Looking forward for feedback on this.

--- src/apps/chat/components/message/RenderCodeMermaid.tsx
+++ src/apps/chat/components/message/RenderCodeMermaid.tsx
@@ -1,15 +1,73 @@
 import * as React from 'react';
+import { create } from 'zustand';
 
 import { Box } from '@mui/joy';
 
 import { appTheme } from '~/common/app.theme';
+import { isBrowser } from '~/common/util/pwaUtils';
 
 
-const RenderMermaidDynamic = React.lazy(async () => {
+/**
+ * We are loading Mermaid from the CDN (and spending all the work to dynamically load it
+ * and strong type it), because the Mermaid dependencies (npm i mermaid) are too heavy
+ * and would slow down development for everyone.
+ *
+ * If you update this file, also make sure the interfaces/type definitions and initialization
+ * options are updated accordingly.
+ */
+const MERMAID_CDN_FILE: string = 'https://cdn.jsdelivr.net/npm/mermaid@10.6.1/dist/mermaid.min.js';
 
-  // dynamic import
-  const { default: mermaidAPI } = await import('mermaid');
 
+interface MermaidAPI {
+  initialize: (config: any) => void;
+  render: (id: string, text: string, svgContainingElement?: Element) => Promise<{ svg: string, bindFunctions?: (element: Element) => void }>;
+}
+
+// extend the Window interface, to allow for the mermaid API to be found
+declare global {
+  // noinspection JSUnusedGlobalSymbols
+  interface Window {
+    mermaid: MermaidAPI;
+  }
+}
+
+const useMermaidStore = create<{
+  mermaidAPI: MermaidAPI | null,
+  loadingError: string | null,
+}>()(
+  () => ({
+    mermaidAPI: null,
+    loadingError: null,
+  }),
+);
+
+let loadingStarted: boolean = false;
+let loadingError: string | null = null;
+
+
+function loadMermaidFromCDN() {
+  if (isBrowser && !loadingStarted) {
+    loadingStarted = true;
+    const script = document.createElement('script');
+    script.src = MERMAID_CDN_FILE;
+    script.defer = true;
+    script.onload = () => {
+      useMermaidStore.setState({
+        mermaidAPI: initializeMermaid(window.mermaid),
+        loadingError: null,
+      });
+    };
+    script.onerror = () => {
+      useMermaidStore.setState({
+        mermaidAPI: null,
+        loadingError: `Script load error for ${script.src}`,
+      });
+    };
+    document.head.appendChild(script);
+  }
+}
+
+function initializeMermaid(mermaidAPI: MermaidAPI): MermaidAPI {
   mermaidAPI.initialize({
     startOnLoad: false,
 
@@ -18,7 +76,6 @@ const RenderMermaidDynamic = React.lazy(async () => {
     altFontFamily: appTheme.fontFamily.body,
 
     // style configuration
-    fontSize: 8,
     htmlLabels: true,
     securityLevel: 'loose',
     theme: 'forest',
@@ -35,65 +92,71 @@ const RenderMermaidDynamic = React.lazy(async () => {
     gantt: { useMaxWidth: false },
     gitGraph: { useMaxWidth: false },
   });
+  return mermaidAPI;
+}
+
+function useMermaidLoader() {
+  const { mermaidAPI } = useMermaidStore();
+  React.useEffect(() => {
+    if (!mermaidAPI)
+      loadMermaidFromCDN();
+  }, [mermaidAPI]);
+  return { mermaidAPI, isSuccess: !!mermaidAPI, isLoading: loadingStarted, error: loadingError };
+}
 
 
-  const MermaidDiagram = React.memo((props: { mermaidCode: string }) => {
+export function RenderCodeMermaid(props: { mermaidCode: string }) {
 
-    // state
-    const isMounted = React.useRef(false);
-    const [svgCode, setSvgCode] = React.useState<string | null>(null);
-    const [error, setError] = React.useState<string | null>(null);
-    const mermaidContainerRef = React.useRef<HTMLDivElement>(null);
+  // state
+  const [svgCode, setSvgCode] = React.useState<string | null>(null);
+  const hasUnmounted = React.useRef(false);
+  const mermaidContainerRef = React.useRef<HTMLDivElement>(null);
 
-
-    // [effect] re-render on code changes
-    React.useEffect(() => {
-
-      const updateSvgCode = () =>
-        mermaidAPI
-          .render(
-            `mermaid-${Math.random().toString(36).substring(2, 9)}`,
-            props.mermaidCode,
-            mermaidContainerRef.current!,
-          )
-          .then(({ svg /*, bindFunctions*/ }) => {
-            if (mermaidContainerRef.current && isMounted.current) {
-              setSvgCode(svg);
-              // bindFunctions?.(mermaidContainerRef.current);
-            }
-          })
-          .catch((error) =>
-            console.error('Mermaid rendering failed:', error),
-          );
-
-      // mounting state and 'strict mode' debounce
-      isMounted.current = true;
-      const timeout = setTimeout(updateSvgCode, 0);
-      return () => {
-        isMounted.current = false;
-        clearTimeout(timeout);
-      };
-    }, [props.mermaidCode]);
+  // external state
+  const { mermaidAPI, error: mermaidError } = useMermaidLoader();
 
 
-    if (error)
-      return <div>Error: {error}</div>;
+  // [effect] re-render on code changes
+  React.useEffect(() => {
 
-    return (
-      <Box
-        component='div'
-        ref={mermaidContainerRef}
-        dangerouslySetInnerHTML={{ __html: svgCode || 'Loading Diagram...' }}
-      />
-    );
-  });
+    if (!mermaidAPI)
+      return;
 
-  MermaidDiagram.displayName = 'MermaidDiagram';
+    const updateSvgCode = () => {
+      const elementId = `mermaid-${Math.random().toString(36).substring(2, 9)}`;
+      mermaidAPI
+        .render(elementId, props.mermaidCode, mermaidContainerRef.current!)
+        .then(({ svg }) => {
+          if (mermaidContainerRef.current && !hasUnmounted.current) {
+            setSvgCode(svg);
+            // bindFunctions?.(mermaidContainerRef.current);
+          }
+        })
+        .catch((error) =>
+          console.warn('Mermaid code is invalid, please try again:', error),
+        );
+    };
 
-  return { default: MermaidDiagram };
-});
+    // strict-mode de-bounce, plus watch for unmounts
+    hasUnmounted.current = false;
+    const timeout = setTimeout(updateSvgCode, 0);
+    return () => {
+      hasUnmounted.current = true;
+      clearTimeout(timeout);
+    };
+  }, [mermaidAPI, props.mermaidCode]);
 
-export const RenderCodeMermaid = (props: { mermaidCode: string }) =>
-  <React.Suspense fallback={<div>Summoning Mermaids...</div>}>
-    <RenderMermaidDynamic mermaidCode={props.mermaidCode} />
-  </React.Suspense>;
+
+  // render errors when loading Mermaid. for syntax errors, the Error SVG will be rendered in-place
+  if (mermaidError)
+    return <div>Error: {mermaidError}</div>;
+
+  return (
+    <Box
+      component='div'
+      ref={mermaidContainerRef}
+      dangerouslySetInnerHTML={{ __html: svgCode || 'Loading Diagram...' }}
+    />
+  );
+
+}
