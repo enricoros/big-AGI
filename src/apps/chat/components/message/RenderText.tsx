92cb5fc1fc343d42aecfb3510f7a810226f06bf9
Enrico Ros <enrico.ros@gmail.com>
2023-11-21 00:28:59 -0800

    Moved chat/commands

--- src/apps/chat/components/message/RenderText.tsx
+++ src/apps/chat/components/message/RenderText.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Chip, Typography } from '@mui/joy';
 import { SxProps } from '@mui/joy/styles/types';
 
-import { extractCommands } from '../../commands';
+import { extractCommands } from '../../editors/commands';
 
 import { TextBlock } from './blocks';
 
