a1d6cb8cd019afb45e787ad8f887854fa978c9e5
Enrico Ros <enrico.ros@gmail.com>
2023-11-21 00:16:35 -0800

    Window management: separate stores again

---
+++ src/apps/chat/store-app-chat-panes.ts
@@ -0,0 +1,220 @@
+import * as React from 'react';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { shallow } from 'zustand/shallow';
+
+import { DConversationId, useChatStore } from '~/common/state/store-chats';
+
+
+// change this to increase/decrease the number history steps per pane
+const MAX_HISTORY_LENGTH = 20;
+
+
+export interface Pane {
+
+  conversationId: DConversationId | null;
+
+  history: DConversationId[]; // History of the conversationIds for this pane
+  historyIndex: number; // Current position in the history for this pane
+
+}
+
+interface AppChatPanesStore {
+
+  // state
+  chatPanes: Pane[];
+  chatPaneFocusIndex: number | null;
+  chatPaneInputMode: 'focused' | 'broadcast';
+
+  // actions
+  openConversationInFocusedPane: (conversationId: DConversationId) => void;
+  focusChatPane: (paneIndex: number) => void;
+  splitChatPane: (numberOfPanes: number) => void;
+  unsplitChatPane: (paneIndexToKeep: number) => void;
+  navigateHistory: (direction: 'back' | 'forward') => void;
+  onConversationsChanged: (conversationIds: DConversationId[]) => void;
+
+}
+
+const useAppChatPanesStore = create<AppChatPanesStore>()(persist(
+  (_set, _get) => ({
+
+    // Initial state: no panes
+    chatPanes: [] as Pane[],
+    chatPaneFocusIndex: null as number | null,
+    chatPaneInputMode: 'focused' as 'focused' | 'broadcast',
+
+    openConversationInFocusedPane: (conversationId: DConversationId) => {
+      _set((state) => {
+        const { chatPanes, chatPaneFocusIndex } = state;
+
+        // If there's no pane or no focused pane, create and focus a new one.
+        if (!chatPanes.length || chatPaneFocusIndex === null) {
+          const newPane = createPane(conversationId);
+          return {
+            chatPanes: [newPane],
+            chatPaneFocusIndex: 0, // Focus the new pane
+          };
+        }
+
+        // Check if the conversation is already open in the focused pane.
+        const focusedPane = chatPanes[chatPaneFocusIndex];
+        if (focusedPane.conversationId === conversationId)
+          return state;
+
+        // Update the focused pane with the new conversation.
+        const newPanes = [...chatPanes];
+        const newHistory = [...focusedPane.history, conversationId].slice(-MAX_HISTORY_LENGTH);
+        newPanes[chatPaneFocusIndex] = {
+          ...focusedPane,
+          conversationId,
+          history: newHistory,
+          historyIndex: newHistory.length - 1,
+        };
+
+        // Return the updated state.
+        return {
+          chatPanes: newPanes,
+        };
+      });
+    },
+
+    focusChatPane: (paneIndex: number) =>
+      _set({
+        chatPaneFocusIndex: paneIndex,
+      }),
+
+    splitChatPane: (numberOfPanes: number) => {
+      const { chatPanes, chatPaneFocusIndex } = _get();
+      const focusedPane = (chatPaneFocusIndex !== null ? chatPanes[chatPaneFocusIndex] : null) ?? createPane();
+
+      _set({
+        chatPanes: Array.from({ length: numberOfPanes }, () => ({ ...focusedPane })),
+        chatPaneFocusIndex: 0,
+      });
+    },
+
+    unsplitChatPane: (paneIndexToKeep: number) =>
+      _set(state => ({
+        chatPanes: [state.chatPanes[paneIndexToKeep] || createPane()],
+        chatPaneFocusIndex: 0,
+      })),
+
+    navigateHistory: (direction: 'back' | 'forward') =>
+      _set(state => {
+        const { chatPanes, chatPaneFocusIndex } = state;
+        if (chatPaneFocusIndex === null)
+          return state;
+
+        const focusedPane = chatPanes[chatPaneFocusIndex];
+        let newHistoryIndex = focusedPane.historyIndex;
+
+        if (direction === 'back' && newHistoryIndex > 0)
+          newHistoryIndex--;
+        else if (direction === 'forward' && newHistoryIndex < focusedPane.history.length - 1)
+          newHistoryIndex++;
+
+        const newPanes = [...chatPanes];
+        newPanes[chatPaneFocusIndex] = {
+          ...focusedPane,
+          conversationId: focusedPane.history[newHistoryIndex],
+          historyIndex: newHistoryIndex,
+        };
+
+        return {
+          chatPanes: newPanes,
+        };
+      }),
+
+    onConversationsChanged: (conversationIds: DConversationId[]) =>
+      _set(state => {
+        const { chatPanes, chatPaneFocusIndex } = state;
+
+        // handle panes
+        let untouched = true;
+        const newPanes: Pane[] = chatPanes.map(chatPane => {
+          const { conversationId, history, historyIndex } = chatPane;
+
+          // adjust history if any is deleted
+          let newHistoryIndex = historyIndex;
+          const newHistory = history.filter((_hId, index) => {
+            const historyStillPresent = conversationIds.includes(_hId);
+            if (!historyStillPresent && index <= historyIndex)
+              newHistoryIndex--;
+            return historyStillPresent;
+          });
+          if (newHistoryIndex < 0 && newHistory.length > 0)
+            newHistoryIndex = 0;
+
+          // check if pointing to a valid conversationId
+          const needsNewConversationId = !conversationId || !conversationIds.includes(conversationId);
+          if (!needsNewConversationId && newHistory.length === history.length)
+            return chatPane;
+
+          const nextConversationId = newHistoryIndex >= 0 && newHistoryIndex < newHistory.length
+            ? newHistory[newHistoryIndex]
+            : newHistory.length > 0
+              ? newHistory[newHistory.length - 1]
+              : conversationIds[0] ?? null;
+
+          untouched = false;
+          return {
+            ...chatPane,
+            conversationId: nextConversationId,
+            history: newHistory,
+            historyIndex: newHistoryIndex,
+          };
+        }).filter(pane => !!pane.conversationId);
+
+        // if untouched, return state as-is
+        if (untouched)
+          return state;
+
+        // play it safe, and make sure a pane exists, and is focused
+        return {
+          chatPanes: newPanes.length ? newPanes : [createPane(conversationIds[0] ?? null)],
+          chatPaneFocusIndex: (newPanes.length && chatPaneFocusIndex !== null && chatPaneFocusIndex < newPanes.length) ? state.chatPaneFocusIndex : 0,
+        };
+      }),
+
+  }), {
+    name: 'app-app-chat-panes',
+  },
+));
+
+function createPane(conversationId: DConversationId | null = null): Pane {
+  return {
+    conversationId,
+    history: conversationId ? [conversationId] : [],
+    historyIndex: conversationId ? 0 : -1,
+  };
+}
+
+export function usePanesManager() {
+  // use Panes
+  const { focusedChatPane, openConversationInFocusedPane, onConversationsChanged } = useAppChatPanesStore(state => {
+    const { chatPaneFocusIndex, chatPanes, openConversationInFocusedPane, onConversationsChanged } = state;
+    const focusedChatPane = chatPaneFocusIndex !== null ? chatPanes[chatPaneFocusIndex] ?? null : null;
+    return {
+      chatPanes: chatPanes as Readonly<Pane[]>,
+      focusedChatPane,
+      openConversationInFocusedPane,
+      onConversationsChanged,
+    };
+  }, shallow);
+
+  // use Conversation IDs[]
+  const conversationIDs: DConversationId[] = useChatStore(state => {
+    return state.conversations.map(_c => _c.id);
+  }, shallow);
+
+  // [Effect] Ensure all Panes have a valid Conversation ID
+  React.useEffect(() => {
+    onConversationsChanged(conversationIDs);
+  }, [conversationIDs, onConversationsChanged]);
+
+  return {
+    focusedChatPane,
+    openConversationInFocusedPane,
+  };
+}
--- src/apps/chat/store-app-chat.ts
+++ src/apps/chat/store-app-chat.ts
@@ -2,36 +2,13 @@ import { create } from 'zustand';
 import { shallow } from 'zustand/shallow';
 import { persist } from 'zustand/middleware';
 
-import { DConversationId, useChatStore } from '~/common/state/store-chats';
-
 
 export type ChatAutoSpeakType = 'off' | 'firstLine' | 'all';
 
 
-// Window Management
-
-interface WindowPane {
-  conversationId: DConversationId | null;
-  conversationHistory: DConversationId[]; // History of the conversationIds for this pane
-  conversationHistoryIndex: number; // Current position in the history for this pane
-}
-
-interface WindowManagerSlice {
-  windowPanes: WindowPane[];
-  windowPaneFocusIndex: number | null;
-  windowPanesInputMode: 'focused' | 'broadcast';
-
-  openChatInFocusedPane: (conversationId: DConversationId) => void;
-  navigateHistory: (direction: 'back' | 'forward') => void;
-  focusPane: (paneIndex: number) => void;
-  splitWindow: (numberOfPanes: number) => void;
-  unsplitWindow: (paneIndexToKeep: number) => void;
-}
-
-
 // Chat Settings (Chat AI & Chat UI)
 
-interface ChatSettingsSlice {
+interface AppChatStore {
 
   autoSpeak: ChatAutoSpeakType;
   setAutoSpeak: (autoSpeak: ChatAutoSpeakType) => void;
@@ -54,100 +31,8 @@ interface ChatSettingsSlice {
 }
 
 
-type AppChatStore = WindowManagerSlice & ChatSettingsSlice;
-
 const useAppChatStore = create<AppChatStore>()(persist(
   (_set, _get) => ({
-
-    // Window Management
-
-    windowPanes: [],
-    windowPaneFocusIndex: null,
-    windowPanesInputMode: 'focused',
-
-    openChatInFocusedPane: (conversationId: DConversationId) => {
-      _set((state) => {
-        let { windowPanes, windowPaneFocusIndex } = state;
-
-        // If there's no pane or no focused pane, create and focus a new one.
-        if (windowPanes.length === 0 || windowPaneFocusIndex === null) {
-          const newPane = createPane(conversationId);
-          return {
-            windowPanes: [newPane],
-            windowPaneFocusIndex: 0, // Focus the new pane
-          };
-        }
-
-        // Check if the conversation is already open in the focused pane.
-        const focusedPane = windowPanes[windowPaneFocusIndex];
-        if (focusedPane.conversationId === conversationId)
-          return {};
-
-        // Update the focused pane with the new conversation.
-        const newHistory = [...focusedPane.conversationHistory, conversationId];
-        windowPanes[windowPaneFocusIndex] = {
-          ...focusedPane,
-          conversationId,
-          conversationHistory: newHistory,
-          conversationHistoryIndex: newHistory.length - 1,
-        };
-
-        // Return the updated state.
-        return {
-          windowPanes, // No need to create a new array since we've modified the existing one
-        };
-      });
-    },
-
-    navigateHistory: (direction: 'back' | 'forward') =>
-      _set(state => {
-        const { windowPanes, windowPaneFocusIndex } = state;
-        if (windowPaneFocusIndex === null)
-          return state;
-
-        const focusedPane = windowPanes[windowPaneFocusIndex];
-        let newHistoryIndex = focusedPane.conversationHistoryIndex;
-
-        if (direction === 'back' && newHistoryIndex > 0)
-          newHistoryIndex--;
-        else if (direction === 'forward' && newHistoryIndex < focusedPane.conversationHistory.length - 1)
-          newHistoryIndex++;
-
-        const newPanes = [...windowPanes];
-        newPanes[windowPaneFocusIndex] = {
-          ...focusedPane,
-          conversationId: focusedPane.conversationHistory[newHistoryIndex],
-          conversationHistoryIndex: newHistoryIndex,
-        };
-
-        return {
-          windowPanes: newPanes,
-        };
-      }),
-
-    focusPane: (paneIndex: number) =>
-      _set({
-        windowPaneFocusIndex: paneIndex,
-      }),
-
-    splitWindow: (numberOfPanes: number) => {
-      const { windowPanes, windowPaneFocusIndex } = _get();
-      const focusedPane = windowPaneFocusIndex !== null ? windowPanes[windowPaneFocusIndex] : createPane();
-
-      _set({
-        windowPanes: Array.from({ length: numberOfPanes }, () => ({ ...focusedPane })),
-        windowPaneFocusIndex: 0,
-      });
-    },
-
-    unsplitWindow: (paneIndexToKeep: number) =>
-      _set(state => ({
-        windowPanes: [state.windowPanes[paneIndexToKeep] || createPane()],
-        windowPaneFocusIndex: 0,
-      })),
-
-
-    // Settings: Chat AI & Chat UI
 
     autoSpeak: 'off',
     setAutoSpeak: (autoSpeak: ChatAutoSpeakType) => _set({ autoSpeak }),
@@ -188,57 +73,6 @@ const useAppChatStore = create<AppChatStore>()(persist(
 ));
 
 
-// Window Manager
-
-function createPane(conversationId: DConversationId | null = null): WindowPane {
-  return {
-    conversationId,
-    conversationHistory: conversationId ? [conversationId] : [],
-    conversationHistoryIndex: conversationId ? 0 : -1,
-  };
-}
-
-// function initFirstPaneFromChats(): Pick<WindowManagerSlice, 'windowPanes' | 'windowPaneFocusIndex'> {
-//   const _ext_conversations = useChatStore.getState().conversations;
-//   const firstConversationId = _ext_conversations.length ? _ext_conversations[0].id : null;
-//   return {
-//     windowPanes: firstConversationId ? [createPane(firstConversationId)] : [],
-//     windowPaneFocusIndex: firstConversationId ? 0 : null,
-//   };
-// }
-
-// if any conversation is deleted from the store, we need to update the window manager
-/*useChatStore.subscribe(
-  ({ conversations }, { conversations: prevConversations }) => {
-    // TODO: autoload the most recent chat?
-
-    // TOOD: monitor deletes to cleanup state across panes
-
-    // if a conversation was deleted, remove it from the window manager
-    // if (conversations.length != prevConversations.length)
-    //   console.log('TEST', conversations.length, prevConversations.length);
-  },
-);*/
-
-
-export const useChatWindowManager = () => useAppChatStore(state => {
-  const { windowPaneFocusIndex, windowPanes } = state;
-  const currentPane = windowPaneFocusIndex !== null ? windowPanes[windowPaneFocusIndex] : null;
-  // console.log('useChatWindowManager', windowPaneFocusIndex, windowPanes, currentPane);
-  return {
-    currentPane,
-    windowPanes: windowPanes as Readonly<WindowPane[]>,
-    openChatInFocusedPane: state.openChatInFocusedPane,
-    navigateHistory: state.navigateHistory,
-    splitWindow: state.splitWindow,
-    unsplitWindow: state.unsplitWindow,
-    focusPane: state.focusPane,
-  };
-}, shallow);
-
-
-// Chat State
-
 export const useChatAutoAI = () => useAppChatStore(state => ({
   autoSpeak: state.autoSpeak,
   autoSuggestDiagrams: state.autoSuggestDiagrams,
@@ -260,7 +94,8 @@ export const getChatAutoAI = (): {
 export const useChatShowTextDiff = (): [boolean, (showDiff: boolean) => void] =>
   useAppChatStore(state => [state.showTextDiff, state.setShowTextDiff], shallow);
 
-export const getChatShowSystemMessages = (): boolean => useAppChatStore.getState().showSystemMessages;
+export const getChatShowSystemMessages = (): boolean =>
+  useAppChatStore.getState().showSystemMessages;
 
 export const useChatShowSystemMessages = (): [boolean, (showSystemMessages: boolean) => void] =>
   useAppChatStore(state => [state.showSystemMessages, state.setShowSystemMessages], shallow);
