07f713fa57d102e80106fe0c1fe30a866b5b9ac8
Enrico Ros <enrico.ros@gmail.com>
2023-12-08 04:13:44 -0800

    Browser: initial screenshot support

--- src/modules/browse/browse.client.ts
+++ src/modules/browse/browse.client.ts
@@ -3,6 +3,10 @@ import { useBrowseStore } from '~/modules/browse/store-module-browsing';
 import { apiAsyncNode } from '~/common/util/trpc.client';
 
 
+// show the screenshot in the dom
+const DEBUG_SHOW_SCREENSHOT = false;
+
+
 export const CmdRunBrowse: string[] = ['/browse'];
 
 
@@ -26,12 +30,28 @@ export async function callBrowseFetchPage(url: string) {
       ...(!!clientWssEndpoint && { wssEndpoint: clientWssEndpoint }),
     },
     subjects: [{ url }],
+    screenshot: DEBUG_SHOW_SCREENSHOT ? {
+      width: 512,
+      height: 512,
+      // quality: 100,
+    } : undefined,
   });
 
   if (pages.length !== 1)
     throw new Error(`Browsing error: expected 1 result, got ${pages.length}`);
 
   const page = pages[0];
+
+  // DEBUG: if there's a screenshot, append it to the dom
+  if (DEBUG_SHOW_SCREENSHOT && page.screenshot) {
+    const img = document.createElement('img');
+    img.src = page.screenshot.imageDataUrl;
+    img.style.width = `${page.screenshot.width}px`;
+    img.style.height = `${page.screenshot.height}px`;
+    document.body.appendChild(img);
+  }
+
+  // throw if there's an error
   if (page.error) {
     console.warn('Browsing service error:', page.error);
     if (!page.content)
