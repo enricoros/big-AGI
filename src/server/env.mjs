a265112ce118e0e30665db70982e713f13864db0
Enrico Ros <enrico.ros@gmail.com>
2023-12-12 16:34:18 -0800

    Mistral Platform: backend-configurable support (#273)

--- src/server/env.mjs
+++ src/server/env.mjs
@@ -21,6 +21,9 @@ export const env = createEnv({
     ANTHROPIC_API_KEY: z.string().optional(),
     ANTHROPIC_API_HOST: z.string().url().optional(),
 
+    // LLM: Mistral
+    MISTRAL_API_KEY: z.string().optional(),
+
     // LLM: Ollama
     OLLAMA_API_HOST: z.string().url().optional(),
 
