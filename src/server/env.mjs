add81fedc40083bd34f750436e3a86148e28a534
Enrico Ros <enrico.ros@gmail.com>
2023-12-18 15:16:02 -0800

    DB: drop URL validation for POSTGRES_PRISMA_URL. #277

--- src/server/env.mjs
+++ src/server/env.mjs
@@ -5,8 +5,8 @@ export const env = createEnv({
   server: {
 
     // Backend Postgres, for optional storage via Prisma
-    POSTGRES_PRISMA_URL: z.string().url().optional(),
-    POSTGRES_URL_NON_POOLING: z.string().url().optional(),
+    POSTGRES_PRISMA_URL: z.string().optional(),
+    POSTGRES_URL_NON_POOLING: z.string().optional(),
 
     // LLM: OpenAI
     OPENAI_API_KEY: z.string().optional(),
