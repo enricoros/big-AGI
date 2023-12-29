bf4ad0e9264272e16831cf33bf86758c0389daf5
Enrico Ros <enrico.ros@gmail.com>
2023-12-29 01:38:32 -0800

    Commands: much improve the parser

--- src/apps/chat/editors/commands.ts
+++ src/apps/chat/editors/commands.ts
@@ -10,7 +10,20 @@ export const CmdAddRoleMessage: string[] = ['/assistant', '/a', '/system', '/s']
 
 export const CmdHelp: string[] = ['/help', '/h', '/?'];
 
-export const commands = [...CmdRunBrowse, ...CmdRunT2I, ...CmdRunReact, ...CmdRunSearch, ...CmdAddRoleMessage, ...CmdHelp];
+const AllNonHelpCommands = [...CmdRunBrowse, ...CmdRunT2I, ...CmdRunReact, ...CmdRunSearch, ...CmdAddRoleMessage];
+const HelpCommands = [...CmdHelp];
+
+
+function buildCommandRegexPattern(commands: string[]): RegExp {
+  // Escape regex special characters in commands
+  const escapedCommands = commands.map(cmd => cmd.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&'));
+  // Join the escaped commands into a single regex pattern
+  const pattern = `(${escapedCommands.join('|')})(?=\\s|$)`;
+  return new RegExp(pattern, 'i');
+}
+
+const allCommandsRegex = buildCommandRegexPattern(AllNonHelpCommands);
+
 
 export interface SentencePiece {
   type: 'text' | 'cmd';
@@ -22,35 +35,33 @@ export interface SentencePiece {
  * Used by rendering functions, as well as input processing functions.
  */
 export function extractCommands(input: string): SentencePiece[] {
-  // 'help' commands are the only without a space and text after
-  if (CmdHelp.includes(input))
-    return [{ type: 'cmd', value: input }, { type: 'text', value: '' }];
-  const regexFromTags = commands.map(tag => `^\\${tag} `).join('\\b|') + '\\b';
-  const pattern = new RegExp(regexFromTags, 'g');
-  const result: SentencePiece[] = [];
-  let lastIndex = 0;
-  let match: RegExpExecArray | null;
 
-  while ((match = pattern.exec(input)) !== null) {
-    if (match.index !== lastIndex)
-      result.push({ type: 'text', value: input.substring(lastIndex, match.index) });
-    result.push({ type: 'cmd', value: match[0].trim() });
-    lastIndex = pattern.lastIndex;
+  // Check for help commands first
+  const inputTrimmed = input.trim();
+  if (HelpCommands.includes(inputTrimmed))
+    return [{ type: 'cmd', value: inputTrimmed }, { type: 'text', value: '' }];
 
-    // Remove the space after the matched tag
-    if (input[lastIndex] === ' ')
-      lastIndex++;
+  // Find the first command match in the input
+  const match = allCommandsRegex.exec(input);
+  if (match) {
+    const beforeCommand = input.substring(0, match.index).trim();
+    const afterCommand = input.substring(match.index + match[0].length).trim();
+
+    if (!beforeCommand && afterCommand) {
+      return [
+        { type: 'cmd', value: match[0].trim() },
+        { type: 'text', value: afterCommand },
+      ];
+    }
   }
 
-  if (lastIndex !== input.length)
-    result.push({ type: 'text', value: input.substring(lastIndex) });
-
-  return result;
+  // If no command is found, return the entire input as text
+  return [{ type: 'text', value: input }];
 }
 
 export function createCommandsHelpMessage(): DMessage {
   let text = 'Available Chat Commands:\n';
-  text += commands.map(c => ` - ${c}`).join('\n');
+  text += AllNonHelpCommands.map(c => ` - ${c}`).join('\n');
   const helpMessage = createDMessage('assistant', text);
   helpMessage.originLLM = Brand.Title.Base;
   return helpMessage;
