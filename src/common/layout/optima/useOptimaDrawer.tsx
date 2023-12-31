26f472b3968e7f1198e808d48c7a0c7ab89fc8e2
Enrico Ros <enrico.ros@gmail.com>
2023-12-31 03:46:34 -0800

    Try to use a second React Context for the Optima drawer, to optimize state changes

---
+++ src/common/layout/optima/useOptimaDrawer.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+
+
+interface OptimaDrawerState {
+  isDrawerOpen: boolean;
+}
+
+interface OptimaDrawerActions {
+  toggleDrawer: () => void;
+  openDrawer: () => void;
+  closeDrawer: () => void;
+}
+
+
+const UseOptimaDrawer = React.createContext<(OptimaDrawerState & OptimaDrawerActions) | undefined>(undefined);
+
+// TRICK: this is how we persist the drawer state across page navigations
+let lastOpenState = true;
+
+export function OptimaDrawerProvider(props: { children: React.ReactNode }) {
+
+  // state
+  const [drawerOpen, setDrawerOpen] = React.useState(lastOpenState);
+
+  // actions
+  const actions: OptimaDrawerActions = React.useMemo(() => ({
+
+    toggleDrawer: () => setDrawerOpen(state => lastOpenState = !state),
+    openDrawer: () => setDrawerOpen(lastOpenState = true),
+    closeDrawer: () => setDrawerOpen(lastOpenState = false),
+
+  }), []);
+
+  return (
+    <UseOptimaDrawer.Provider value={{ isDrawerOpen: drawerOpen, ...actions }}>
+      {props.children}
+    </UseOptimaDrawer.Provider>
+  );
+}
+
+
+/**
+ * Optima Drawer access for getting state and actions
+ */
+export const useOptimaDrawer = () => {
+  const context = React.useContext(UseOptimaDrawer);
+  if (!context)
+    throw new Error('useOptimaDrawer must be used within an OptimaDrawerProvider');
+  return context;
+};
