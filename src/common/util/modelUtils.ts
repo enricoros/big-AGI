245815b8144550c8a7351bf55e95f5b8838a02ca
Enrico Ros <enrico.ros@gmail.com>
2024-01-04 03:36:38 -0800

    LM Studio: improve model name

--- src/common/util/modelUtils.ts
+++ src/common/util/modelUtils.ts
@@ -1,3 +1,9 @@
+function getModelFromFile(filePath: string): string {
+  const normalizedPath = filePath.replace(/\\/g, '/');
+  return normalizedPath.split('/').pop() || '';
+}
+
+
 export function prettyBaseModel(model: string | undefined): string {
   if (!model) return '';
   if (model.includes('gpt-4-vision-preview')) return 'GPT-4 Vision';
@@ -9,6 +15,9 @@ export function prettyBaseModel(model: string | undefined): string {
   if (model.includes('gpt-3.5-turbo-16k')) return '3.5 Turbo 16k';
   if (model.includes('gpt-3.5-turbo')) return '3.5 Turbo';
   if (model.endsWith('.bin')) return model.slice(0, -4);
+  // [LM Studio]
+  if (model.startsWith('C:\\') || model.startsWith('D:\\'))
+    return getModelFromFile(model).replace('.gguf', '');
   // [Ollama]
   if (model.includes(':'))
     return model.replace(':latest', '').replaceAll(':', ' ');
