1908b2f670037be0ac8db84b4c8540b4735c19d1
Enrico Ros <enrico.ros@gmail.com>
2024-01-18 16:33:26 -0800

    Style the Desktop Nav Panel

--- src/common/layout/optima/components/BringTheLove.tsx
+++ src/common/layout/optima/components/BringTheLove.tsx
@@ -1,42 +1,42 @@
 import * as React from 'react';
 
 import type { SxProps } from '@mui/joy/styles/types';
-import { Button, IconButton, Tooltip } from '@mui/joy';
+import { Button, Tooltip } from '@mui/joy';
 
 import { Link } from '~/common/components/Link';
 import { cssRainbowColorKeyframes } from '~/common/app.theme';
+
+import { DesktopNavIcon, navItemClasses } from './DesktopNavIcon';
 
 
 export function BringTheLove(props: { text: string, link: string, asIcon?: boolean, icon: React.FC, sx?: SxProps }) {
   // state
   const [loved, setLoved] = React.useState(false);
-  const icon = loved ? '❤️' : <props.icon /> ?? null; // '❤️' : '🤍';
 
-  // reset loved after 5 seconds
+  // reset loved after 6.9 seconds
   React.useEffect(() => {
     if (loved) {
-      const timer = setTimeout(() => setLoved(false), 5000);
+      const timer = setTimeout(() => setLoved(false), 6900);
       return () => clearTimeout(timer);
     }
   }, [loved]);
 
+  const icon = loved ? '❤️' : <props.icon /> ?? null; // '❤️' : '🤍';
+
   return (
     <Tooltip followCursor title={props.text}>
       {props.asIcon ? (
-        <IconButton
-          variant='solid'
-          size='sm'
-          onClick={() => setLoved(true)}
-          component={Link} href={props.link} target='_blank' noLinkStyle
-          sx={{
-            '&:hover': { animation: `${cssRainbowColorKeyframes} 5s linear infinite` },
-            background: 'transparent',
+        <DesktopNavIcon className={navItemClasses.typeLinkOrModal} onClick={() => setLoved(true)} sx={{
+          opacity: loved ? 1 : 0.4,
+          '&:hover': { opacity: 1 },
+        }}>
+          <Link href={props.link} target='_blank' sx={{
             textDecoration: 'none',
-            ...props.sx,
-          }}
-        >
-          {icon}
-        </IconButton>
+            '&:hover': { textDecoration: 'none' },
+          }}>
+            {icon}
+          </Link>
+        </DesktopNavIcon>
       ) : (
         <Button
           onClick={() => setLoved(true)}
