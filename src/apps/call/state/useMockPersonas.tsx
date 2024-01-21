ed2284716b24b3b28b51a169fedb90de3f3b003c
Enrico Ros <enrico.ros@gmail.com>
2024-01-20 19:09:21 -0800

    Call: hide hidden personas

--- src/apps/call/state/useMockPersonas.tsx
+++ src/apps/call/state/useMockPersonas.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import { usePurposeStore } from '../../chat/components/persona-selector/store-purposes';
+
 import { SystemPurposeData, SystemPurposeId, SystemPurposes } from '../../../data';
 
 
@@ -12,13 +14,18 @@ export interface MockPersona extends SystemPurposeData {
 }
 
 export function useMockPersonas(): { personas: MockPersona[], personaIDs: SystemPurposeId[] } {
+  // only react to hiddenPurposeIDs changes
+  const hiddenPurposeIDs = usePurposeStore(state => state.hiddenPurposeIDs);
+
   // Dependency array is empty because SystemPurposes is constant
   return React.useMemo(() => {
     const personaIDs = Object.keys(SystemPurposes) as SystemPurposeId[];
-    const personas = personaIDs.map((key) => ({
-      ...SystemPurposes[key as SystemPurposeId],
-      personaId: key as SystemPurposeId,
-    }));
+    const personas = personaIDs
+      .filter((key) => !hiddenPurposeIDs.includes(key))
+      .map((key) => ({
+        ...SystemPurposes[key as SystemPurposeId],
+        personaId: key as SystemPurposeId,
+      }));
     return { personas, personaIDs };
   }, []);
 }
