d4246d305e579f8e1eb25ccd0773711774c95cb3
Enrico Ros <enrico.ros@gmail.com>
2024-01-29 17:21:25 -0800

    Draw: Commit to v1.13

--- src/common/app.nav.ts
+++ src/common/app.nav.ts
@@ -31,7 +31,6 @@ import SettingsIcon from '@mui/icons-material/Settings';
 
 import { Brand } from '~/common/app.config';
 import { hasNoChatLinkItems } from '~/modules/trade/link/store-link';
-import { useUXLabsStore } from '~/common/state/store-ux-labs';
 
 
 // enable to show all items, for layout development
@@ -115,7 +114,6 @@ export const navItems: {
       route: '/draw',
       // hideOnMobile: true,
       hideDrawer: true,
-      hideIcon: () => !useUXLabsStore.getState().labsDrawing,
     },
     {
       name: 'Cortex',
