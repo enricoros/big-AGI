843a8dcd698b82cae29b193c8b3bbd7dbff4c80b
Enrico Ros <enrico.ros@gmail.com>
2024-02-03 00:33:52 -0800

    HTML5Video ops: use async/await

--- src/common/util/videoUtils.ts
+++ src/common/util/videoUtils.ts
@@ -5,7 +5,7 @@
  */
 
 export function downloadVideoFrameAsPNG(videoElement: HTMLVideoElement, prefixName: string) {
-  // video to canvas to png
+  // current video frame -> canvas -> dataURL PNG
   const renderedFrame = _renderVideoFrameToCanvas(videoElement);
   const imageDataURL = renderedFrame.toDataURL('image/png');
 
@@ -13,20 +13,20 @@ export function downloadVideoFrameAsPNG(videoElement: HTMLVideoElement, prefixNa
   const link = document.createElement('a');
   link.download = _prettyFileName(prefixName, renderedFrame);
   link.href = imageDataURL;
+  document.body.appendChild(link); // Ensure visibility in the DOM for Firefox
   link.click();
+  document.body.removeChild(link); // Clean up
 }
 
-export function renderVideoFrameToFile(videoElement: HTMLVideoElement, prefixName: string, callback: (file: File) => void) {
-  // video to canvas
+export async function renderVideoFrameAsPNGFile(videoElement: HTMLVideoElement, prefixName: string): Promise<File> {
+  // current video frame -> canvas -> Blob PNG
   const renderedFrame = _renderVideoFrameToCanvas(videoElement);
+  const blob = await _canvasToBlob(renderedFrame, 'image/png');
 
-  // canvas to blob to file to callback
-  renderedFrame.toBlob((blob) => {
-    if (blob) {
-      const file = new File([blob], _prettyFileName(prefixName, renderedFrame), { type: blob.type });
-      callback(file);
-    }
-  }, 'image/png');
+  // to File
+  if (!blob)
+    throw new Error('Failed to convert canvas to Blob');
+  return new File([blob], _prettyFileName(prefixName, renderedFrame), { type: blob.type });
 }
 
 function _prettyFileName(prefixName: string, renderedFrame: HTMLCanvasElement) {
@@ -45,3 +45,11 @@ function _renderVideoFrameToCanvas(videoElement: HTMLVideoElement): HTMLCanvasEl
   return canvas;
 }
 
+/**
+ * Creates a Blob object representing the image contained in the canvas
+ * @param canvas The canvas element to convert to a Blob.
+ * @param imageFormat Browsers are required to support image/png; many will support additional formats including image/jpeg and image/webp.
+ */
+async function _canvasToBlob(canvas: HTMLCanvasElement, imageFormat: string = 'image/png'): Promise<Blob | null> {
+  return new Promise((resolve) => canvas.toBlob(resolve, imageFormat));
+}
