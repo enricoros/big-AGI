b8b1dd2cfb7fa5d9473cebf76e61603977818316
Enrico Ros <enrico.ros@gmail.com>
2024-02-10 20:06:35 -0800

    Optimize

--- src/apps/chat/components/scroll-to-bottom/ScrollToBottomButton.tsx
+++ src/apps/chat/components/scroll-to-bottom/ScrollToBottomButton.tsx
@@ -1,9 +1,32 @@
 import * as React from 'react';
 
+import type { SxProps } from '@mui/joy/styles/types';
 import { IconButton } from '@mui/joy';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 import { useScrollToBottom } from './useScrollToBottom';
+
+// const object
+const buttonSx: SxProps = {
+  // place this on the bottom-right corner (FAB-like)
+  position: 'absolute',
+  bottom: '2rem',
+  right: {
+    xs: '1rem',
+    md: '2rem',
+  },
+
+  // style it
+  backgroundColor: 'background.surface',
+  borderRadius: '50%',
+  boxShadow: 'md',
+
+  // fade it in when hovering
+  // transition: 'all 0.15s',
+  // '&:hover': {
+  //   transform: 'scale(1.1)',
+  // },
+} as const;
 
 
 export function ScrollToBottomButton() {
@@ -20,37 +43,8 @@ export function ScrollToBottomButton() {
     return null;
 
   return (
-    // <Tooltip title={
-    //   <Typography variant='solid' level='title-sm' sx={{ px: 1 }}>
-    //     Scroll to bottom
-    //   </Typography>
-    // }>
-    <IconButton
-      variant='outlined'
-      onClick={handleStickToBottom}
-      sx={{
-        // place this on the bottom-right corner (FAB-like)
-        position: 'absolute',
-        bottom: '2rem',
-        right: {
-          xs: '1rem',
-          md: '2rem',
-        },
-
-        // style it
-        backgroundColor: 'background.surface',
-        borderRadius: '50%',
-        boxShadow: 'md',
-
-        // fade it in when hovering
-        // transition: 'all 0.15s',
-        // '&:hover': {
-        //   transform: 'scale(1.1)',
-        // },
-      }}
-    >
+    <IconButton aria-label='Scroll To Bottom' variant='outlined' onClick={handleStickToBottom} sx={buttonSx}>
       <KeyboardDoubleArrowDownIcon />
     </IconButton>
-    // </Tooltip>
   );
 }
