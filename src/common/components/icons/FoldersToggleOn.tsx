5729cfe2ca090cf1a12883748cf5c16554f09c2c
Enrico Ros <enrico.ros@gmail.com>
2024-02-10 23:44:04 -0800

    Improve Icon

--- src/common/components/icons/FoldersToggleOn.tsx
+++ src/common/components/icons/FoldersToggleOn.tsx
@@ -7,6 +7,6 @@ import { SvgIcon } from '@mui/joy';
 export function FoldersToggleOn(props: { sx?: SxProps }) {
   return <SvgIcon viewBox='0 0 24 24' width='24' height='24' stroke='none' fill='currentColor' {...props}>
     <path d='m9.17 6 2 2H20v10H4V6zM10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8z' />
-    <path d='M 16,16 12,11 8,16 Z' />
+    <path d='M 16,15 12,10 8,15 Z' />
   </SvgIcon>;
 }
