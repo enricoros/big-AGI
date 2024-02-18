b2db6f98209671c4bd122ee17f399428245caf3d
Enrico Ros <enrico.ros@gmail.com>
2024-02-17 19:44:36 -0800

    Relocate Prisma to src/server/prisma

--- src/server/prisma/prismaDb.ts
+++ src/server/prisma/prismaDb.ts
@@ -5,10 +5,10 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
+export const prismaDb =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prismaDb;
