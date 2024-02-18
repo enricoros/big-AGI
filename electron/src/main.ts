36f9812342cf0a62192dc469413b34e0b7d36b0a
Enrico Ros <enrico.ros@gmail.com>
2024-02-18 02:20:04 -0800

    Electron: skeleton

---
+++ electron/src/main.ts
@@ -0,0 +1,74 @@
+import electronServe from 'electron-serve';
+import { app, BrowserWindow } from 'electron';
+import { join as pathJoin } from 'path';
+
+// Simulate app.isPackaged if FORCE_PACKAGED environment variable is set
+const isPackaged = true; // process.env.FORCE_PACKAGED ? true : app.isPackaged;
+
+
+let mainWindow: BrowserWindow | null = null;
+
+const appServe = isPackaged ? electronServe({
+  directory: pathJoin(__dirname, '../../dist'),
+}) : null;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 960,
+    frame: false,
+    // backgroundColor: '#ff0000',
+    // transparent: false,
+    // thickFrame: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: pathJoin(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.setMenu(null);
+  // win.setIcon(null);
+
+  if (isPackaged) {
+    appServe(mainWindow).then(() => {
+      mainWindow.webContents.openDevTools();
+      void mainWindow.loadURL('app://-');
+    });
+  } else {
+    void mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.on('did-fail-load', (e, code, desc) => {
+      mainWindow.webContents.reloadIgnoringCache();
+    });
+  }
+};
+
+
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance. We should focus our window.
+    if (mainWindow) {
+      mainWindow.isMinimized() && mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+
+  app.on('ready', createWindow);
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
