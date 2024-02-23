fredliubojin/nextjs-chatgpt-app

Enable custom PlantUML server, Fixes #415

src/modules/blocks/code/RenderCode.tsx
@@ -26,9 +26,15 @@ async function fetchPlantUmlSvg(plantUmlCode: string): Promise<string | null> {
     // Dynamically import the PlantUML encoder - it's a large library that slows down app loading
     const { encode: plantUmlEncode } = await import('plantuml-encoder');
 
+    // Get the PlantUML server from inline env var
+    var plantUmlServerUrl = process.env.NEXT_PUBLIC_PLANTUML_SERVER_URL;
+    if (!plantUmlServerUrl) {
+      plantUmlServerUrl = 'https://www.plantuml.com/plantuml/svg/';
+    }
+
     // retrieve and manually adapt the SVG, to remove the background
     const encodedPlantUML: string = plantUmlEncode(plantUmlCode);
-    const response = await fetch(`https://www.plantuml.com/plantuml/svg/${encodedPlantUML}`);
+    const response = await fetch(`${plantUmlServerUrl}${encodedPlantUML}`);
     text = await response.text();
   } catch (e) {
     return null;

enricoros/big-AGI

(cherry picked from commit 49392acfd6ab55cc4ba8a951272e921b7e8ff64c, fredliubojin main)

src/modules/blocks/code/RenderCode.tsx
@@ -27,9 +27,15 @@ async function fetchPlantUmlSvg(plantUmlCode: string): Promise<string | null> {
     // Dynamically import the PlantUML encoder - it's a large library that slows down app loading
     const { encode: plantUmlEncode } = await import('plantuml-encoder');
 
+    // Get the PlantUML server from inline env var
+    let plantUmlServerUrl = process.env.NEXT_PUBLIC_PLANTUML_SERVER_URL;
+    if (!plantUmlServerUrl) {
+      plantUmlServerUrl = 'https://www.plantuml.com/plantuml/svg/';
+    }
+
     // retrieve and manually adapt the SVG, to remove the background
     const encodedPlantUML: string = plantUmlEncode(plantUmlCode);
-    const response = await frontendSideFetch(`https://www.plantuml.com/plantuml/svg/${encodedPlantUML}`);
+    const response = await frontendSideFetch(`${plantUmlServerUrl}${encodedPlantUML}`);
     text = await response.text();
   } catch (e) {
     return null;