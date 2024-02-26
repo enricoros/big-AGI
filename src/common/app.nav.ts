bbce8ad7e97b0eaadf42d338e765bd316c1db1d3
Enrico Ros <enrico.ros@gmail.com>
2024-02-26 12:45:43 -0800

    Temp: hide Draw and Workspaces

--- src/common/app.nav.ts
+++ src/common/app.nav.ts
@@ -113,7 +113,7 @@ export const navItems: {
       route: '/draw',
       // hideOnMobile: true,
       hideDrawer: true,
-      // _delete: true,
+      _delete: true,
     },
     {
       name: 'Cortex',
@@ -139,7 +139,7 @@ export const navItems: {
       route: '/workspace',
       hideDrawer: true,
       hideOnMobile: true,
-      // _delete: true,
+      _delete: true,
     },
     // <-- divider here -->
     {
