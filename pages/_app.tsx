2656d0dfa5e983c88ca98945c2732c35e7e522e8
Enrico Ros <enrico.ros@gmail.com>
2024-02-27 00:26:13 -0800

    GA: Infrastructure. Enables data analysis for product improvement.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { MyAppProps } from 'next/app';
 import { Analytics as VercelAnalytics } from '@vercel/analytics/next';
 import { SpeedInsights as VercelSpeedInsights } from '@vercel/speed-insights/next';
 
-
 import { Brand } from '~/common/app.config';
 import { apiQuery } from '~/common/util/trpc.client';
 
@@ -20,6 +19,7 @@ import { ProviderSingleTab } from '~/common/providers/ProviderSingleTab';
 import { ProviderSnacks } from '~/common/providers/ProviderSnacks';
 import { ProviderTRPCQueryClient } from '~/common/providers/ProviderTRPCQueryClient';
 import { ProviderTheming } from '~/common/providers/ProviderTheming';
+import { hasGoogleAnalytics, OptionalGoogleAnalytics } from '~/common/components/GoogleAnalytics';
 import { isVercelFromFrontend } from '~/common/util/pwaUtils';
 
 
@@ -47,6 +47,7 @@ const MyApp = ({ Component, emotionCache, pageProps }: MyAppProps) =>
 
     {isVercelFromFrontend && <VercelAnalytics debug={false} />}
     {isVercelFromFrontend && <VercelSpeedInsights debug={false} sampleRate={1 / 2} />}
+    {hasGoogleAnalytics && <OptionalGoogleAnalytics />}
 
   </>;
 
