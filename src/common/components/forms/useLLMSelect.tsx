77a28eb810f4d38d34c0688073b574681ea334ed
Enrico Ros <enrico.ros@gmail.com>
2024-03-09 12:43:56 -0800

    Optimize LLMSelect

--- src/common/components/forms/useLLMSelect.tsx
+++ src/common/components/forms/useLLMSelect.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { shallow } from 'zustand/shallow';
 
-import { Box, FormControl, ListDivider, ListItemDecorator, Option, Select } from '@mui/joy';
+import { FormControl, ListDivider, ListItemDecorator, Option, Select } from '@mui/joy';
 
 import { DLLM, DLLMId, useModelsStore } from '~/modules/llms/store-llms';
 import { findVendorById } from '~/modules/llms/vendors/vendors.registry';
@@ -41,87 +41,98 @@ export function useLLMSelect(
 ): [DLLM | null, React.JSX.Element | null] {
 
   // external state
-  const llms = useModelsStore(state => state.llms, shallow);
+  const _filteredLLMs = useModelsStore(state => {
+    return state.llms.filter(llm => !llm.hidden || (chatLLMId && llm.id === chatLLMId));
+  }, shallow);
 
   // derived state
-  const chatLLM = chatLLMId ? llms.find(llm => llm.id === chatLLMId) ?? null : null;
+  const chatLLM = chatLLMId
+    ? _filteredLLMs.find(llm => llm.id === chatLLMId) ?? null
+    : null;
 
 
-  const component = React.useMemo(() => {
-    // hide invisible models, except the current model
-    const filteredLLMs = llms.filter(llm => !llm.hidden || llm.id === chatLLMId);
-
+  // Memo the LLM Options for the Select
+  const componentOptions = React.useMemo(() => {
     // create the option items
     let formerVendor: IModelVendor | null = null;
-    const options = filteredLLMs.map((llm) => {
+    return _filteredLLMs.reduce((acc, llm, index) => {
 
       const vendor = findVendorById(llm._source?.vId);
       const vendorChanged = vendor !== formerVendor;
-      const addSeparator = vendorChanged && formerVendor !== null;
       if (vendorChanged)
         formerVendor = vendor;
 
-      return (
-        <React.Fragment key={'llm-' + llm.id}>
-          {addSeparator && <ListDivider />}
-          <Option
-            value={llm.id}
-            sx={llm.id === chatLLMId ? { fontWeight: 'md' } : undefined}
-          >
-            {!!vendor?.Icon && (
-              <ListItemDecorator>
-                <vendor.Icon />
-              </ListItemDecorator>
-            )}
-            {/*<Tooltip title={llm.description}>*/}
-            {llm.label}
-            {/*</Tooltip>*/}
-            {/*{llm.gen === 'sdxl' && <Chip size='sm' variant='outlined'>XL</Chip>} {llm.label}*/}
-          </Option>
-        </React.Fragment>
+      // add separators if the vendor changed (and more than one vendor)
+      const addSeparator = vendorChanged && formerVendor !== null;
+      if (addSeparator)
+        acc.push(<ListDivider key={'llm-sep-' + llm.id}>{vendor?.name}</ListDivider>);
+
+      // the option component
+      acc.push(
+        <Option
+          key={'llm-' + llm.id}
+          value={llm.id}
+          // Disabled to avoid regenerating the memo too frequently
+          // sx={llm.id === chatLLMId ? { fontWeight: 'md' } : undefined}
+        >
+          {!!vendor?.Icon && (
+            <ListItemDecorator>
+              <vendor.Icon />
+            </ListItemDecorator>
+          )}
+          {/*<Tooltip title={llm.description}>*/}
+          {llm.label}
+          {/*</Tooltip>*/}
+          {/*{llm.gen === 'sdxl' && <Chip size='sm' variant='outlined'>XL</Chip>} {llm.label}*/}
+        </Option>,
       );
-    });
 
-    // create the component
-    return (
-      <FormControl orientation={isHorizontal ? 'horizontal' : undefined}>
-        {!!label && <FormLabelStart title={label} />}
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Select
-            variant='outlined'
-            value={chatLLMId}
-            size={smaller ? 'sm' : undefined}
-            onChange={(_event, value) => value && setChatLLMId(value)}
-            placeholder={placeholder}
-            slotProps={{
-              listbox: {
-                sx: {
-                  // larger list
-                  '--ListItem-paddingLeft': '1rem',
-                  '--ListItem-minHeight': '2.5rem',
-                  // minWidth: '100%',
-                },
-              },
-              button: {
-                sx: {
-                  // show the full name on the button
-                  whiteSpace: 'inherit',
-                },
-              },
-            }}
-            sx={{
-              flex: 1,
-              backgroundColor: 'background.popup',
-              // minWidth: '200',
-            }}
-          >
-            {options}
-          </Select>
-        </Box>
-      </FormControl>
-    );
-  }, [chatLLMId, isHorizontal, label, llms, placeholder, setChatLLMId, smaller]);
+      return acc;
+    }, [] as React.JSX.Element[]);
+  }, [_filteredLLMs]);
 
 
-  return [chatLLM, component];
+  const onSelectChange = React.useCallback((_event: unknown, value: DLLMId | null) => value && setChatLLMId(value), [setChatLLMId]);
+
+  // Memo the Select component
+  const llmSelectComponent = React.useMemo(() => (
+    <FormControl orientation={isHorizontal ? 'horizontal' : undefined}>
+      {!!label && <FormLabelStart title={label} />}
+      {/*<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>*/}
+      <Select
+        variant='outlined'
+        value={chatLLMId}
+        size={smaller ? 'sm' : undefined}
+        onChange={onSelectChange}
+        placeholder={placeholder}
+        slotProps={{
+          listbox: {
+            sx: {
+              // larger list
+              '--ListItem-paddingLeft': '1rem',
+              '--ListItem-minHeight': '2.5rem',
+              // minWidth: '100%',
+            },
+          },
+          button: {
+            sx: {
+              // show the full name on the button
+              whiteSpace: 'inherit',
+            },
+          },
+        }}
+        sx={{
+          flex: 1,
+          backgroundColor: 'background.popup',
+          // minWidth: '200',
+        }}
+      >
+        {componentOptions}
+      </Select>
+      {/*</Box>*/}
+    </FormControl>
+  ), [chatLLMId, componentOptions, isHorizontal, label, onSelectChange, placeholder, smaller]);
+
+
+  return [chatLLM, llmSelectComponent];
 }
