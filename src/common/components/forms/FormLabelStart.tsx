aeb29d983a50ffd507ecc8aba6121f27fd163dca
Enrico Ros <enrico.ros@gmail.com>
2024-03-11 23:27:58 -0700

    FormLabelStart: optimize

--- src/common/components/forms/FormLabelStart.tsx
+++ src/common/components/forms/FormLabelStart.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Box, FormHelperText, FormLabel } from '@mui/joy';
+import { FormHelperText, FormLabel } from '@mui/joy';
 import { SxProps } from '@mui/joy/styles/types';
 import InfoIcon from '@mui/icons-material/Info';
 
@@ -17,37 +17,35 @@ const FormLabelStartBase = (props: {
   tooltip?: string | React.JSX.Element,
   onClick?: (event: React.MouseEvent) => void,
   sx?: SxProps,
-}) => React.useMemo(() =>
-    <Box>
-      {/* Title */}
-      <FormLabel
-        onClick={props.onClick}
+}) =>
+  <div>
+    {/* Title */}
+    <FormLabel
+      onClick={props.onClick}
+      sx={{
+        minWidth: formLabelStartWidth,
+        ...(!!props.onClick && { cursor: 'pointer', textDecoration: 'underline' }),
+        ...props.sx,
+      }}
+    >
+      {props.title} {props.tooltip && (
+      <GoodTooltip title={props.tooltip}>
+        <InfoIcon sx={{ ml: 0.5, cursor: 'pointer', fontSize: 'md', color: 'primary.solidBg' }} />
+      </GoodTooltip>
+    )}
+    </FormLabel>
+
+    {/* [SubTitle] */}
+    {!!props.description && (
+      <FormHelperText
         sx={{
-          minWidth: formLabelStartWidth,
-          ...(!!props.onClick && { cursor: 'pointer', textDecoration: 'underline' }),
-          ...props.sx,
+          display: 'block',
         }}
       >
-        {props.title} {props.tooltip && (
-        <GoodTooltip title={props.tooltip}>
-          <InfoIcon sx={{ mx: 0.5, cursor: 'pointer', fontSize: 'md', color: 'primary.solidBg' }} />
-        </GoodTooltip>
-      )}
-      </FormLabel>
-
-      {/* [SubTitle] */}
-      {!!props.description && (
-        <FormHelperText
-          sx={{
-            display: 'block',
-          }}
-        >
-          {props.description}
-        </FormHelperText>
-      )}
-    </Box>
-  , [props.onClick, props.sx, props.title, props.tooltip, props.description],
-);
+        {props.description}
+      </FormHelperText>
+    )}
+  </div>;
 FormLabelStartBase.displayName = 'FormLabelStart';
 
 export const FormLabelStart = React.memo(FormLabelStartBase);
