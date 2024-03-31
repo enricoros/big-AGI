f36f6353814e5b26c774dac637fd6fcc9416b3e1
Enrico Ros <enrico.ros@gmail.com>
2024-03-30 19:04:37 -0700

    1.15.0: version number

--- src/apps/news/news.version.tsx
+++ src/apps/news/news.version.tsx
@@ -4,7 +4,7 @@
 import { useAppStateStore } from '~/common/state/store-appstate';
 
 
-export const incrementalNewsVersion: number = 14.1;
+export const incrementalNewsVersion: number = 15;
 
 
 export function shallRedirectToNews() {
