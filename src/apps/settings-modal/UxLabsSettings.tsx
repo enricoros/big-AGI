7479b50feae1a4c78f393bcde5e845bc88e5735a
Enrico Ros <enrico.ros@gmail.com>
2024-03-30 19:03:45 -0700

    1.15.0: Disable Title Bar Setting (2 people got confused)

--- src/apps/settings-modal/UxLabsSettings.tsx
+++ src/apps/settings-modal/UxLabsSettings.tsx
@@ -15,7 +15,7 @@ import { useUXLabsStore } from '~/common/state/store-ux-labs';
 
 
 // uncomment for more settings
-// const DEV_MODE_SETTINGS = false;
+export const DEV_MODE_SETTINGS = false;
 
 
 export function UxLabsSettings() {
@@ -42,10 +42,10 @@ export function UxLabsSettings() {
       checked={labsHighPerformance} onChange={setLabsHighPerformance}
     />
 
-    <FormSwitchControl
+    {DEV_MODE_SETTINGS && <FormSwitchControl
       title={<><TitleIcon color={labsChatBarAlt ? 'primary' : undefined} sx={{ mr: 0.25 }} />Chat Title</>} description={'v1.14 · ' + (labsChatBarAlt === 'title' ? 'Show Title' : 'Show Models')}
       checked={labsChatBarAlt === 'title'} onChange={(on) => setLabsChatBarAlt(on ? 'title' : false)}
-    />
+    />}
 
     {!isMobile && <FormSwitchControl
       title={<><ScreenshotMonitorIcon color={labsAttachScreenCapture ? 'primary' : undefined} sx={{ mr: 0.25 }} /> Screen Capture</>} description={'v1.13 · ' + (labsAttachScreenCapture ? 'Enabled' : 'Disabled')}
