e5bd37880cae272370647ba73dafb36f0c1c6021
Enrico Ros <enrico.ros@gmail.com>
2024-04-08 22:15:15 -0700

    Test client-side fetch (no cors)

--- src/modules/youtube/youtube.router.ts
+++ src/modules/youtube/youtube.router.ts
@@ -2,32 +2,16 @@
 // This subsystem is responsible for fetching the transcript of a YouTube video.
 // It is used by the Big-AGI Persona Creator to create a character sheet.
 
-import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc.server';
-import { fetchJsonOrTRPCError, fetchTextOrTRPCError } from '~/server/api/trpc.router.fetchers';
+import { fetchTextOrTRPCError } from '~/server/api/trpc.router.fetchers';
+
+import { fetchYouTubeTranscript } from './youtube.fetcher';
 
 
 const inputSchema = z.object({
   videoId: z.string().min(1),
-});
-
-const youtubeTranscriptionSchema = z.object({
-  wireMagic: z.literal('pb3'),
-  events: z.array(
-    z.object({
-      tStartMs: z.number(),
-      dDurationMs: z.number().optional(),
-      aAppend: z.number().optional(),
-      segs: z.array(
-        z.object({
-          utf8: z.string(),
-          tOffsetMs: z.number().optional(),
-        }),
-      ).optional(),
-    }),
-  ),
 });
 
 
@@ -40,47 +24,10 @@ export const youtubeRouter = createTRPCRouter({
     .input(inputSchema)
     .query(async ({ input }) => {
       const { videoId } = input;
-
-      // 1. find the cpations URL within the video HTML page
-      const html = await fetchTextOrTRPCError(`https://www.youtube.com/watch?v=${videoId}`, 'GET', {}, undefined, 'YouTube Transcript');
-
-      const captionsUrlEnc = extractFromTo(html, 'https://www.youtube.com/api/timedtext', '"', 'Captions URL');
-      const captionsUrl = decodeURIComponent(captionsUrlEnc.replaceAll('\\u0026', '&'));
-      const thumbnailUrl = extractFromTo(html, 'https://i.ytimg.com/vi/', '"', 'Thumbnail URL').replaceAll('maxres', 'hq');
-      const videoTitle = extractFromTo(html, '<title>', '</title>', 'Video Title').slice(7).replaceAll(' - YouTube', '').trim();
-
-      // 2. fetch the captions
-      // note: the desktop player appends this much: &fmt=json3&xorb=2&xobt=3&xovt=3&cbr=Chrome&cbrver=114.0.0.0&c=WEB&cver=2.20230628.07.00&cplayer=UNIPLAYER&cos=Windows&cosver=10.0&cplatform=DESKTOP
-      const captions = await fetchJsonOrTRPCError(captionsUrl + `&fmt=json3`, 'GET', {}, undefined, 'YouTube Captions');
-      const safeData = youtubeTranscriptionSchema.safeParse(captions);
-      if (!safeData.success) {
-        console.error(safeData.error);
-        throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: '[YouTube API Issue] Could not parse the captions' });
-      }
-
-      // 3. flatten to text
-      const transcript = safeData.data.events
-        .flatMap(event => event.segs ?? [])
-        .map(seg => seg.utf8)
-        .join('');
-
-      return {
-        videoId,
-        videoTitle,
-        thumbnailUrl,
-        transcript,
-      };
+      return await fetchYouTubeTranscript(videoId, url => fetchTextOrTRPCError(url, 'GET', {}, undefined, 'YouTube Transcript'));
     }),
 
 });
-
-function extractFromTo(html: string, from: string, to: string, label: string): string {
-  const indexStart = html.indexOf(from);
-  const indexEnd = html.indexOf(to, indexStart);
-  if (indexStart < 0 || indexEnd <= indexStart)
-    throw new TRPCError({ code: 'BAD_REQUEST', message: `[YouTube API Issue] Could not find ${label}` });
-  return html.substring(indexStart, indexEnd);
-}
 
 
 /*
