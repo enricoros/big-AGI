b185fbc57d9f54a76f3c2993b766839fc763e46f
Enrico Ros <enrico.ros@gmail.com>
2024-04-21 23:24:52 -0700

    Beam: fallback llm Id

--- src/modules/beam/scatter/beam.scatter.ts
+++ src/modules/beam/scatter/beam.scatter.ts
@@ -178,7 +178,7 @@ export const createScatterSlice: StateCreator<RootStoreSlice & ScatterStoreSlice
 
 
   setRayCount: (count: number) => {
-    const { rays, inputChatLlmId, _syncRaysStateToScatter } = _get();
+    const { rays, fallbackLlmId, _syncRaysStateToScatter } = _get();
     if (count < rays.length) {
       // Terminate exceeding rays
       rays.slice(count).forEach(rayScatterStop);
@@ -188,8 +188,8 @@ export const createScatterSlice: StateCreator<RootStoreSlice & ScatterStoreSlice
     } else if (count > rays.length) {
       _set({
         rays: [...rays, ...Array(count - rays.length).fill(null)
-          // Create missing rays, using the last ray llmId as a fallback, or the inputChatLlmId
-          .map(() => createBRay(rays[rays.length - 1]?.rayLlmId || inputChatLlmId)),
+          // Create missing rays, copying the llmId of the former Ray, or using the fallback
+          .map(() => createBRay(rays[rays.length - 1]?.rayLlmId || fallbackLlmId)),
         ],
       });
     }
