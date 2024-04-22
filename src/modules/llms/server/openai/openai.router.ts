181a3881e243e98055427ddf6a86f83b60b929f0
Enrico Ros <enrico.ros@gmail.com>
2024-04-22 02:52:19 -0700

    Groq: update models
    (cherry picked from commit 3eef03b303fd43bf93fb6385b65a0cbb7841e025)

--- src/modules/llms/server/openai/openai.router.ts
+++ src/modules/llms/server/openai/openai.router.ts
@@ -11,7 +11,7 @@ import { Brand } from '~/common/app.config';
 import { fixupHost } from '~/common/util/urlUtils';
 
 import { OpenAIWire, WireOpenAICreateImageOutput, wireOpenAICreateImageOutputSchema, WireOpenAICreateImageRequest } from './openai.wiretypes';
-import { azureModelToModelDescription, groqModelToModelDescription, lmStudioModelToModelDescription, localAIModelToModelDescription, mistralModelsSort, mistralModelToModelDescription, oobaboogaModelToModelDescription, openAIModelFilter, openAIModelToModelDescription, openRouterModelFamilySortFn, openRouterModelToModelDescription, perplexityAIModelDescriptions, perplexityAIModelSort, togetherAIModelsToModelDescriptions } from './models.data';
+import { azureModelToModelDescription, groqModelSortFn, groqModelToModelDescription, lmStudioModelToModelDescription, localAIModelToModelDescription, mistralModelsSort, mistralModelToModelDescription, oobaboogaModelToModelDescription, openAIModelFilter, openAIModelToModelDescription, openRouterModelFamilySortFn, openRouterModelToModelDescription, perplexityAIModelDescriptions, perplexityAIModelSort, togetherAIModelsToModelDescriptions } from './models.data';
 import { llmsChatGenerateWithFunctionsOutputSchema, llmsListModelsOutputSchema, ModelDescriptionSchema } from '../llm.server.types';
 import { wilreLocalAIModelsApplyOutputSchema, wireLocalAIModelsAvailableOutputSchema, wireLocalAIModelsListOutputSchema } from './localai.wiretypes';
 
@@ -168,7 +168,8 @@ export const llmOpenAIRouter = createTRPCRouter({
 
         case 'groq':
           models = openAIModels
-            .map(groqModelToModelDescription);
+            .map(groqModelToModelDescription)
+            .sort(groqModelSortFn);
           break;
 
         case 'lmstudio':
