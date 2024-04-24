eb92c3fb6bb24bc5f2dd6efe6a4af9cede4b8e49
Enrico Ros <enrico.ros@gmail.com>
2024-04-24 01:59:08 -0700

    Diagrams: improve instructions

--- src/modules/aifn/digrams/diagrams.data.ts
+++ src/modules/aifn/digrams/diagrams.data.ts
@@ -7,7 +7,7 @@ export type DiagramLanguage = 'mermaid' | 'plantuml';
 
 // NOTE: keep these global, or it will trigger re-renders
 export const diagramTypes: FormRadioOption<DiagramType>[] = [
-  { label: 'Auto-diagram', value: 'auto' },
+  { label: 'Automatic', value: 'auto' },
   { label: 'Mindmap', value: 'mind' },
 ];
 
@@ -34,6 +34,9 @@ mindmap
       Mermaid
 `.trim();
 
+const sysSuffixPM = 'The following three message contain: 1. your personality, 2. the data to operate on, and 3. the final message is a restatement of the instruction.';
+const usrSuffixCoT = 'Please think step by step, and then write valid code as instructed.';
+
 function mermaidDiagramPrompt(diagramType: DiagramType): { sys: string, usr: string } {
   let promptDetails = diagramType === 'auto'
     ? 'You create a valid Mermaid diagram markdown (```mermaid\\n...), ready to be rendered into a diagram. Ensure the code contains no external references, and all names are properly enclosed in double quotes and escaped if necessary. Choose the most suitable diagram type from the following supported types: flowchart, sequence, class, state, erd, gantt, pie, git.'
@@ -48,12 +51,12 @@ function plantumlDiagramPrompt(diagramType: DiagramType): { sys: string, usr: st
   switch (diagramType) {
     case 'auto':
       return {
-        sys: 'You are an AI that writes PlantUML code based on provided text. You create a valid PlantUML string, enclosed by "```\n@startuml" and "@enduml\n```", ready to be rendered into a diagram or mindmap, ensuring the code contains no external references and all names are properly escaped without spaces. You choose the most suitable diagram type—sequence, class, use case, activity, component, state, object, deployment, wireframe, mindmap, gantt, or flowchart.',
+        sys: 'You are an AI that writes PlantUML code based on provided text. You create a valid PlantUML string, enclosed by "```\\n@startuml" and "@enduml\\n```", ready to be rendered into a diagram or mindmap, ensuring the code contains no external references and all names are properly escaped without spaces. You choose the most suitable diagram type—sequence, class, use case, activity, component, state, object, deployment, wireframe, mindmap, gantt, or flowchart.',
         usr: 'Generate the PlantUML code for the diagram type that best represents the preceding assistant message.',
       };
     case 'mind':
       return {
-        sys: 'You are an AI that writes PlantUML code based on provided text. You create a valid PlantUML string, enclosed by "```\n@startmindmap" and "@endmindmap\n```", ready to be rendered into a mind map, ensuring the code contains no external references and all names are properly escaped without spaces.',
+        sys: 'You are an AI that writes PlantUML code based on provided text. You create a valid PlantUML string, enclosed by "```\\n@startmindmap" and "@endmindmap\\n```", ready to be rendered into a mind map, ensuring the code contains no external references and all names are properly escaped without spaces.',
         usr: 'Generate the PlantUML code for a mind map based on the preceding assistant message.',
       };
   }
@@ -61,13 +64,10 @@ function plantumlDiagramPrompt(diagramType: DiagramType): { sys: string, usr: st
 
 export function bigDiagramPrompt(diagramType: DiagramType, diagramLanguage: DiagramLanguage, chatSystemPrompt: string, subject: string, customInstruction: string): VChatMessageIn[] {
   const { sys, usr } = diagramLanguage === 'mermaid' ? mermaidDiagramPrompt(diagramType) : plantumlDiagramPrompt(diagramType);
-  if (customInstruction) {
-    customInstruction = 'Also consider the following instructions: ' + customInstruction;
-  }
   return [
-    { role: 'system', content: sys },
-    { role: 'system', content: chatSystemPrompt },
+    { role: 'system', content: sys + '\n' + sysSuffixPM },
+    { role: 'user', content: chatSystemPrompt },
     { role: 'assistant', content: subject },
-    { role: 'user', content: `${usr} ${customInstruction}` },
+    { role: 'user', content: (!customInstruction?.trim() ? usr : `${usr} Also consider the following instructions: ${customInstruction.trim()}`) + '\n' + usrSuffixCoT },
   ];
 }
