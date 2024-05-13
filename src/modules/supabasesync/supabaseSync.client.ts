3ec54824705cc205702379da283001a1638710ba
Joel M <joel.meikle@gmail.com>
2024-05-13 09:45:12 +0000

    wip on supabase connection

--- src/modules/supabasesync/supabaseSync.client.ts
+++ src/modules/supabasesync/supabaseSync.client.ts
@@ -1,4 +1,29 @@
+import { apiAsync } from '~/common/util/trpc.client';
 
+import { createClient } from '@supabase/supabase-js'
+import { Database } from './types/supabase' // Import the generated Supabase types
 
-export const isValidSupabaseConnection = (supabaseUrl?: string, supabaseAnonKey?: string) =>!!supabaseUrl && !!supabaseAnonKey;
+import { useSupabaseSyncStore } from "./store-module-supabase-sync";
 
+export const isValidSupabaseConnection = (url?: string, key?: string) => !!url && !!key;
+
+/**
+ * This function tests the Supabase connection
+ * @param url 
+ * @param key 
+ * @returns true if the connection is valid, false otherwise
+ */
+export async function testSupabaseConnection(url: string, key: string): Promise<boolean> {
+
+    // get the keys (empty if they're on server)
+    const { supabaseUrl, supabaseKey } = useSupabaseSyncStore.getState();
+    
+    try {
+        const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+        await supabase.auth.api.getUser();
+        return true;
+    } catch (error: any) {
+        console.error(`testSupabaseConnection: ${error}`);
+        return false;
+    }
+}
