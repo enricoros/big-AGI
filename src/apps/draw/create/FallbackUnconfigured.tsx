69e0fc5dc4342c6ae30c4f9b072df23bbda565f4
Enrico Ros <enrico.ros@gmail.com>
2024-06-12 02:24:56 -0700

    Draw: fix fallbacks

--- src/apps/draw/create/FallbackUnconfigured.tsx
+++ src/apps/draw/create/FallbackUnconfigured.tsx
@@ -5,23 +5,27 @@ import { Button, Card, CardActions, CardContent, Typography } from '@mui/joy';
 import { PreferencesTab, useOptimaLayout } from '~/common/layout/optima/useOptimaLayout';
 
 
-export function DrawUnconfigured() {
+export function FallbackUnconfigured() {
 
   // external state
   const { openPreferencesTab } = useOptimaLayout();
 
-  const handleConfigure = () => openPreferencesTab(PreferencesTab.Draw);
+  const handleConfigureDrawing = () => openPreferencesTab(PreferencesTab.Draw);
 
   return (
-    <Card variant='outlined' color='warning'>
+    <Card variant='outlined' color='neutral' sx={{
+      m: 'auto',
+      boxShadow: 'sm',
+      maxWidth: 'max(60%, 320px)',
+    }}>
       <CardContent>
         <Typography>
-          <strong>Text-to-Image</strong> does not seem available.
+          <strong>AI Text-to-Image</strong> does not seem available.<br />
           Please configure one service, such as an OpenAI LLM service, or the Prodia service.
         </Typography>
       </CardContent>
       <CardActions buttonFlex='0'>
-        <Button onClick={handleConfigure} sx={{ minWidth: '160px' }}>
+        <Button color='danger' onClick={handleConfigureDrawing} sx={{ minWidth: '160px' }}>
           Configure
         </Button>
       </CardActions>
