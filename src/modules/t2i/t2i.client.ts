cee91eec351e9855acab27d0d8ed3956ccd83904
Enrico Ros <enrico.ros@gmail.com>
2024-06-13 18:14:30 -0700

    MP: share common draw/store logic

--- src/modules/t2i/t2i.client.ts
+++ src/modules/t2i/t2i.client.ts
@@ -3,11 +3,14 @@ import { shallow } from 'zustand/shallow';
 import { useShallow } from 'zustand/react/shallow';
 import { useStoreWithEqualityFn } from 'zustand/traditional';
 
+import type { DBlobDBAsset } from '~/modules/dblobs/dblobs.types';
 import type { ModelVendorId } from '~/modules/llms/vendors/vendors.registry';
 import { DLLM, DModelSource, DModelSourceId, useModelsStore } from '~/modules/llms/store-llms';
+import { addDBImageAsset } from '~/modules/dblobs/dblobs.images';
 import { getBackendCapabilities } from '~/modules/backend/store-backend-capabilities';
 
 import type { CapabilityTextToImage, TextToImageProvider } from '~/common/components/useCapabilities';
+import { createDMessageDataRefDBlob, createImageContentFragment, DMessageContentFragment } from '~/common/stores/chat/chat.message';
 
 import type { T2iCreateImageOutput } from './t2i.server';
 import { openAIGenerateImagesOrThrow } from './dalle/openaiGenerateImages';
@@ -89,7 +92,7 @@ export function getActiveTextToImageProviderOrThrow() {
   return activeProvider;
 }
 
-export async function t2iGenerateImagesOrThrow({ id, vendor }: TextToImageProvider, prompt: string, count: number): Promise<T2iCreateImageOutput[]> {
+async function _t2iGenerateImagesOrThrow({ id, vendor }: TextToImageProvider, prompt: string, count: number): Promise<T2iCreateImageOutput[]> {
   switch (vendor) {
 
     case 'localai':
@@ -111,6 +114,59 @@ export async function t2iGenerateImagesOrThrow({ id, vendor }: TextToImageProvid
       return await prodiaGenerateImages(prompt, count);
 
   }
+}
+
+/**
+ * Generate image content fragments using the provided TextToImageProvider
+ * If t2iprovider is null, the active provider will be used
+ */
+export async function t2iGenerateImageContentFragments(
+  t2iProvider: TextToImageProvider | null, prompt: string, count: number,
+  contextId: DBlobDBAsset['contextId'], scopeId: DBlobDBAsset['scopeId'],
+): Promise<DMessageContentFragment[]> {
+
+  // T2I: Use the active provider if null
+  if (!t2iProvider)
+    t2iProvider = getActiveTextToImageProviderOrThrow();
+
+  // T2I: Generate
+  const generatedImages = await _t2iGenerateImagesOrThrow(t2iProvider, prompt, count);
+  if (!generatedImages?.length)
+    throw new Error('No image generated');
+
+  const imageFragments: DMessageContentFragment[] = [];
+  for (const _i of generatedImages) {
+
+    // add the image to the DB
+    const dblobAssetId = await addDBImageAsset(contextId, scopeId, {
+      label: prompt,
+      data: {
+        mimeType: _i.mimeType as any,
+        base64: _i.base64Data,
+      },
+      origin: {
+        ot: 'generated',
+        source: 'ai-text-to-image',
+        generatorName: _i.generatorName,
+        prompt: _i.altText,
+        parameters: _i.parameters,
+        generatedAt: _i.generatedAt,
+      },
+      metadata: {
+        width: _i.width || 0,
+        height: _i.height || 0,
+        // description: '',
+      },
+    });
+
+    // create a data reference for the image
+    const imageAssetDataRef = createDMessageDataRefDBlob(dblobAssetId, _i.mimeType, _i.base64Data.length);
+
+    // create an Image Content Fragment
+    const imageContentFragment = createImageContentFragment(imageAssetDataRef, _i.altText, _i.width, _i.height);
+    imageFragments.push(imageContentFragment);
+  }
+  return imageFragments;
 }
 
 
