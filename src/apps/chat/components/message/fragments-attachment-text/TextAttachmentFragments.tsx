1d1fda77803b1ecdf4b47718f4adf6b4984d7c9e
Enrico Ros <enrico.ros@gmail.com>
2024-06-21 03:13:30 -0700

    TextAttachmentFragment: works

--- src/apps/chat/components/message/fragments-attachment-text/TextAttachmentFragments.tsx
+++ src/apps/chat/components/message/fragments-attachment-text/TextAttachmentFragments.tsx
@@ -1,84 +1,125 @@
 import * as React from 'react';
 
 import type { SxProps } from '@mui/joy/styles/types';
-import { Box } from '@mui/joy';
+import { Box, Button } from '@mui/joy';
+
+import { BlocksRenderer } from '~/modules/blocks/BlocksRenderer';
 
 import type { ContentScaling } from '~/common/app.theme';
 import type { DMessageAttachmentFragment, DMessageFragmentId, DMessageRole } from '~/common/stores/chat/chat.message';
+import { ellipsizeMiddle } from '~/common/util/textUtils';
+import { marshallWrapText } from '~/common/stores/chat/chat.tokens';
 
-import { ContentPartPlaceholder } from '../fragments-content/ContentPartPlaceholder';
 
+const buttonPressedSx: SxProps = {
+  minHeight: '2rem',
+  minWidth: '5rem',
+  border: '1px solid',
+  borderColor: 'neutral.solidBg',
+  boxShadow: 'xs',
+};
 
-const attachmentFragmentsLayoutSx: SxProps = {
-  // the container is a grid, in ChatMessage
-  height: '100%',
-  overflowX: 'auto',
-  pr: 5,
+const buttonSx: SxProps = {
+  ...buttonPressedSx,
+  borderColor: 'primary.outlinedBorder',
+  backgroundColor: 'background.surface',
+};
 
-  // layout
-  display: 'flex',
-  gap: 1,
-
-  '& *': {
-    // border imporant! debug
-    border: '1px solid red!important',
-  },
+const viewPaneSx: SxProps = {
+  backgroundColor: 'background.surface',
+  border: '1px solid',
+  borderColor: 'primary.outlinedBorder',
+  borderRadius: 'sm',
+  boxShadow: 'inset 2px 0px 5px -4px var(--joy-palette-background-backdrop)',
+  mt: 0.5,
+  p: 1.5,
+  pt: 0,
 };
 
 
 /**
  * Displays a list of 'cards' which are buttons with a mutually exclusive active state.
  * When one is active, there is a content part just right under (with the collapse mechanism in case it's a user role).
- * If one is clicked the content part (use ContentFragments with a single Fragment) is displayed.
+ * If one is clicked the content part (use ContentPartText) is displayed.
  */
 export function TextAttachmentFragments(props: {
   textFragments: DMessageAttachmentFragment[],
   messageRole: DMessageRole,
   contentScaling: ContentScaling,
   isMobile?: boolean,
+  renderTextAsMarkdown: boolean;
   onFragmentDelete: (fragmentId: DMessageFragmentId) => void,
 }) {
 
   // state
   const [selectedFragmentId, setSelectedFragmentId] = React.useState<DMessageFragmentId | null>(null);
+  const [editedText, setEditedText] = React.useState<{ [fragmentId: string]: string }>({});
 
+  const handleSelectFragment = React.useCallback((fragmentId: DMessageFragmentId) => {
+    setSelectedFragmentId(prevId => prevId === fragmentId ? null : fragmentId);
+  }, []);
+
+  const selectedFragment = props.textFragments.find(fragment => fragment.fId === selectedFragmentId);
 
   return (
-    <Box aria-label={`${props.textFragments.length} text attachments`}>
+    <Box aria-label={`${props.textFragments.length} text attachments`} sx={{
+      //
+      maxWidth: '100%',
+      overflowX: 'auto',
+
+      // layout
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
 
       {/* Horizontally scrollable Attachments */}
-      <Box sx={attachmentFragmentsLayoutSx}>
+      <Box sx={{
+        // px: '4px', // to show the button shadow
+        pb: '4px', // to show the button shadow
 
-        {/* render each text attachment */}
+        // layout
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: 1,
+        justifyContent: props.messageRole === 'assistant' ? 'flex-start' : 'flex-end',
+      }}>
         {props.textFragments.map((attachmentFragment) => {
           // only operate on text
           if (attachmentFragment.part.pt !== 'text')
             throw new Error('Unexpected part type: ' + attachmentFragment.part.pt);
 
+          const isSelected = selectedFragmentId === attachmentFragment.fId;
+          const buttonText = ellipsizeMiddle(attachmentFragment.title || 'Text', 28 /* totally arbitrary length */);
+
           return (
-            <ContentPartPlaceholder
-              key={'att-txt-' + attachmentFragment.fId}
-              placeholderText={`Attachment Placeholder: ${attachmentFragment.part.text}`}
-              messageRole={props.messageRole}
-              contentScaling={props.contentScaling}
-            />
+            <Button
+              key={'text-toggle-' + attachmentFragment.fId}
+              size='sm'
+              variant={isSelected ? 'solid' : 'soft'}
+              color={isSelected ? 'neutral' : 'neutral'}
+              // color={isSelected ? 'primary' : 'neutral'}
+              onClick={() => handleSelectFragment(attachmentFragment.fId)}
+              sx={isSelected ? buttonPressedSx : buttonSx}
+            >
+              {buttonText}
+            </Button>
           );
         })}
-
-
-        {/*{llmAttachmentDrafts.map((llmAttachment) =>*/}
-        {/*  <LLMAttachmentItem*/}
-        {/*    key={llmAttachment.attachmentDraft.id}*/}
-        {/*    llmAttachment={llmAttachment}*/}
-        {/*    menuShown={llmAttachment.attachmentDraft.id === itemMenuAttachmentDraftId}*/}
-        {/*    onToggleMenu={handleDraftMenuToggle}*/}
-        {/*  />,*/}
-        {/*)}*/}
-
       </Box>
 
       {/* Viewer for the selected attachment */}
-
+      {!!selectedFragment && selectedFragment.part.pt === 'text' && (
+        <Box sx={viewPaneSx}>
+          <BlocksRenderer
+            text={marshallWrapText(selectedFragment.part.text, '', 'markdown-code')}
+            // text={selectedFragment.part.text}
+            fromRole={props.messageRole}
+            contentScaling={props.contentScaling}
+            fitScreen={props.isMobile}
+            renderTextAsMarkdown={props.renderTextAsMarkdown}
+          />
+        </Box>
+      )}
 
     </Box>
   );
