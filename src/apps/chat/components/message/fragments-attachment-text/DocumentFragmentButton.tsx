8c3354a48fd5425e0015221b11419b5d61431228
Enrico Ros <enrico.ros@gmail.com>
2024-06-21 03:47:53 -0700

    DocumentFragment: improve

---
+++ src/apps/chat/components/message/fragments-attachment-text/DocumentFragmentButton.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+import type { SxProps } from '@mui/joy/styles/types';
+import { Button } from '@mui/joy';
+
+import type { DMessageAttachmentFragment, DMessageFragmentId } from '~/common/stores/chat/chat.message';
+import { ellipsizeMiddle } from '~/common/util/textUtils';
+
+
+const buttonPressedSx: SxProps = {
+  minHeight: '2rem',
+  minWidth: '5rem',
+  border: '1px solid',
+  borderColor: 'neutral.solidBg',
+  boxShadow: 'xs',
+};
+
+const buttonSx: SxProps = {
+  ...buttonPressedSx,
+  borderColor: 'primary.outlinedBorder',
+  backgroundColor: 'background.surface',
+};
+
+
+export function DocumentFragmentButton(props: {
+  fragment: DMessageAttachmentFragment,
+  isSelected: boolean,
+  toggleSelected: (fragmentId: DMessageFragmentId) => void
+}) {
+
+  // derived state
+  const { fragment, isSelected, toggleSelected } = props;
+
+  // handlers
+  const handleSelectFragment = React.useCallback(() => {
+    toggleSelected(fragment.fId);
+  }, [fragment.fId, toggleSelected]);
+
+  // only operate on text
+  if (fragment.part.pt !== 'text')
+    throw new Error('Unexpected part type: ' + fragment.part.pt);
+
+  const buttonText = ellipsizeMiddle(fragment.title || 'Text', 28 /* totally arbitrary length */);
+
+  return (
+    <Button
+      size='sm'
+      variant={isSelected ? 'solid' : 'soft'}
+      color={isSelected ? 'neutral' : 'neutral'}
+      onClick={handleSelectFragment}
+      sx={isSelected ? buttonPressedSx : buttonSx}
+    >
+      {buttonText}
+    </Button>
+  );
+}
---
+++ src/apps/chat/components/message/fragments-attachment-text/DocumentFragments.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react';
+import { Box } from '@mui/joy';
+
+import type { ContentScaling } from '~/common/app.theme';
+import type { DMessageAttachmentFragment, DMessageFragmentId, DMessageRole } from '~/common/stores/chat/chat.message';
+
+import { DocumentFragmentButton } from './DocumentFragmentButton';
+import { DocumentFragmentEditor } from './DocumentFragmentEditor';
+import { ChatMessageTextPartEditState } from '../ChatMessage';
+
+
+/**
+ * Displays a list of 'cards' which are buttons with a mutually exclusive active state.
+ * When one is active, there is a content part just right under (with the collapse mechanism in case it's a user role).
+ * If one is clicked the content part (use ContentPartText) is displayed.
+ */
+export function DocumentFragments(props: {
+  attachmentFragments: DMessageAttachmentFragment[],
+  messageRole: DMessageRole,
+  contentScaling: ContentScaling,
+  isMobile?: boolean,
+  renderTextAsMarkdown: boolean;
+  onFragmentDelete: (fragmentId: DMessageFragmentId) => void,
+  onFragmentReplace: (fragmentId: DMessageFragmentId, newContent: DMessageAttachmentFragment) => void,
+}) {
+
+  // state
+  const [selectedFragmentId, setSelectedFragmentId] = React.useState<DMessageFragmentId | null>(null);
+  const [textAttachmentsEditState, setTextAttachmentsEditState] = React.useState<ChatMessageTextPartEditState | null>(null);
+
+  // [effect] clear edits on onmount
+  React.useEffect(() => {
+    return () => setTextAttachmentsEditState(null);
+  }, []);
+
+  const handleToggleSelected = React.useCallback((fragmentId: DMessageFragmentId) => {
+    setSelectedFragmentId(prevId => prevId === fragmentId ? null : fragmentId);
+  }, []);
+
+  const selectedFragment = props.attachmentFragments.find(fragment => fragment.fId === selectedFragmentId);
+
+  return (
+    <Box aria-label={`${props.attachmentFragments.length} attachments`} sx={{
+      //
+      maxWidth: '100%',
+      overflowX: 'auto',
+
+      // layout
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+
+      {/* Horizontally scrollable Attachments */}
+      <Box sx={{
+        // px: '4px', // to show the button shadow
+        pb: '4px', // to show the button shadow
+
+        // layout
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: 1,
+        justifyContent: props.messageRole === 'assistant' ? 'flex-start' : 'flex-end',
+      }}>
+        {props.attachmentFragments.map((attachmentFragment) =>
+          <DocumentFragmentButton
+            key={attachmentFragment.fId}
+            fragment={attachmentFragment}
+            isSelected={selectedFragmentId === attachmentFragment.fId}
+            toggleSelected={handleToggleSelected}
+          />,
+        )}
+      </Box>
+
+      {/* Viewer for the selected attachment */}
+      {!!selectedFragment && (
+        <DocumentFragmentEditor
+          fragment={selectedFragment}
+          messageRole={props.messageRole}
+          editedText={textAttachmentsEditState?.[selectedFragment.fId]}
+          contentScaling={props.contentScaling}
+          isMobile={props.isMobile}
+          renderTextAsMarkdown={props.renderTextAsMarkdown}
+          onFragmentDelete={props.onFragmentDelete}
+          onFragmentReplace={props.onFragmentReplace}
+        />
+      )}
+
+    </Box>
+  );
+}
