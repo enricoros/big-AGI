33c64a3815900efc51f3ccee07a9a0c55a808257
Enrico Ros <enrico.ros@gmail.com>
2024-06-22 16:05:54 -0700

    Document Editor: inline editing

--- src/apps/chat/components/message/fragments-attachment-text/DocumentFragmentEditor.tsx
+++ src/apps/chat/components/message/fragments-attachment-text/DocumentFragmentEditor.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
-
-import type { SxProps } from '@mui/joy/styles/types';
-import { Box } from '@mui/joy';
+import { Box, Button } from '@mui/joy';
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
 
 import { BlocksRenderer } from '~/modules/blocks/BlocksRenderer';
 
@@ -9,22 +12,13 @@ import type { ContentScaling } from '~/common/app.theme';
 import type { DMessageAttachmentFragment, DMessageFragmentId, DMessageRole } from '~/common/stores/chat/chat.message';
 import { marshallWrapText } from '~/common/stores/chat/chat.tokens';
 
-
-const viewPaneSx: SxProps = {
-  backgroundColor: 'background.surface',
-  border: '1px solid',
-  borderColor: 'primary.outlinedBorder',
-  borderRadius: 'sm',
-  boxShadow: 'inset 2px 0px 5px -4px var(--joy-palette-background-backdrop)',
-  mt: 0.5,
-  p: 1.5,
-  pt: 0,
-};
+import { ContentPartTextEdit } from '../fragments-content/ContentPartTextEdit';
 
 
 export function DocumentFragmentEditor(props: {
   fragment: DMessageAttachmentFragment,
   editedText?: string,
+  setEditedText: (fragmentId: DMessageFragmentId, value: string) => void,
   messageRole: DMessageRole,
   contentScaling: ContentScaling,
   isMobile?: boolean,
@@ -36,6 +30,7 @@ export function DocumentFragmentEditor(props: {
   // derived state
   const { fragment, onFragmentDelete, onFragmentReplace } = props;
   const [isEditing, setIsEditing] = React.useState(false);
+  const [isDeleteArmed, setIsDeleteArmed] = React.useState(false);
 
   const fragmentId = fragment.fId;
   const fragmentTitle = fragment.title;
@@ -55,16 +50,97 @@ export function DocumentFragmentEditor(props: {
   if (part.pt !== 'text')
     throw new Error('Unexpected part type: ' + part.pt);
 
+  const handleEditToggle = React.useCallback(() => {
+    setIsDeleteArmed(false);
+    setIsEditing(on => !on);
+  }, []);
+
+  const handleEditEnterPressed = React.useCallback(() => {
+    // setIsEditing(false);
+    // TODO...
+  }, []);
+
+  const handleDeleteArmedToggle = React.useCallback(() => {
+    setIsEditing(false);
+    setIsDeleteArmed(on => !on);
+  }, []);
+
+
   return (
-    <Box sx={viewPaneSx}>
-      <BlocksRenderer
-        text={marshallWrapText(part.text, '', 'markdown-code')}
-        // text={selectedFragment.part.text}
-        fromRole={props.messageRole}
-        contentScaling={props.contentScaling}
-        fitScreen={props.isMobile}
-        renderTextAsMarkdown={props.renderTextAsMarkdown}
-      />
+    <Box sx={{
+      backgroundColor: 'background.level2',
+      border: '1px solid',
+      borderColor: 'neutral.outlinedBorder',
+      borderRadius: 'sm',
+      boxShadow: 'inset 2px 0px 5px -4px var(--joy-palette-background-backdrop)',
+      p: 1,
+      mt: 0.5,
+    }}>
+
+      {isEditing ? (
+        // Document Editor
+        <ContentPartTextEdit
+          textPart={part}
+          fragmentId={fragmentId}
+          contentScaling={props.contentScaling}
+          editedText={props.editedText}
+          setEditedText={props.setEditedText}
+          onEnterPressed={handleEditEnterPressed}
+          onEscapePressed={handleEditToggle}
+        />
+      ) : (
+        // Document viewer, including collapse/expand
+        <BlocksRenderer
+          text={marshallWrapText(part.text, '', 'markdown-code')}
+          // text={selectedFragment.part.text}
+          fromRole={props.messageRole}
+          contentScaling={props.contentScaling}
+          fitScreen={props.isMobile}
+          specialCodePlain
+          renderTextAsMarkdown={props.renderTextAsMarkdown}
+        />
+      )}
+
+      {/* Edit / Delete commands */}
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', mt: 1 }}>
+
+        <Box sx={{ display: 'flex', gap: 1 }}>
+          {isDeleteArmed ? (
+            <Button variant='solid' color='neutral' size='sm' onClick={handleDeleteArmedToggle} startDecorator={<CloseRoundedIcon />}>
+              Cancel
+            </Button>
+          ) : (
+            <Button variant='plain' color='neutral' size='sm' onClick={handleDeleteArmedToggle} startDecorator={<DeleteOutlineIcon />}>
+              Delete
+            </Button>
+          )}
+          {isDeleteArmed && (
+            <Button variant='plain' color='danger' size='sm' onClick={handleDeleteFragment} startDecorator={<DeleteForeverIcon />}>
+              Delete
+            </Button>
+          )}
+        </Box>
+
+        <Box sx={{ ml: 'auto', display: 'flex', gap: 1 }}>
+          {isEditing ? (
+            <Button variant='plain' color='neutral' size='sm' onClick={handleEditToggle} startDecorator={<CloseRoundedIcon />}>
+              Cancel
+            </Button>
+          ) : (
+            <Button variant='plain' color='neutral' size='sm' onClick={handleEditToggle} startDecorator={<EditRoundedIcon />}>
+              Edit
+            </Button>
+          )}
+          {isEditing && (
+            <Button variant='plain' color='success' onClick={undefined} size='sm' startDecorator={<CheckRoundedIcon />}>
+              Save
+            </Button>
+          )}
+        </Box>
+
+      </Box>
+
+
     </Box>
   );
 }
