a303d00900a9873d954a72f9074e07d6829af311
Enrico Ros <enrico.ros@gmail.com>
2024-06-22 16:34:26 -0700

    style

--- src/apps/chat/components/message/ChatMessage.tsx
+++ src/apps/chat/components/message/ChatMessage.tsx
@@ -523,7 +523,7 @@ export function ChatMessage(props: {
           // layout
           display: 'flex',
           flexDirection: 'column',
-          gap: 2,       // 2 because we give a bit more space between the 'classes' of fragments (content, attachments, etc.)
+          gap: 1.5,     // we give a bit more space between the 'classes' of fragments (content, attachments, etc.)
         }}>
 
           {/* (optional) Message date */}
