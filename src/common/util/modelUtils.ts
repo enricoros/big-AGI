5f97d178373896e8ce608d5ec69167bdc274aacc
Enrico Ros <enrico.ros@gmail.com>
2024-06-23 01:10:53 -0700

    anthropic model naming

--- src/common/util/modelUtils.ts
+++ src/common/util/modelUtils.ts
@@ -6,6 +6,7 @@ function getModelFromFile(filePath: string): string {
 
 export function prettyBaseModel(model: string | undefined): string {
   if (!model) return '';
+  // [OpenAI]
   if (model.includes('gpt-4-vision-preview')) return 'GPT-4 Vision';
   if (model.includes('gpt-4-1106-preview')) return 'GPT-4 Turbo';
   if (model.includes('gpt-4-32k')) return 'gpt-4-32k';
@@ -14,10 +15,11 @@ export function prettyBaseModel(model: string | undefined): string {
   if (model.includes('gpt-3.5-turbo-1106')) return '3.5 Turbo 16k';
   if (model.includes('gpt-3.5-turbo-16k')) return '3.5 Turbo 16k';
   if (model.includes('gpt-3.5-turbo')) return '3.5 Turbo';
+  // [LocalAI?]
   if (model.endsWith('.bin')) return model.slice(0, -4);
   // [Anthropic]
-  if (model.includes('claude-3-opus')) return 'Claude 3 Opus';
-  if (model.includes('claude-3-sonnet')) return 'Claude 3 Sonnet';
+  const prettyAnthropic = prettyAnthropicModelName(model);
+  if (prettyAnthropic) return prettyAnthropic;
   // [LM Studio]
   if (model.startsWith('C:\\') || model.startsWith('D:\\'))
     return getModelFromFile(model).replace('.gguf', '');
@@ -25,4 +27,19 @@ export function prettyBaseModel(model: string | undefined): string {
   if (model.includes(':'))
     return model.replace(':latest', '').replaceAll(':', ' ');
   return model;
+}
+
+function prettyAnthropicModelName(modelId: string): string | null {
+  const claudeIndex = modelId.indexOf('claude-3');
+  if (claudeIndex === -1) return null;
+
+  const subStr = modelId.slice(claudeIndex);
+  const is35 = subStr.includes('-3-5-');
+  const version = is35 ? '3.5' : '3';
+
+  if (subStr.includes(`-opus`)) return `Claude ${version} Opus`;
+  if (subStr.includes(`-sonnet`)) return `Claude ${version} Sonnet`;
+  if (subStr.includes(`-haiku`)) return `Claude ${version} Haiku`;
+
+  return `Claude ${version}`;
 }
