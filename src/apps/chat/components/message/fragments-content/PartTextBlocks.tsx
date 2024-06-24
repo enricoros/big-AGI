2fd694d22e824f8c3b38535900e7f72b6d720b89
Enrico Ros <enrico.ros@gmail.com>
2024-06-23 19:56:55 -0700

    Blocks: cleanups

--- src/apps/chat/components/message/fragments-content/PartTextBlocks.tsx
+++ src/apps/chat/components/message/fragments-content/PartTextBlocks.tsx
@@ -14,7 +14,7 @@ import { explainServiceErrors } from '../explainServiceErrors';
  * The OG part, comprised of text, which can be markdown, have code blocks, etc.
  * Uses BlocksRenderer to render the markdown/code/html/text, etc.
  */
-export function ContentPartText(props: {
+export function PartTextBlocks(props: {
   textPartText: string,
 
   messageRole: DMessageRole,
---
+++ src/modules/blocks/blocks.styles.ts
@@ -0,0 +1,37 @@
+import { Box, styled } from '@mui/joy';
+import { lineHeightChatTextMd } from '~/common/app.theme';
+
+
+export const blocksTextStyleSx = {
+  // note: this will be used for non-blocks mainly (errors and other strings ourside of RenderXYX)
+  lineHeight: lineHeightChatTextMd,
+
+  // customize the text selection color (also in edit mode)
+  '& *::selection': {
+    // backgroundColor: '#fc70c3',
+    backgroundColor: 'var(--joy-palette-primary-solidBg)',
+    color: 'var(--joy-palette-primary-solidColor)',
+  },
+};
+
+
+/**
+ * This style is reused by all the Fragments (BlocksRenderer being the Text one),
+ * contained within a singe Grid (1fr) in the Message component.
+ */
+export const BlocksContainer = styled(Box)({
+  // the parent is a Grid, and this takes up to the Grid's width
+  // - maxWidth: '100%' makes sure we don't x-scroll the whole chat window
+  // - width: '100%' would also maximize the fragment width to the containing grid even if smaller
+  // NOTE 1: we choose just `maxWidth` to allow the grid to place this towards the start/end of the message
+  // however see `ContentPartTextEdit` where we set the width to 100% as we need a large editor
+  // NOTE 2: after seeing html/code fragments smaller than the text fragments before them, we're back
+  // to using width: 100% to make sure the fragments are all the same width (we lose the alignment unfortunately)
+  width: '100%',
+
+  // enables children's x-scrollbars (clips to the Fragment, so sub-parts will stay within this)
+  overflowX: 'auto',
+
+  // text style
+  ...blocksTextStyleSx,
+});
