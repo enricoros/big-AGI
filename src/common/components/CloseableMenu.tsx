f1611bd75d2b53e59fd4ff5f11f6fe478a44ca4d
Eric Friday <ericfriday@gmail.com>
2024-07-01 12:56:14 -0500

    wip

--- src/common/components/CloseableMenu.tsx
+++ src/common/components/CloseableMenu.tsx
@@ -4,12 +4,10 @@ import { ClickAwayListener, Popper, PopperPlacementType } from '@mui/base';
 import { MenuList, styled } from '@mui/joy';
 import { SxProps } from '@mui/joy/styles/types';
 
-
 // adds the 'sx' prop to the Popper, and defaults zIndex to 1000
 const Popup = styled(Popper)({
   zIndex: 1000,
 });
-
 
 /**
  * Workaround to the Menu in Joy 5-beta.0.
@@ -21,23 +19,24 @@ const Popup = styled(Popper)({
  *  - ...
  */
 export function CloseableMenu(props: {
-  open: boolean, anchorEl: HTMLElement | null, onClose: () => void,
-  dense?: boolean,
-  bigIcons?: boolean,
+  open: boolean;
+  anchorEl: HTMLElement | null;
+  onClose: () => void;
+  dense?: boolean;
+  bigIcons?: boolean;
   // variant?: VariantProp,
   // color?: ColorPaletteProp,
   // size?: 'sm' | 'md' | 'lg',
-  placement?: PopperPlacementType,
-  placementOffset?: number[],
-  maxHeightGapPx?: number,
-  noTopPadding?: boolean,
-  noBottomPadding?: boolean,
-  sx?: SxProps,
-  zIndex?: number,
-  listRef?: React.Ref<HTMLUListElement>,
-  children?: React.ReactNode,
+  placement?: PopperPlacementType;
+  placementOffset?: number[];
+  maxHeightGapPx?: number;
+  noTopPadding?: boolean;
+  noBottomPadding?: boolean;
+  sx?: SxProps;
+  zIndex?: number;
+  listRef?: React.Ref<HTMLUListElement>;
+  children?: React.ReactNode;
 }) {
-
   const handleClose = (event: MouseEvent | TouchEvent | React.KeyboardEvent) => {
     event.stopPropagation();
     props.onClose();
@@ -47,8 +46,7 @@ export function CloseableMenu(props: {
     if (event.key === 'Tab') {
       handleClose(event);
     } else if (event.key === 'Escape') {
-      if (props.anchorEl)
-        props.anchorEl?.focus();
+      if (props.anchorEl) props.anchorEl?.focus();
       handleClose(event);
     }
   };
@@ -60,16 +58,15 @@ export function CloseableMenu(props: {
       anchorEl={props.anchorEl}
       placement={props.placement}
       disablePortal={false}
-      modifiers={[{
-        name: 'offset',
-        options: {
-          offset: props.placementOffset || [0, 4],
+      modifiers={[
+        {
+          name: 'offset',
+          options: {
+            offset: props.placementOffset || [0, 4],
+          },
         },
-      }]}
-      sx={props.zIndex
-        ? { zIndex: props.zIndex }
-        : {}
-      }
+      ]}
+      sx={props.zIndex ? { zIndex: props.zIndex } : {}}
     >
       <ClickAwayListener onClickAway={handleClose}>
         <MenuList
@@ -79,14 +76,16 @@ export function CloseableMenu(props: {
           sx={{
             '--ListItem-minHeight': props.dense
               ? '2.25rem' /* 2.25 is the default */
-              : '2.5rem', /* we enlarge the default  */
+              : '2.5rem' /* we enlarge the default  */,
             ...(props.bigIcons && {
               '--Icon-fontSize': 'var(--joy-fontSize-xl2)',
               // '--ListItemDecorator-size': '2.75rem',
             }),
             backgroundColor: 'background.popup',
             boxShadow: 'md',
-            ...(props.maxHeightGapPx !== undefined ? { maxHeight: `calc(100dvh - ${props.maxHeightGapPx}px)`, overflowY: 'auto' } : {}),
+            ...(props.maxHeightGapPx !== undefined
+              ? { maxHeight: `calc(100dvh - ${props.maxHeightGapPx}px)`, overflowY: 'auto' }
+              : {}),
             ...(props.noTopPadding ? { pt: 0 } : {}),
             ...(props.noBottomPadding ? { pb: 0 } : {}),
             ...(props.sx || {}),
