72cd872d7ba6e542b59211edbab5a6fdf9d53665
Eric Friday <efriday@nmdp.org>
2024-07-01 20:59:01 -0500

    updates to setting, tooling, deps, and app config

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,23 +8,22 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
+    Base: 'EricFriday.dev | Big AGI',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'EF.dev ~ Big AGI',
   },
   Meta: {
-    Description: 'Launch big-AGI to unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
-    SiteName: 'big-AGI | Precision AI for You',
-    ThemeColor: '#32383E',
-    TwitterSite: '@enricoros',
+    Description: 'Big AGI client.',
+    SiteName: 'EricFriday.dev | Big AGI Client & Dev Tooling',
+    ThemeColor: '#80f077',
   },
   URIs: {
-    Home: 'https://big-agi.com',
-    // App: 'https://get.big-agi.com',
-    CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
+    Home: 'https://ericfriday.dev',
+    // App: 'https://get.ericfriday.dev',
+    CardImage: 'https://ericfriday.dev/icons/card-dark-1200.png',
     OpenRepo: 'https://github.com/enricoros/big-agi',
     OpenProject: 'https://github.com/users/enricoros/projects/4',
     SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
     // Twitter: 'https://www.twitter.com/enricoros',
-    PrivacyPolicy: 'https://big-agi.com/privacy',
+    PrivacyPolicy: 'https://ericfriday.dev/privacy',
   },
 } as const;
