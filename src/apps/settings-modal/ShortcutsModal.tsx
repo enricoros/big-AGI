abe5391af265b69412054e6490f81195cbe83d56
Enrico Ros <enrico.ros@gmail.com>
2024-07-08 17:39:23 -0700

    Close #581

--- src/apps/settings-modal/ShortcutsModal.tsx
+++ src/apps/settings-modal/ShortcutsModal.tsx
@@ -3,38 +3,36 @@ import * as React from 'react';
 import { BlocksRenderer } from '~/modules/blocks/BlocksRenderer';
 
 import { GoodModal } from '~/common/components/GoodModal';
-import { isMacUser } from '~/common/util/pwaUtils';
 import { platformAwareKeystrokes } from '~/common/components/KeyStroke';
 import { useIsMobile } from '~/common/components/useMatchMedia';
 
 
 const shortcutsMd = platformAwareKeystrokes(`
 
-| Shortcut                                | Description                                     |
-|-----------------------------------------|-------------------------------------------------|
-| **Edit**                                |                                                 |
-| Shift + Enter                           | Newline                                         |
-| Alt + Enter                             | Append (no response)                            |
-| Ctrl + Shift + B                        | **Beam** last message                           |
-| Ctrl + Shift + R                        | **Regenerate** last message                     |
-| Ctrl + Shift + V                        | Attach clipboard (better than Ctrl + V)         |
-| Ctrl + M                                | Microphone (voice typing)                       |
-| **Chats**                               |                                                 |
-| Ctrl + O                                | Open Chat ...                                   |
-| Ctrl + S                                | Save Chat ...                                   |
-| Ctrl + ${isMacUser ? '' : 'Alt +'} N    | **New** chat                                    |
-| Ctrl + ${isMacUser ? '' : 'Alt +'} X    | **Reset** chat                                  |
-| Ctrl + ${isMacUser ? '' : 'Alt +'} D    | **Delete** chat                                 |
-| Ctrl + ${isMacUser ? '' : 'Alt +'} B    | **Branch** chat                                 |
-| Ctrl + Alt + Left                       | **Previous** chat (in history)                  |
-| Ctrl + Alt + Right                      | **Next** chat (in history)                      |
-| **Settings**                            |                                                 |
-| Ctrl + Shift + P                        | ⚙️ Preferences                                  |
-| Ctrl + Shift + M                        | 🧠 Models                                       |
-| Ctrl + Shift + O                        | 💬 Options (current Chat Model)                 |
-| Ctrl + Shift + +                        | Increase Text Size                              |
-| Ctrl + Shift + -                        | Decrease Text Size                              |
-| Ctrl + Shift + ${isMacUser ? '/' : '?'} | Shortcuts                                       |
+| Shortcut         | Description                             |
+|------------------|-----------------------------------------|
+| **Edit**         |                                         |
+| Shift + Enter    | Newline                                 |
+| Alt + Enter      | Append (no response)                    |
+| Ctrl + Shift + B | **Beam** last message                   |
+| Ctrl + Shift + G | Re**generate** last message             |
+| Ctrl + Shift + V | Attach clipboard (better than Ctrl + V) |
+| Ctrl + M         | Microphone (voice typing)               |
+| **Chats**        |                                         |
+| Ctrl + O         | Open Chat ...                           |
+| Ctrl + S         | Save Chat ...                           |
+| Ctrl + Shift + N | **New** chat                            |
+| Ctrl + Shift + X | **Reset** chat                          |
+| Ctrl + Shift + D | **Delete** chat                         |
+| Ctrl + [         | **Previous** chat (in history)          |
+| Ctrl + ]         | **Next** chat (in history)              |
+| **Settings**     |                                         |
+| Ctrl + ,         | ⚙️ Preferences                          |
+| Ctrl + Shift + M | 🧠 Models                               |
+| Ctrl + Shift + O | 💬 Options (current Chat Model)         |
+| Ctrl + Shift + + | Increase Text Size                      |
+| Ctrl + Shift + - | Decrease Text Size                      |
+| Ctrl + Shift + / | Shortcuts                               |
 
 `).trim();
 
