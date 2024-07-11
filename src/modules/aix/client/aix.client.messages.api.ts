fa70e6ac9d8e7e7a944ff28690d205733eff8f0c
Enrico Ros <enrico.ros@gmail.com>
2024-07-11 00:44:09 -0700

    AIX: Messages API: rescale assistant messages

--- src/modules/aix/client/aix.client.messages.api.ts
+++ src/modules/aix/client/aix.client.messages.api.ts
@@ -2,10 +2,18 @@ import { getImageAsset } from '~/modules/dblobs/dblobs.images';
 
 import type { DMessage } from '~/common/stores/chat/chat.message';
 import { DMessageImageRefPart, isContentFragment, isContentOrAttachmentFragment, isTextPart } from '~/common/stores/chat/chat.fragments';
+import { LLMImageResizeMode, resizeBase64ImageIfNeeded } from '~/common/util/imageUtils';
 
 import { AixChatContentGenerateRequest, AixChatMessage, AixChatMessageModel, AixChatMessageUser, createAixInlineImagePart } from './aix.client.api';
 
+
 // TODO: remove console messages to zero, or replace with throws or something
+
+
+// configuration
+export const MODEL_IMAGE_RESCALE_MIMETYPE = 'image/webp';
+export const MODEL_IMAGE_RESCALE_QUALITY = 0.90;
+
 
 //
 // AIX <> Chat Messages API helpers
@@ -36,7 +44,30 @@ export async function conversationMessagesToAixGenerateRequest(messageSequence: 
 
     // map the other parts
     let aixChatMessage: AixChatMessage | undefined = undefined;
-    if (m.role === 'assistant') {
+    if (m.role === 'user') {
+
+      aixChatMessage = await m.fragments.reduce(async (mMsgPromise, srcFragment) => {
+        const mMsg = await mMsgPromise;
+        if (!isContentOrAttachmentFragment(srcFragment))
+          return mMsg;
+        switch (srcFragment.part.pt) {
+          case 'text':
+            mMsg.parts.push(srcFragment.part);
+            break;
+          case 'image_ref':
+            // note, we don't resize, as the user image is resized following the user's preferences
+            mMsg.parts.push(await _convertImageRefToInlineImageOrThrow(srcFragment.part, false));
+            break;
+          case 'doc':
+            mMsg.parts.push(srcFragment.part);
+            break;
+          default:
+            console.warn('historyToChatGenerateRequest: unexpected user fragment part type', srcFragment.part);
+        }
+        return mMsg;
+      }, Promise.resolve({ role: 'user', parts: [] } as AixChatMessageUser));
+
+    } else if (m.role === 'assistant') {
 
       aixChatMessage = await m.fragments.reduce(async (mMsgPromise, srcFragment) => {
         const mMsg = await mMsgPromise;
@@ -48,8 +79,10 @@ export async function conversationMessagesToAixGenerateRequest(messageSequence: 
             mMsg.parts.push(srcFragment.part);
             break;
           case 'image_ref':
-            const assistantInlineImage = await _convertImageRefToInlineImageOrThrow(srcFragment.part);
-            mMsg.parts.push(assistantInlineImage);
+            // TODO: rescale shall be dependent on the LLM here - and be careful with the high-res options, as they can
+            //  be really space consuming. how to choose between high and low? global option?
+            const resizeMode: LLMImageResizeMode = 'openai-low-res';
+            mMsg.parts.push(await _convertImageRefToInlineImageOrThrow(srcFragment.part, resizeMode));
             break;
           default:
             console.warn('historyToChatGenerateRequest: unexpected assistant fragment part type', srcFragment.part);
@@ -57,29 +90,6 @@ export async function conversationMessagesToAixGenerateRequest(messageSequence: 
         }
         return mMsg;
       }, Promise.resolve({ role: 'model', parts: [] } as AixChatMessageModel));
-
-    } else if (m.role === 'user') {
-
-      aixChatMessage = await m.fragments.reduce(async (mMsgPromise, srcFragment) => {
-        const mMsg = await mMsgPromise;
-        if (!isContentOrAttachmentFragment(srcFragment))
-          return mMsg;
-        switch (srcFragment.part.pt) {
-          case 'text':
-            mMsg.parts.push(srcFragment.part);
-            break;
-          case 'image_ref':
-            const inlineImage = await _convertImageRefToInlineImageOrThrow(srcFragment.part);
-            mMsg.parts.push(inlineImage);
-            break;
-          case 'doc':
-            mMsg.parts.push(srcFragment.part);
-            break;
-          default:
-            console.warn('historyToChatGenerateRequest: unexpected user fragment part type', srcFragment.part);
-        }
-        return mMsg;
-      }, Promise.resolve({ role: 'user', parts: [] } as AixChatMessageUser));
 
     } else {
       // TODO: impement mid-chat system messages
@@ -91,22 +101,31 @@ export async function conversationMessagesToAixGenerateRequest(messageSequence: 
   }, Promise.resolve({ chatSequence: [] } as AixChatContentGenerateRequest));
 }
 
-async function _convertImageRefToInlineImageOrThrow(imageRefPart: DMessageImageRefPart) {
-  console.log('DEV: convertImageRefToInlineImage:', imageRefPart);
+async function _convertImageRefToInlineImageOrThrow(imageRefPart: DMessageImageRefPart, resizeMode: LLMImageResizeMode | false) {
 
   // validate
   const { dataRef } = imageRefPart;
   if (dataRef.reftype !== 'dblob' || !('dblobAssetId' in dataRef)) {
-    console.warn('convertImageRefToInlineImage: unexpected data ref', dataRef);
-    throw new Error('Unexpected data ref');
+    console.warn('Image reference is not supported', imageRefPart);
+    throw new Error('Image reference is not supported');
   }
 
   // get image asset
   const imageAsset = await getImageAsset(dataRef.dblobAssetId);
   if (!imageAsset) {
-    console.warn('convertImageRefToInlineImage: missing image asset', dataRef);
-    throw new Error('Missing image asset');
+    console.warn('Image asset not found', imageRefPart);
+    throw new Error('Image asset not found');
   }
 
-  return createAixInlineImagePart(imageAsset.data.base64, dataRef.mimeType || imageAsset.data.mimeType);
+  // convert if requested
+  let { mimeType, base64: base64Data } = imageAsset.data;
+  if (resizeMode) {
+    const resizedData = await resizeBase64ImageIfNeeded(mimeType, base64Data, resizeMode, MODEL_IMAGE_RESCALE_MIMETYPE, MODEL_IMAGE_RESCALE_QUALITY).catch(() => null);
+    if (resizedData) {
+      base64Data = resizedData.base64;
+      mimeType = resizedData.mimeType as any;
+    }
+  }
+
+  return createAixInlineImagePart(base64Data, mimeType || dataRef.mimeType);
 }
