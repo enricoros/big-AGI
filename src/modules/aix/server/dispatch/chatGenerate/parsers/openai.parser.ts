8428d2af7f7d9547d65862ef80140b5ff5d63980
Enrico Ros <enrico.ros@gmail.com>
2024-07-15 03:45:52 -0700

    AIX: OpenAI basic FC algo
    Verified on: Azure, Groq, LMStudio, LocalAI, Mistral, OpenAI, TogetherAI
    FC not supported on: Deepseek, Perplexity
    FC poorly supported on: OpenRouter (basically only in OpenAI it works)

--- src/modules/aix/server/dispatch/chatGenerate/parsers/openai.parser.ts
+++ src/modules/aix/server/dispatch/chatGenerate/parsers/openai.parser.ts
@@ -10,6 +10,23 @@ export function createOpenAIMessageCreateParser(): ChatGenerateParseFunction {
   let hasWarned = false;
   // NOTE: could compute rate (tok/s) from the first textful event to the last (to ignore the prefill time)
 
+  // Supporting structure to accumulate the assistant message
+  const accumulator: {
+    content: string | null;
+    tool_calls: {
+      id: string;
+      type: 'function';
+      function: {
+        name: string;
+        arguments: string;
+      };
+    }[];
+  } = {
+    content: null,
+    tool_calls: [],
+  };
+
+
   return function* (eventData: string): Generator<ChatGenerateMessageAction> {
 
     // Throws on malformed event data
@@ -33,37 +50,92 @@ export function createOpenAIMessageCreateParser(): ChatGenerateParseFunction {
       console.log('/api/llms/stream: OpenAI dispatch warning:', json.warning);
     }
 
+    // [Azure] we seem to get 'prompt_annotations' or 'prompt_filter_results' objects - which we will ignore to suppress the error
+    if (json.id === '' && json.object === '' && json.model === '')
+      return;
+
+
     // -> Stats
-    if (json.usage && json.usage.completion_tokens)
-      yield { op: 'set', value: { stats: { chatInTokens: json.usage.prompt_tokens || -1, chatOutTokens: json.usage.completion_tokens } } };
+    if (json.usage) {
+      if (json.usage.completion_tokens !== undefined)
+        yield { op: 'set', value: { stats: { chatInTokens: json.usage.prompt_tokens || -1, chatOutTokens: json.usage.completion_tokens } } };
 
-    // expect: 1 completion, or stop
-    if (json.choices.length !== 1) {
-
-      // Usage objects will likely have an empty completion
-      if (json.usage)
+      // [OpenAI] Expected: the last object has usage, but an empty choices array
+      if (!json.choices.length)
         return;
-
-      // [Azure] we seem to get 'prompt_annotations' or 'prompt_filter_results' objects - which we will ignore to suppress the error
-      if (json.id === '' && json.object === '' && json.model === '')
-        return;
-
-      throw new Error(`expected 1 completion, got ${json.choices.length}`);
     }
 
-    // expect: index=0 (n: 1)
-    const index = json.choices[0].index;
-    if (index !== 0 && index !== undefined /* [OpenRouter->Gemini] */)
-      throw new Error(`expected completion index 0, got ${index}`);
+    // expect: 1 completion, or stop
+    if (json.choices.length !== 1)
+      throw new Error(`expected 1 completion, got ${json.choices.length}`);
 
-    // -> Text
-    const text = json.choices[0].delta?.content /*|| json.choices[0]?.text*/ || '';
-    if (text?.length)
-      yield { op: 'text', text };
+    for (const choice of json.choices) {
+
+      const { index, delta, finish_reason } = choice;
+
+      // n=1 -> single Choice only
+      if (index !== 0 && index !== undefined /* [OpenRouter->Gemini] */)
+        throw new Error(`expected completion index 0, got ${choice.index}`);
+
+      // handle missing content
+      if (!delta)
+        throw new Error(`server response missing content (finish_reason: ${finish_reason})`);
+
+      // delta: Text
+      if (typeof delta.content === 'string') {
+
+        const text = delta.content;
+        accumulator.content = (accumulator.content || '') + text;
+        yield { op: 'text', text };
+
+      }
+
+      // delta: Tool Calls
+      if (delta.tool_calls?.length) {
+        for (const deltaToolCall of delta.tool_calls) {
+
+          // validation
+          if (deltaToolCall.type !== undefined && deltaToolCall.type !== 'function')
+            throw new Error(`unexpected tool_call type: ${deltaToolCall.type}`);
+
+          // Ensure the tool call exists in our accumulated structure
+          const tcIndex = deltaToolCall.index ?? accumulator.tool_calls.length;
+          let created = false;
+          if (!accumulator.tool_calls[tcIndex]) {
+            accumulator.tool_calls[tcIndex] = {
+              id: deltaToolCall.id || '',
+              type: 'function',
+              function: { name: deltaToolCall.function.name || '', arguments: '' },
+            };
+            created = true;
+          }
+          const accumulatedToolCall = accumulator.tool_calls[tcIndex];
+
+          if (!created) {
+            if (deltaToolCall.id && deltaToolCall.id !== accumulatedToolCall.id)
+              throw new Error(`unexpected tool_call id: ${deltaToolCall.id}`);
+            if (deltaToolCall.function.name)
+              throw new Error(`unexpected tool_call function name: ${deltaToolCall.function.name}`);
+          }
+
+          // Update the accumulated tool call
+          // if (deltaToolCall.id)
+          //   accumulatedToolCall.id = deltaToolCall.id;
+          // if (deltaToolCall.function?.name)
+          //   accumulatedToolCall.function.name +=;
+          if (deltaToolCall.function?.arguments)
+            accumulatedToolCall.function.arguments += deltaToolCall.function.arguments;
+
+          // assume it's a function, we have the index, and we may have an id for this tool call
+          console.log('atc', JSON.stringify(accumulator, null, 2));
+        }
+      }
+    }
 
     // Note: not needed anymore - Workaround for implementations that don't send the [DONE] event
     // use the finish_reason to close the parser
     // if (json.choices[0].finish_reason)
     //   return yield { op: 'parser-close' };
+
   };
 }
