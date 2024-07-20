2b573eab4458d161753beea9197529f71d4525a8
Enrico Ros <enrico.ros@gmail.com>
2024-07-20 15:36:42 -0700

    AIX: PartTransmitter update

--- src/modules/aix/server/api/PartTransmitter.ts
+++ src/modules/aix/server/api/PartTransmitter.ts
@@ -9,37 +9,19 @@ export const IssueSymbols = {
 };
 
 
-// export type ChatGenerateMessageAction = {
-//   op: 'text',
-//   text: string;
-// } | {
-//   op: 'issue';
-//   issue: string;
-//   symbol: string;
-// } | {
-//   op: 'parser-close';
-// } | {
-//   op: 'set';
-//   value: {
-//     model?: string;
-//     stats?: {
-//       chatInTokens?: number; // -1: unknown
-//       chatOutTokens: number;
-//       chatOutRate?: number;
-//       timeInner?: number;
-//       timeOuter?: number;
-//     }
-//   };
-// };
-//
-// export type ChatGenerateParseFunction = (eventData: string, eventName?: string) => Generator<ChatGenerateMessageAction>;
-
-
+// TODO: fully implement this class
+// Needs to:
+// - serialize the operations into a buffer that can be then yielded to the client (ideally via some Generator pattern?) which will allow PartReassembler to reassemble into AixWire_Parts objects
+// - verify the consistency of the operations via some basic state machinery
+//   - throw on state issues
+// - implement the actual operations
 export class PartTransmitter {
 
   constructor(private readonly prettyDialect: string) {
   }
 
+
+  /// Control plane
 
   terminateParser(reasonId: 'message-stop' | 'dialect-issue') {
     // return yield { op: 'parser-close' };
@@ -56,44 +38,64 @@ export class PartTransmitter {
     this.terminateParser('dialect-issue');
   }
 
-
-  setModelName(modelName: string) {
-    console.log('setModelName', modelName);
+  flush() {
+    console.log('flush');
   }
 
+  /// Parts data
+
+  // Appends text, creating a part if missing [throttled]
   appendText(text: string) {
     console.log('appendText', text);
     // yield { op: 'text', text: ` ${TEXT_SYMBOL_MAX_TOKENS}` /* Interrupted: MAX_TOKENS reached */ };
   }
 
-
+  /**
+   * Creates a FC part, flushing the previous one if needed, and starts adding data to it
+   * @param id if null [Gemini], a new id will be generated to keep it linked to future tool responses
+   * @param functionName required.
+   * @param expectedArgsFmt 'incr_str' | 'json_object' - 'incr_str' for incremental string, 'json_object' for JSON object
+   * @param args must be undefined, or match the expected Args Format
+   */
   startFunctionToolCall(id: string | null, functionName: string, expectedArgsFmt: 'incr_str' | 'json_object', args?: string | object | null) {
     // NOTE the object(!) returned at least by anthropic and gemini when complete
     if (!id) id = serverSideId('aix-tool-call-id');
     console.log('FC', id, functionName, args);
   }
 
+  // Appends data to a FC part [throttled]
   appendFunctionToolCallArgsIStr(id: string | null, argsJson: string) {
     console.log('FC+', id, argsJson);
   }
 
-
+  // Creates a CE request part, flushing the previous one if needed, and completes it
   addCodeExecutionToolCall(id: string | null, language: string, code: string) {
     if (!id) id = serverSideId('aix-tool-call-id');
     console.log('CE_Req', id, language, code);
     this.endPart();
   }
 
+  // Creates a CE result part, flushing the previous one if needed, and completes it
   addCodeExecutionResponse(id: string | null, output: string, error: string | undefined) {
     if (!id) id = serverSideId('aix-tool-response-id');
     console.log('CE_Resp', id, output, error);
     this.endPart();
   }
 
+  // Closes the current part, flushing it out
   endPart() {
     console.log('endPart');
   }
 
+
+  /// More data
+
+  // Sent right away
+  setModelName(modelName: string) {
+    console.log('setModelName', modelName);
+  }
+
+  // Sent right away and on completion, if there have been updates
   setCounters(counts: {
     chatIn?: number,
     chatOut?: number,
@@ -103,4 +105,5 @@ export class PartTransmitter {
   }) {
     console.log('setTokenCounts', counts);
   }
+
 }
