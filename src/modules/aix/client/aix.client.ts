ee57c5f78fb3a72dc47a3a978cf090d2f29c65ac
Enrico Ros <enrico.ros@gmail.com>
2024-07-24 05:44:45 -0700

    FF: snap tools

--- src/modules/aix/client/aix.client.ts
+++ src/modules/aix/client/aix.client.ts
@@ -102,42 +102,50 @@ async function _aixChatGenerateContent(
   onUpdate: (update: StreamingClientUpdate, done: boolean) => void,
 ): Promise<void> {
 
+  const sampleFC: boolean = false;
+  const sampleCE: boolean = true;
 
-  aixChatGenerate.tools = [
-    {
-      type: 'function_call',
-      function_call: {
-        name: 'get_capybara_info_given_name_and_color_very_long',
-        description: 'Get the info about capybaras. Call one each per name.',
-        input_schema: {
-          properties: {
-            'name': {
-              type: 'string',
-              description: 'The name of the capybara',
-              enum: ['enrico', 'coolio'],
+  if (sampleFC) {
+    aixChatGenerate.tools = [
+      {
+        type: 'function_call',
+        function_call: {
+          name: 'get_capybara_info_given_name_and_color_very_long',
+          description: 'Get the info about capybaras. Call one each per name.',
+          input_schema: {
+            properties: {
+              'name': {
+                type: 'string',
+                description: 'The name of the capybara',
+                enum: ['enrico', 'coolio'],
+              },
+              'color': {
+                type: 'string',
+                description: 'The color of the capybara. Mandatory!!',
+              },
+              // 'story': {
+              //   type: 'string',
+              //   description: 'A fantastic story about the capybara. Please 10 characters maximum.',
+              // },
             },
-            'color': {
-              type: 'string',
-              description: 'The color of the capybara. Mandatory!!',
-            },
-            // 'story': {
-            //   type: 'string',
-            //   description: 'A fantastic story about the capybara. Please 10 characters maximum.',
-            // },
+            required: ['name'],
           },
-          required: ['name'],
         },
       },
-    },
-    // {
-    //   type: 'code_execution',
-    //   variant: 'gemini_auto_inline',
-    // },
-  ];
+    ];
+  }
+  if (sampleCE) {
+    aixChatGenerate.tools = [
+      {
+        type: 'code_execution',
+        variant: 'gemini_auto_inline',
+      },
+    ];
+  }
 
 
   const operation = await apiStream.aix.chatGenerateContent.mutate(
-    { access: aixAccess, model: aixModel, chatGenerate: aixChatGenerate, context: aixContext, streaming: false, connectionOptions: getLabsDevMode() ? { debugDispatchRequestbody: true } : undefined },
+    { access: aixAccess, model: aixModel, chatGenerate: aixChatGenerate, context: aixContext, streaming: true, connectionOptions: getLabsDevMode() ? { debugDispatchRequestbody: true } : undefined },
     { signal: abortSignal },
   );
 
