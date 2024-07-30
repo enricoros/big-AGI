8e1167d848c2aab4b4743bc7f13edb26e5bb386c
Enrico Ros <enrico.ros@gmail.com>
2024-07-30 02:52:42 -0700

    Remove debug hook.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,9 @@ import * as React from 'react';
 import { AppChat } from '../src/apps/chat/AppChat';
 
 import { withNextJSPerPageLayout } from '~/common/layout/withLayout';
-import { useDebugHook } from '~/common/components/useDebugHook';
 
 
 export default withNextJSPerPageLayout({ type: 'optima' }, () => {
-
-  useDebugHook('IndexPage');
 
   // TODO: This Index page will point to the Dashboard (or a landing page)
   // For now it offers the chat experience, but this will change. #299
