b99e956b0bfcf1eccd0c68bad904bcd7b8f34883
Enrico Ros <enrico.ros@gmail.com>
2024-08-07 00:39:58 -0700

    useAgiAttachmentPrompts: just. perfect.

--- src/apps/chat/components/composer/llmattachments/LLMAttachmentsPromptsButton.tsx
+++ src/apps/chat/components/composer/llmattachments/LLMAttachmentsPromptsButton.tsx
@@ -5,6 +5,7 @@ import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
 
 import type { AgiAttachmentPromptsData } from '~/modules/aifn/attachmentprompts/useAgiAttachmentPrompts';
 import { SxProps } from '@mui/joy/styles/types';
+import { AGI_SUGGESTIONS_COLOR } from '../textarea/ComposerTextAreaActions';
 
 
 export const LLMAttachmentsPromptsButtonMemo = React.memo(LLMAttachmentsPromptsButton);
@@ -13,8 +14,10 @@ export const LLMAttachmentsPromptsButtonMemo = React.memo(LLMAttachmentsPromptsB
 const promptGenIconButtonSx: SxProps = {
   minWidth: 40,
   backgroundColor: 'background.level1',
-  boxShadow: 'inset 0 4px 6px -4px rgb(var(--joy-palette-primary-darkChannel) / 40%)',
+  boxShadow: `inset 0 4px 6px -4px rgb(var(--joy-palette-${AGI_SUGGESTIONS_COLOR}-darkChannel) / 40%)`,
   borderRadius: '2rem',
+  borderBottomLeftRadius: 0,
+  borderColor: `${AGI_SUGGESTIONS_COLOR}.outlinedColor`,
   // '&:hover': {
   //   backgroundColor: 'background.level1',
   // },
@@ -37,7 +40,7 @@ function LLMAttachmentsPromptsButton({ data }: { data: AgiAttachmentPromptsData 
   const button = (
     <IconButton
       variant={data.error ? 'soft' : data.hasData ? 'outlined' : 'soft'}
-      color={data.error ? 'danger' : data.hasData ? 'success' : 'success'}
+      color={data.error ? 'danger' : data.hasData ? AGI_SUGGESTIONS_COLOR : AGI_SUGGESTIONS_COLOR}
       size='sm'
       disabled={data.isFetching}
       onClick={data.refetch}
@@ -52,7 +55,7 @@ function LLMAttachmentsPromptsButton({ data }: { data: AgiAttachmentPromptsData 
   );
 
   return !tooltipTitle ? button : (
-    <Tooltip variant='outlined' disableInteractive placement='top' title={tooltipTitle}>
+    <Tooltip variant='outlined' disableInteractive placement='top-end' arrow title={tooltipTitle}>
       {button}
     </Tooltip>
   );
