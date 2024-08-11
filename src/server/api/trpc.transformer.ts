d9f8ed6d2b98e32d8b82548570d1ca2a79729e13
Enrico Ros <enrico.ros@gmail.com>
2024-08-10 17:16:59 -0700

    SuperJSON: The NextJS-immutability hack doesn't seem to be needed anymore.
    With this hack, deserialization of `json` arrays (rather than objects) was also broken,
    and would break the downstream tRPC streaming functionality.

--- src/server/api/trpc.transformer.ts
+++ src/server/api/trpc.transformer.ts
@@ -6,45 +6,48 @@
 import superjson from 'superjson';
 
 /**
- * WORKAROUND: an incredibly weird bug with NextJS-14.2.4 + tRPC v11 + SuperJSON
- *
- * If the serialized object has 'undefined' values (e.g. {json.name: null, meta.values.name=['undefined']},
- * superjson will try to apply the format conversion from the 'null' to the 'undefined' value directly to the input object.
- *
- * This somehow fails with tRPC-11 and NextJS-14.2.4. There may be immutability somewhere.
- *
- * While a slot workaround would be to deepcopy (JSON.parse(JSON.stringify(object)) the object
- * we use a more lightweight approach that only works on top-level properties.
- *
- * May be related to https://github.com/blitz-js/superjson/issues/242 ?
- * May be related to https://github.com/blitz-js/superjson/issues/283 ?
+ * NOTE: 2024-08-10: this doesn't seem to be needed anymore, as such we use plain `superjson` as-is.
  */
-function deserializeWithWorkaround<T = unknown>(object: any): T {
+export const transformer = superjson;
 
-  // Second-level (object.json: {..}) mutability workaround
-  if (object && object instanceof Object && object?.meta && object?.json) {
-    console.log('deserializeWithWorkaround', object, superjson.deserialize(object));
-    // object = { ...object, json: { ...object.json } };
-    // console.log('deserializeWithWorkaround', object, superjson.deserialize(object));
-  }
-
-  return superjson.deserialize<T>(object);
-}
-
-
-export const transformer = {
-  serialize: superjson.serialize,
-  deserialize: deserializeWithWorkaround,
-
-  // The following commented code is here just to let us place breakpoints quickly
-
-  // serialize: (object: any): any => {
-  //   const serialized = superjson.serialize(object);
-  //   return serialized;
-  // }
-
-  // deserialize: (object: any): any => {
-  //   const des = superjson.deserialize(object);
-  //   return des;
-  // }
-};
+// /**
+//  * WORKAROUND: an incredibly weird bug with NextJS-14.2.4 + tRPC v11 + SuperJSON
+//  *
+//  * If the serialized object has 'undefined' values (e.g. {json.name: null, meta.values.name=['undefined']},
+//  * superjson will try to apply the format conversion from the 'null' to the 'undefined' value directly to the input object.
+//  *
+//  * This somehow fails with tRPC-11 and NextJS-14.2.4. There may be immutability somewhere.
+//  *
+//  * While a slot workaround would be to deepcopy (JSON.parse(JSON.stringify(object)) the object
+//  * we use a more lightweight approach that only works on top-level properties.
+//  *
+//  * May be related to https://github.com/blitz-js/superjson/issues/242 ?
+//  * May be related to https://github.com/blitz-js/superjson/issues/283 ?
+//  */
+// function deserializeWithWorkaround<T = unknown>(object: any): T {
+//
+//   // Second-level (object.json: {..}) mutability workaround
+//   if (object && object instanceof Object && object?.meta && object?.json) {
+//     object = { ...object, json: { ...object.json } };
+//   }
+//
+//   return superjson.deserialize<T>(object);
+// }
+//
+//
+// export const transformer = {
+//   serialize: superjson.serialize,
+//   deserialize: deserializeWithWorkaround,
+//
+//   // The following commented code is here just to let us place breakpoints quickly
+//
+//   // serialize: (object: any): any => {
+//   //   const serialized = superjson.serialize(object);
+//   //   return serialized;
+//   // }
+//
+//   // deserialize: (object: any): any => {
+//   //   const des = superjson.deserialize(object);
+//   //   return des;
+//   // }
+// };
