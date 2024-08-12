395d977e9773471c02a58d962d77626e8675b8b6
Enrico Ros <enrico.ros@gmail.com>
2024-08-12 11:42:39 -0700

    EnhancedRenderCode: expand/collapse all

--- src/modules/blocks/code/EnhancedRenderCodeMenu.tsx
+++ src/modules/blocks/code/EnhancedRenderCodeMenu.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
-import { ListItemDecorator, MenuItem } from '@mui/joy';
+import { ListDivider, ListItem, ListItemDecorator, MenuItem, Typography } from '@mui/joy';
 import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
 
 import { CloseableMenu } from '~/common/components/CloseableMenu';
@@ -9,11 +9,16 @@ import { ConfirmationModal } from '~/common/components/ConfirmationModal';
 import { useOverlayComponents } from '~/common/layout/overlays/useOverlayComponents';
 import { useUXLabsStore } from '~/common/state/store-ux-labs';
 
+import { getCodeCollapseManager } from './codeCollapseManager';
+
 
 /**
  * Small hidden context menu to toggle the code enhancer, globally.
  */
-export function EnhancedRenderCodeMenu(props: { anchor: HTMLElement, onClose: () => void }) {
+export function EnhancedRenderCodeMenu(props: {
+  anchor: HTMLElement,
+  onClose: () => void,
+}) {
 
   // state
   const { showPromisedOverlay } = useOverlayComponents();
@@ -21,6 +26,17 @@ export function EnhancedRenderCodeMenu(props: { anchor: HTMLElement, onClose: ()
     labsEnhanceCodeBlocks: state.labsEnhanceCodeBlocks,
     setLabsEnhanceCodeBlocks: state.setLabsEnhanceCodeBlocks,
   })));
+
+
+  // handlers
+
+  const handleCollapseAllCodeBlocks = React.useCallback(() => {
+    getCodeCollapseManager().triggerCollapseAll(true);
+  }, []);
+
+  const handleExpandAllCodeBlocks = React.useCallback(() => {
+    getCodeCollapseManager().triggerCollapseAll(false);
+  }, []);
 
   const toggleEnhanceCodeBlocks = React.useCallback(() => {
     // turn blocks on (may not even be called, ever)
@@ -32,23 +48,41 @@ export function EnhancedRenderCodeMenu(props: { anchor: HTMLElement, onClose: ()
     showPromisedOverlay('blocks-off-enhance-code', {}, ({ onResolve, onUserReject }) =>
       <ConfirmationModal
         open onClose={onUserReject} onPositive={() => onResolve(true)}
-        confirmationText='Turn off automatic code enhancements? This will disable LiveFile functionality. You can turn it back on anytime by going to Settings > Labs > Enhance Legacy Code.'
+        title='Turn off enhanced code blocks?'
+        confirmationText='This will disable LiveFile functionality. You can turn it back on anytime by going to Settings > Labs > Enhance Legacy Code.'
         positiveActionText='Turn Off'
       />,
     ).then(() => setLabsEnhanceCodeBlocks(false)).catch(() => null /* ignore closure */);
   }, [labsEnhanceCodeBlocks, setLabsEnhanceCodeBlocks, showPromisedOverlay]);
 
+
   return (
     <CloseableMenu
       dense
       open anchorEl={props.anchor} onClose={props.onClose}
-      sx={{ minWidth: 240 }}
+      sx={{ minWidth: 250 }}
     >
+
+      <ListItem>
+        <Typography level='body-sm'>Code Blocks</Typography>
+      </ListItem>
+
+      <MenuItem onClick={handleExpandAllCodeBlocks}>
+        <ListItemDecorator />
+        Expand All
+      </MenuItem>
+
+      <MenuItem onClick={handleCollapseAllCodeBlocks}>
+        <ListItemDecorator />
+        Collapse All
+      </MenuItem>
+
+      <ListDivider />
 
       {/* A mix in between UxLabsSettings (labsEnhanceCodeBlocks) and the ChatDrawer MenuItems */}
       <MenuItem onClick={toggleEnhanceCodeBlocks}>
         <ListItemDecorator>{labsEnhanceCodeBlocks && <CheckRoundedIcon />}</ListItemDecorator>
-        Enhance Legacy Code
+        Enhanced Code Blocks
       </MenuItem>
 
     </CloseableMenu>
