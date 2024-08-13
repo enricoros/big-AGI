a105efe3bdcfe1a8225c5771cb31769afa98081b
Enrico Ros <enrico.ros@gmail.com>
2024-08-13 01:46:24 -0700

    EnhancedRenderCode x LiveFileSync: extract WorkspaceLiveFilePicker

--- src/modules/blocks/enhanced-code/livefile-patch/useLiveFilePatch.tsx
+++ src/modules/blocks/enhanced-code/livefile-patch/useLiveFilePatch.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
-import { Box, Dropdown, IconButton, ListDivider, ListItemDecorator, Menu, MenuButton, MenuItem, Typography } from '@mui/joy';
-import CodeIcon from '@mui/icons-material/Code';
+import { Typography } from '@mui/joy';
 
 import { useContextWorkspaceId } from '~/common/stores/workspace/WorkspaceIdProvider';
 import { useWorkspaceContentsMetadata } from '~/common/stores/workspace/useWorkspaceContentsMetadata';
 
-import type { LiveFileId, LiveFileMetadata } from '~/common/livefile/liveFile.types';
-import { LiveFilePatchIcon } from '~/common/components/icons/LiveFilePatchIcon';
+import type { LiveFileId } from '~/common/livefile/liveFile.types';
 import { isLiveFileSupported } from '~/common/livefile/store-live-file';
 import { useUXLabsStore } from '~/common/state/store-ux-labs';
+
+import { WorkspaceLiveFilePicker } from './WorkspaceLiveFilePicker';
 
 
 export function useLiveFilePatch(title: string, code: string, isPartial: boolean, isMobile: boolean) {
@@ -25,19 +25,6 @@ export function useLiveFilePatch(title: string, code: string, isPartial: boolean
   const workspaceId = useContextWorkspaceId();
   const { liveFilesMetadata } = useWorkspaceContentsMetadata(isEnabled ? workspaceId : null);
 
-  // [effect] auto-select a LiveFileId
-  React.useEffect(() => {
-    if (liveFilesMetadata.length === 1) {
-      // auto-select the only LiveFile
-      setLiveFileId(liveFilesMetadata[0].id);
-    } else {
-      // auto-select matching the title
-      const lfm = liveFilesMetadata.find(lfm => lfm.name === title);
-      if (lfm)
-        setLiveFileId(lfm.id);
-    }
-  }, [isEnabled, liveFilesMetadata, title]);
-
 
   // reset enablement if no live files
   if (!liveFilesMetadata?.length)
@@ -51,60 +38,14 @@ export function useLiveFilePatch(title: string, code: string, isPartial: boolean
 
   const button = React.useMemo(() => !isEnabled ? null : <>
 
+    <WorkspaceLiveFilePicker
+      autoSelectName={title}
+      enabled={isEnabled}
+      liveFileId={liveFileId}
+      onSelectLiveFile={setLiveFileId}
+    />
 
-    {/* LiveFile selector */}
-    <Dropdown>
-
-      {/*<TooltipOutlined color='success' title='Select a LiveFile to patch'>*/}
-      <MenuButton
-        aria-label='Pair File'
-        slots={{ root: IconButton }}
-        slotProps={{
-          root: {
-            color: liveFileId ? 'success' : undefined,
-            variant: liveFileId ? undefined : undefined,
-            size: 'sm',
-          },
-        }}
-        sx={{
-          ml: 'auto',
-        }}
-      >
-        <LiveFilePatchIcon />
-      </MenuButton>
-      {/*</TooltipOutlined>*/}
-
-      <Menu
-        placement='bottom-start'
-        sx={{
-          minWidth: 200,
-          zIndex: 'var(--joy-zIndex-modal)', /* on top of its own modal in FS */
-        }}
-      >
-        {/*<ListItem>*/}
-        {/*  <Typography level='body-sm'>Recent Workspace Files</Typography>*/}
-        {/*</ListItem>*/}
-
-        {liveFilesMetadata.map((lfm: LiveFileMetadata) => (
-          <MenuItem key={lfm.id} onClick={() => setLiveFileId(lfm.id)}>
-            <ListItemDecorator><CodeIcon sx={{ fontSize: 'lg' }} /></ListItemDecorator>
-            <Box>
-              {lfm.name}
-              <Typography level='body-xs'>{lfm.size?.toLocaleString() || '(unknown)'} bytes {lfm.type ? `· ${lfm.type}` : ''}</Typography>
-            </Box>
-          </MenuItem>
-        ))}
-
-        <ListDivider />
-
-        <MenuItem disabled={!liveFileId} onClick={() => setLiveFileId(null)}>
-          <ListItemDecorator />
-          Remove Pairing
-        </MenuItem>
-      </Menu>
-    </Dropdown>
-
-  </>, [isEnabled, liveFileId, liveFilesMetadata]);
+  </>, [isEnabled, liveFileId, title]);
 
 
   const actionBar = React.useMemo(() => (!isEnabled || !liveFilesMetadata || true) ? null : (
