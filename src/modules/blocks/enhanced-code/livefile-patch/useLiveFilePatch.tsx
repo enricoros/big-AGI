1f95aef89e5882da54786d9f012dade2805a9d9c
Enrico Ros <enrico.ros@gmail.com>
2024-08-14 00:06:12 -0700

    ERC x LFS: pick an additional file

--- src/modules/blocks/enhanced-code/livefile-patch/useLiveFilePatch.tsx
+++ src/modules/blocks/enhanced-code/livefile-patch/useLiveFilePatch.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
+import { fileOpen } from 'browser-fs-access';
 
 import { Box, Button, Typography } from '@mui/joy';
 
-import type { LiveFileId } from '~/common/livefile/liveFile.types';
-import { isLiveFileSupported, liveFileCreateOrThrow } from '~/common/livefile/store-live-file';
 import { useUXLabsStore } from '~/common/state/store-ux-labs';
 
+// Workspace
+import type { DWorkspaceId } from '~/common/stores/workspace/workspace.types';
 import { WorkspaceLiveFilePicker } from '~/common/stores/workspace/WorkspaceLiveFilePicker';
+import { workspaceActions } from '~/common/stores/workspace/store-client-workspace';
+
+// LiveFile
+import type { LiveFileId } from '~/common/livefile/liveFile.types';
+import { isLiveFileSupported, liveFileCreateOrThrow } from '~/common/livefile/store-live-file';
 
 
 export function useLiveFilePatch(title: string, code: string, isPartial: boolean, isMobile: boolean) {
@@ -26,18 +32,35 @@ export function useLiveFilePatch(title: string, code: string, isPartial: boolean
     setLiveFileId(id);
   }, []);
 
-  const handleSelectNewFile = React.useCallback(async () => {
+  const handleSelectNewFile = React.useCallback(async (workspaceId: DWorkspaceId | null) => {
+    // pick a file
+    const fileWithHandle = await fileOpen({ description: 'Insert into file...' }).catch(() => null /* The User closed the files picker */);
+    if (!fileWithHandle)
+      return;
+    const fileSystemFileHandle = fileWithHandle.handle;
+    if (!fileSystemFileHandle) {
+      // setStatus({ message: `Browser does not support LiveFile operations. ${isLiveFileSupported() ? 'No filesystem handles.' : ''}`, mtype: 'error' });
+      return;
+    }
+
     try {
-      const fileHandle = await window.showSaveFilePicker({
-        suggestedName: title,
-        // types: [{ description: 'Text Files', accept: { 'text/plain': ['.txt'] } }],
-      });
-      const newLiveFileId = await liveFileCreateOrThrow(fileHandle);
+      const newLiveFileId = await liveFileCreateOrThrow(fileSystemFileHandle);
       setLiveFileId(newLiveFileId);
+
+      // Pair the file with the workspace
+      if (!workspaceId)
+        console.warn('[DEV] No workspaceId to pair the file with.');
+      else
+        workspaceActions().liveFileAssign(workspaceId, newLiveFileId);
+
+      // TODO: Implement file content writing logic here
+      // For example:
+      // await liveFileContentWriteAndReload(code);
+
     } catch (error) {
       console.error('Error creating new file:', error);
     }
-  }, [title]);
+  }, []);
 
 
   // components
@@ -72,7 +95,7 @@ export function useLiveFilePatch(title: string, code: string, isPartial: boolean
       />
 
     </Box>
-  ), [handleLiveFileSelected, isEnabled, liveFileId, title]);
+  ), [handleLiveFileSelected, handleSelectNewFile, isEnabled, liveFileId, title]);
 
 
   const actionBar = React.useMemo(() => (!isEnabled || !liveFileId || true) ? null : (
