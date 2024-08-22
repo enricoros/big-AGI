a70e31e199af6c1aed7212e78672af1f0b465888
Enrico Ros <enrico.ros@gmail.com>
2024-08-21 23:01:02 -0700

    Fix Fred's demo

--- src/modules/youtube/youtube.utils.ts
+++ src/modules/youtube/youtube.utils.ts
@@ -1,5 +1,5 @@
 export function extractYoutubeVideoIDFromURL(videoURL: string): string | null {
-  const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/)|youtu\.be\/)([^#&?]*).*/;
+  const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/)|youtu\.be\/)([\w-]{11})(?:\S+)?$/;
   const match = videoURL.match(regExp);
-  return (match && match[1]?.length == 11) ? match[1] : null;
+  return match ? match[1] : null;
 }
