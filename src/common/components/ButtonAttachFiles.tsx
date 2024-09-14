80a5cca30e930e16bd42c6201f43c59338994463
Enrico Ros <enrico.ros@gmail.com>
2024-09-13 17:02:13 -0700

    Attach: File

--- src/common/components/ButtonAttachFiles.tsx
+++ src/common/components/ButtonAttachFiles.tsx
@@ -4,12 +4,35 @@ import { fileOpen, FileWithHandle } from 'browser-fs-access';
 import { Box, Button, IconButton, Tooltip } from '@mui/joy';
 import AttachFileRoundedIcon from '@mui/icons-material/AttachFileRounded';
 
+import { KeyStroke } from '~/common/components/KeyStroke';
+
 
 const attachFileLegend =
   <Box sx={{ px: 1, py: 0.75, lineHeight: '1.5rem' }}>
     <b>Attach files</b><br />
     Drag & drop in chat for faster loads ⚡
+    <KeyStroke combo='Ctrl + Shift + F' sx={{ mt: 1, mb: 0.5 }} />
   </Box>;
+
+
+export async function openFileForAttaching(
+  multiple: boolean,
+  onAttachFiles: (files: FileWithHandle[]) => void,
+) {
+  try {
+    const selectedFiles = await fileOpen({ multiple });
+    if (selectedFiles) {
+      if (Array.isArray(selectedFiles)) {
+        if (selectedFiles.length)
+          onAttachFiles(selectedFiles);
+      } else {
+        onAttachFiles([selectedFiles]);
+      }
+    }
+  } catch (error) {
+    // ignore...
+  }
+}
 
 
 export const ButtonAttachFilesMemo = React.memo(ButtonAttachFiles);
@@ -18,22 +41,7 @@ function ButtonAttachFiles(props: { isMobile?: boolean, fullWidth?: boolean, mul
 
   const { onAttachFiles } = props;
 
-  const handleAttachFilePicker = React.useCallback(async () => {
-    try {
-      const selectedFiles = await fileOpen({ multiple: props.multiple });
-      if (selectedFiles) {
-        if (Array.isArray(selectedFiles)) {
-          if (selectedFiles.length)
-            onAttachFiles(selectedFiles);
-        } else {
-          onAttachFiles([selectedFiles]);
-        }
-      }
-    } catch (error) {
-      // ignore...
-    }
-  }, [onAttachFiles, props.multiple]);
-
+  const handleAttachFilePicker = React.useCallback(() => openFileForAttaching(props.multiple || false, onAttachFiles), [onAttachFiles, props.multiple]);
 
   return props.isMobile ? (
     <IconButton onClick={handleAttachFilePicker}>
