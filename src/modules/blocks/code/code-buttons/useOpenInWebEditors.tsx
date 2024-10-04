8754bbada9ef20f125c04b3481bcbf7e2186ae0a
Enrico Ros <enrico.ros@gmail.com>
2024-10-04 11:22:13 -0700

    Will be replaced.

--- src/modules/blocks/code/code-buttons/useOpenInWebEditors.tsx
+++ src/modules/blocks/code/code-buttons/useOpenInWebEditors.tsx
@@ -7,7 +7,6 @@ import { GoogleColabIcon } from '~/common/components/icons/3rdparty/GoogleColabI
 import { JSFiddleIcon } from '~/common/components/icons/3rdparty/JSFiddleIcon';
 import { StackBlitzIcon } from '~/common/components/icons/3rdparty/StackBlitzIcon';
 
-import { BLOCK_CODE_VND_AGI_CHARTJS } from '../RenderCode';
 import { isCodePenSupported, openInCodePen } from './openInCodePen';
 import { isGoogleColabSupported, openInGoogleColab } from './openInGoogleColab';
 import { isJSFiddleSupported, openInJsFiddle } from './openInJsFiddle';
@@ -24,8 +23,6 @@ export function useOpenInWebEditors(
 ) {
   return React.useMemo(() => {
     if (blockIsPartial) return stableNoButtons;
-
-    if (blockTitle === BLOCK_CODE_VND_AGI_CHARTJS) return stableNoButtons;
 
     const mayExternal = code?.indexOf('\n') > 0;
     if (!mayExternal) return stableNoButtons;
