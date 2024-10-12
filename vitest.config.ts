838e9e562954aa1cb986ea51d31ae92c1ab30f94
Enrico Ros <enrico.ros@gmail.com>
2024-10-12 00:23:51 -0700

    Moved vitest to Dev.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [],
-  test: {},
+  test: {
+    // Your test-specific configurations can go here
+  },
 });
