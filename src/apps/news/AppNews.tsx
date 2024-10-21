c30cf5eeb64e647c670db8807e9b6b8017249aad
Enrico Ros <enrico.ros@gmail.com>
2024-10-21 14:26:29 -0700

    News: show build date

--- src/apps/news/AppNews.tsx
+++ src/apps/news/AppNews.tsx
@@ -9,6 +9,7 @@ import LaunchIcon from '@mui/icons-material/Launch';
 import { Brand } from '~/common/app.config';
 import { Link } from '~/common/components/Link';
 import { ROUTE_INDEX } from '~/common/app.routes';
+import { Release } from '~/common/app.release';
 import { animationColorBlues, animationColorRainbow } from '~/common/util/animUtils';
 import { capitalizeFirstLetter } from '~/common/util/textUtils';
 
@@ -21,6 +22,8 @@ import { bigAgi2NewsCallout } from './bigAgi2.data';
 const NEWS_INITIAL_COUNT = 3;
 const NEWS_LOAD_STEP = 2;
 
+
+const _frontendBuild = Release.buildInfo('frontend');
 
 export const newsRoadmapCallout =
   <Card variant='solid' invertedColors>
@@ -143,7 +146,9 @@ export function AppNews() {
                       </Box>
                     </Typography>
                     <Typography level='body-sm' sx={{ ml: 'auto' }}>
-                      {!!ni.versionDate && <TimeAgo date={ni.versionDate} />}
+                      {idx === 0 && _frontendBuild.timestamp
+                        ? <TimeAgo date={_frontendBuild.timestamp} />
+                        : !!ni.versionDate && <TimeAgo date={ni.versionDate} />}
                     </Typography>
                   </Box>
 
