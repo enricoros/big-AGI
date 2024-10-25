d7ed7a92842bfb50df154530dedae876a7e02496
Enrico Ros <enrico.ros@gmail.com>
2024-10-25 09:15:07 -0700

    Ctrl+Up/Down message navigation: improve 3

--- src/apps/settings-modal/ShortcutsModal.tsx
+++ src/apps/settings-modal/ShortcutsModal.tsx
@@ -28,8 +28,8 @@ const shortcutsMd = platformAwareKeystrokes(`
 | Ctrl + Shift + N | **New** chat                            |
 | Ctrl + Shift + X | **Reset** chat                          |
 | Ctrl + Shift + D | **Delete** chat                         |
-| Ctrl + Up        | Previous message                        |
-| Ctrl + Down      | Next message                            |
+| Ctrl + Up        | Previous message (hold shift for top)   |
+| Ctrl + Down      | Next message (hold shift to bottom)     |
 | Ctrl + [         | **Previous** chat (in history)          |
 | Ctrl + ]         | **Next** chat (in history)              |
 | **Settings**     |                                         |
