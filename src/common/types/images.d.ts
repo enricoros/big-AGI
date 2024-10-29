c21833dd16ff67621dfa5f1677107f5b1468ee6f
Enrico Ros <enrico.ros@gmail.com>
2024-10-28 19:23:56 -0700

    Revert: a52732d, a74893c, 070ad72, dfdaabb, 838e9e5 conflicts across branches.

--- src/common/types/images.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.png' {
-  const value: StaticImageData;
-  export default value;
-}
--- vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  plugins: [],
-  test: {
-    // Your test-specific configurations can go here
-  },
-});
