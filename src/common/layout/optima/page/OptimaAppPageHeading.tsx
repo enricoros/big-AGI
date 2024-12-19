078c6400ba987b0f355e4e9f2cb3e513d7347cd3
Enrico Ros <enrico.ros@gmail.com>
2024-12-19 02:21:40 -0800

    Update heading

--- src/common/layout/optima/page/OptimaAppPageHeading.tsx
+++ src/common/layout/optima/page/OptimaAppPageHeading.tsx
@@ -1,21 +1,24 @@
-import { Box, ListDivider, Typography } from '@mui/joy';
 import * as React from 'react';
+
+import { Box, ListDivider, Typography } from '@mui/joy';
+
 
 export function OptimaAppPageHeading(props: {
   title: React.ReactNode;
   tagline: React.ReactNode;
   accentedTagline?: boolean;
   startDecorator?: React.ReactNode;
+  noDivider?: boolean;
 }) {
   return (
-    <Box>
+    <Box mb={2.25}>
       {!!props.title && <Typography level='h2' sx={{ textAlign: 'start' }} startDecorator={props.startDecorator}>
         {props.title}
       </Typography>}
       {!!props.tagline && <Typography level='body-sm' sx={{ color: !props.accentedTagline ? undefined : 'text.secondary', textAlign: 'start', mt: 0.75 }}>
         {props.tagline}
       </Typography>}
-      <ListDivider sx={{ my: 2.25 }} />
+      {!props.noDivider && <ListDivider sx={{ mt: 2.25 }} />}
     </Box>
   );
 }
