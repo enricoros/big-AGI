e5981aac438e9a0b03f2ded6618c1bf23cbd286b
Enrico Ros <enrico.ros@gmail.com>
2024-12-21 18:03:16 -0800

    Optima page heading: add end deco

--- src/common/layout/optima/page/OptimaAppPageHeading.tsx
+++ src/common/layout/optima/page/OptimaAppPageHeading.tsx
@@ -8,11 +8,12 @@ export function OptimaAppPageHeading(props: {
   tagline?: React.ReactNode;
   accentedTagline?: boolean;
   startDecorator?: React.ReactNode;
+  endDecorator?: React.ReactNode;
   noDivider?: boolean;
 }) {
   return (
     <Box mb={2.25}>
-      {!!props.title && <Typography level='h2' sx={{ textAlign: 'start' }} startDecorator={props.startDecorator}>
+      {!!props.title && <Typography level='h2' sx={{ textAlign: 'start' }} startDecorator={props.startDecorator} endDecorator={props.endDecorator}>
         {props.title}
       </Typography>}
       {!!props.tagline && <Typography level='body-sm' sx={{ color: !props.accentedTagline ? undefined : 'text.secondary', textAlign: 'start', mt: 0.75 }}>
