e5e9f489d384097d2b7b43c5aa4ac2c19e30cc8a
Enrico Ros <enrico.ros@gmail.com>
2025-01-09 02:32:32 -0800

    DLLMs: Recyclable Parameters configurator

---
+++ src/modules/llms/models-modal/LLMOptionsGlobal.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+
+import type { DLLM } from '~/common/stores/llms/llms.types';
+import type { DModelParameterId, DModelParameterValues } from '~/common/stores/llms/llms.parameters';
+import { InlineError } from '~/common/components/InlineError';
+import { llmsStoreActions } from '~/common/stores/llms/store-llms';
+
+import { LLMParametersEditor } from './LLMParametersEditor';
+
+
+export function LLMOptionsGlobal(props: { llm: DLLM }) {
+
+  // derived input
+  const llm = props.llm;
+  const llmId = llm?.id ?? null;
+
+  // handlers
+
+  const handleChangeParameter = React.useCallback((partial: Partial<DModelParameterValues>) => {
+    llmsStoreActions().updateLLMUserParameters(llmId, partial);
+  }, [llmId]);
+
+  const handleRemoveParameter = React.useCallback((parameterId: DModelParameterId) => {
+    llmsStoreActions().deleteLLMUserParameter(llmId, parameterId);
+  }, [llmId]);
+
+
+  if (!llmId)
+    return <InlineError error='No model selected' />;
+
+  return (
+    <LLMParametersEditor
+      maxOutputTokens={llm.maxOutputTokens}
+      parameterSpecs={llm.parameterSpecs}
+      baselineParameters={llm.initialParameters}
+      parameters={llm.userParameters}
+      onChangeParameter={handleChangeParameter}
+      onRemoveParameter={handleRemoveParameter}
+    />
+  );
+}
