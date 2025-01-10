42fd9daa79a1688b92512d0170283d30d5ff9950
Enrico Ros <enrico.ros@gmail.com>
2025-01-09 16:02:58 -0800

    Uniform Attach buttons

--- src/apps/chat/components/composer/buttons/ButtonAttachClipboard.tsx
+++ src/apps/chat/components/composer/buttons/ButtonAttachClipboard.tsx
@@ -4,27 +4,39 @@ import { Box, Button, IconButton, Tooltip } from '@mui/joy';
 import ContentPasteGoIcon from '@mui/icons-material/ContentPasteGo';
 
 import { KeyStroke } from '~/common/components/KeyStroke';
-
-
-const pasteClipboardLegend =
-  <Box sx={{ px: 1, py: 0.75, lineHeight: '1.5rem' }}>
-    <b>Attach clipboard 📚</b><br />
-    Auto-converts to the best types<br />
-    <KeyStroke combo='Ctrl + Shift + V' sx={{ mt: 1, mb: 0.5 }} />
-  </Box>;
+import { buttonAttachSx } from '~/common/components/ButtonAttachFiles';
 
 
 export const ButtonAttachClipboardMemo = React.memo(ButtonAttachClipboard);
 
-function ButtonAttachClipboard(props: { isMobile?: boolean, onClick: () => void }) {
+function ButtonAttachClipboard(props: {
+  isMobile?: boolean,
+  disabled?: boolean,
+  fullWidth?: boolean,
+  noToolTip?: boolean,
+  onAttachClipboard: () => void,
+}) {
   return props.isMobile ? (
-    <IconButton onClick={props.onClick}>
+    <IconButton disabled={props.disabled} onClick={props.onAttachClipboard}>
       <ContentPasteGoIcon />
     </IconButton>
   ) : (
-    <Tooltip disableInteractive variant='solid' placement='top-start' title={pasteClipboardLegend}>
-      <Button fullWidth variant='plain' color='neutral' startDecorator={<ContentPasteGoIcon />} onClick={props.onClick}
-              sx={{ justifyContent: 'flex-start' }}>
+    <Tooltip arrow disableInteractive placement='top-start' title={props.noToolTip ? null : (
+      <Box sx={buttonAttachSx.tooltip}>
+        <b>Attach clipboard 📚</b><br />
+        Auto-converts to the best types<br />
+        <KeyStroke combo='Ctrl + Shift + V' sx={{ mt: 1, mb: 0.5 }} />
+      </Box>
+    )}>
+      <Button
+        variant='plain'
+        color='neutral'
+        disabled={props.disabled}
+        fullWidth={props.fullWidth}
+        startDecorator={<ContentPasteGoIcon />}
+        onClick={props.onAttachClipboard}
+        sx={buttonAttachSx.desktop}
+      >
         Paste
       </Button>
     </Tooltip>
--- src/apps/chat/components/composer/buttons/ButtonAttachScreenCapture.tsx
+++ src/apps/chat/components/composer/buttons/ButtonAttachScreenCapture.tsx
@@ -4,12 +4,19 @@ import { Box, Button, IconButton, Tooltip } from '@mui/joy';
 import ScreenshotMonitorIcon from '@mui/icons-material/ScreenshotMonitor';
 
 import { Is } from '~/common/util/pwaUtils';
+import { buttonAttachSx } from '~/common/components/ButtonAttachFiles';
 import { takeScreenCapture } from '~/common/util/screenCaptureUtils';
 
 
 export const ButtonAttachScreenCaptureMemo = React.memo(ButtonAttachScreenCapture);
 
-function ButtonAttachScreenCapture(props: { isMobile?: boolean, onAttachScreenCapture: (file: File) => void }) {
+function ButtonAttachScreenCapture(props: {
+  isMobile?: boolean,
+  disabled?: boolean,
+  fullWidth?: boolean,
+  noToolTip?: boolean,
+  onAttachScreenCapture: (file: File) => void
+}) {
 
   // state
   const [capturing, setCapturing] = React.useState(false);
@@ -34,31 +41,29 @@ function ButtonAttachScreenCapture(props: { isMobile?: boolean, onAttachScreenCa
 
 
   return props.isMobile ? (
-    <IconButton onClick={handleTakeScreenCapture}>
+    <IconButton disabled={props.disabled} onClick={handleTakeScreenCapture}>
       <ScreenshotMonitorIcon />
     </IconButton>
   ) : (
-    <Tooltip
-      arrow disableInteractive placement='top-start'
-      title={
-        <Box sx={{ px: 1, py: 0.75, lineHeight: '1.5rem' }}>
-          <b>Attach screen capture</b><br />
-          {error || 'Attach the image of a window, a browser tab, or a screen'}
-          {!error && Is.OS.MacOS && Is.Browser.Safari && (
-            <Box sx={{ mt: 1 }}><b>Safari</b>: canceling the window selection may cause a 60-second delay.</Box>
-          )}
-        </Box>
-      }
-    >
+    <Tooltip arrow disableInteractive placement='top-start' title={props.noToolTip ? null : (
+      <Box sx={buttonAttachSx.tooltip}>
+        <b>Attach screen capture</b><br />
+        {error || 'Attach the image of a window, a browser tab, or a screen'}
+        {!error && Is.OS.MacOS && Is.Browser.Safari && (
+          <Box sx={{ mt: 1 }}><b>Safari</b>: canceling the window selection may cause a 60-second delay.</Box>
+        )}
+      </Box>
+    )}>
       <Button
-        fullWidth
         variant={capturing ? 'solid' : 'plain'}
         color={!!error ? 'danger' : 'neutral'}
-        onClick={handleTakeScreenCapture}
+        disabled={props.disabled}
+        fullWidth={props.fullWidth}
         loading={capturing}
         loadingPosition={capturing ? 'start' : 'center'}
         startDecorator={<ScreenshotMonitorIcon />}
-        sx={{ justifyContent: 'flex-start' }}
+        onClick={handleTakeScreenCapture}
+        sx={buttonAttachSx.desktop}
       >
         Screen
       </Button>
