b1d94e790f866742bd1eb71e1edb051246a7e85d
Enrico Ros <enrico.ros@gmail.com>
2025-01-09 17:18:05 -0800

    AttachmentDraftsStoreApi: improve typing

--- src/common/attachment-drafts/store-attachment-drafts_vanilla.ts
+++ src/common/attachment-drafts/store-attachment-drafts_vanilla.ts
@@ -1,10 +1,9 @@
-import type { StoreApi } from 'zustand';
 import { createStore as createVanillaStore } from 'zustand/vanilla';
 
-import { AttachmentsDraftsStore, createAttachmentDraftsStoreSlice } from './store-attachment-drafts_slice';
+import { AttachmentDraftsStoreApi, AttachmentsDraftsStore, createAttachmentDraftsStoreSlice } from './store-attachment-drafts_slice';
 
 
-export const createAttachmentDraftsVanillaStore = (): StoreApi<AttachmentsDraftsStore> => createVanillaStore<AttachmentsDraftsStore>()((...a) => ({
+export const createAttachmentDraftsVanillaStore = (): AttachmentDraftsStoreApi => createVanillaStore<AttachmentsDraftsStore>()((...a) => ({
 
   // Attachments: attachment drafts
   ...createAttachmentDraftsStoreSlice(...a),
@@ -15,5 +14,5 @@ export const createAttachmentDraftsVanillaStore = (): StoreApi<AttachmentsDrafts
 // const _fallbackStoreApi = createPerChatVanillaStore();
 
 // // usages: useAttachmentDrafts
-// export const useChatAttachmentsStore = <T, >(vanillaStore: Readonly<StoreApi<AttachmentsDraftsStore>> | null, selector: (store: AttachmentsDraftsStore) => T): T =>
+// export const useChatAttachmentsStore = <T, >(vanillaStore: Readonly<AttachmentDraftsStoreApi> | null, selector: (store: AttachmentsDraftsStore) => T): T =>
 //   useStore(vanillaStore || fallbackStoreApi, selector);
