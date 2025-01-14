5839cf91ad3e222805932b9795f4c4f2315efb0a
Enrico Ros <enrico.ros@gmail.com>
2025-01-14 09:38:53 -0800

    Improve the dark mode toggle

--- src/common/components/DarkModeToggleButton.tsx
+++ src/common/components/DarkModeToggleButton.tsx
@@ -16,11 +16,22 @@ export function DarkModeToggleButton(props: { hasText?: boolean }) {
   };
 
   return props.hasText ? (
-    <Button size='sm' variant='soft' color='neutral' onClick={handleToggleDarkMode} sx={{ ml: 'auto' }} startDecorator={colorMode !== 'dark' ? <DarkModeIcon /> : <LightModeIcon />}>
+    <Button
+      size='sm'
+      variant='soft'
+      color='neutral'
+      onClick={handleToggleDarkMode}
+      sx={{
+        ml: 'auto',
+        boxShadow: 'xs',
+        backgroundColor: 'background.surface',
+      }}
+      startDecorator={colorMode !== 'dark' ? <DarkModeIcon color='primary' /> : <LightModeIcon />}
+    >
       {colorMode === 'dark' ? 'Light Mode' : 'Dark Mode'}
     </Button>
   ) : (
-    <IconButton size="sm" variant="soft" onClick={handleToggleDarkMode} sx={{ ml: 'auto', /*mr: '2px',*/ my: '-0.25rem' /* absorb the menuItem padding */ }}>
+    <IconButton size='sm' variant='soft' onClick={handleToggleDarkMode} sx={{ ml: 'auto', /*mr: '2px',*/ my: '-0.25rem' /* absorb the menuItem padding */ }}>
       {colorMode !== 'dark' ? <DarkModeIcon /> : <LightModeIcon />}
     </IconButton>
   );
