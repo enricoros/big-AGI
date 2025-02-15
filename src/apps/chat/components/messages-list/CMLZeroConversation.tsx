550a60f4af5c7272dfcf107fe6f80026be47de8b
Enrico Ros <enrico.ros@gmail.com>
2025-02-15 14:04:24 -0800

    Panes: Zero notices

--- src/apps/chat/components/messages-list/CMLZeroConversation.tsx
+++ src/apps/chat/components/messages-list/CMLZeroConversation.tsx
@@ -1,35 +1,56 @@
 import * as React from 'react';
 
-import { Alert, Button } from '@mui/joy';
+import { Box, Button, Sheet, Typography } from '@mui/joy';
 import AddIcon from '@mui/icons-material/Add';
+
+
+const _styles = {
+  center: {
+    mx: 'auto',
+    mt: 6.5, // roughly matches the 'AI Persona' buttons
+    // my: 'auto',
+  } as const,
+
+  sheet: {
+    p: 1.5,
+    borderRadius: 'sm',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 1,
+  } as const,
+
+  button: {
+    // width: '100%',
+    border: '1px solid',
+    borderColor: 'neutral.outlinedBorder',
+    borderRadius: 'sm',
+    justifyContent: 'start',
+  } as const,
+} as const;
 
 
 export function CMLZeroConversation(props: {
   onConversationNew: (forceNoRecycle: boolean, isIncognito: boolean) => void,
 }) {
   return (
-    <Alert
-      variant='soft'
-      startDecorator={
+    <Box sx={_styles.center}>
+      <Sheet
+        variant='outlined'
+        sx={_styles.sheet}
+      >
+        <Typography level='body-sm'>
+          Please select or create<br />
+          a conversation.
+        </Typography>
         <Button
           variant='soft'
           onClick={() => props.onConversationNew(true, false)}
           startDecorator={<AddIcon />}
-          sx={{
-            border: '1px solid',
-            borderColor: 'primary.outlinedBorder',
-            borderRadius: 'sm',
-          }}
+          sx={_styles.button}
         >
           New chat
         </Button>
-      }
-      sx={{
-        m: 2,
-        // justifyContent: 'center',
-      }}
-    >
-      Please select or create a conversation.
-    </Alert>
+      </Sheet>
+    </Box>
   );
 }
