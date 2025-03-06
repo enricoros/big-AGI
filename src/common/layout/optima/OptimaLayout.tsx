370f1563eaff7c10a4d699961f984513da428df1
Enrico Ros <enrico.ros@gmail.com>
2025-03-05 18:27:39 -0800

    DNT-DT: wire up

--- src/common/layout/optima/OptimaLayout.tsx
+++ src/common/layout/optima/OptimaLayout.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useRouter } from 'next/router';
 import { PanelGroup } from 'react-resizable-panels';
 
+import { GlobalDragOverlay } from '~/common/components/dnd-dt/GlobalDragOverlay';
 import { Is } from '~/common/util/pwaUtils';
 import { checkVisibleNav, navItems } from '~/common/app.nav';
 import { useGlobalShortcuts } from '~/common/components/shortcuts/useGlobalShortcuts';
@@ -95,7 +96,7 @@ export function OptimaLayout(props: { suspendAutoModelsSetup?: boolean, children
     </PanelGroup>
 
     {/* Global Window Overlay */}
-    {/*<GlobalDragOverlay />*/}
+    {Is.Desktop && <GlobalDragOverlay />}
 
     {/* Overlay Modals */}
     <Modals suspendAutoModelsSetup={props.suspendAutoModelsSetup} />
