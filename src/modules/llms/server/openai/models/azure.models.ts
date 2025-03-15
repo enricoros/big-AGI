94adf3cda69a435c2443dfab5189e32950bc8cfa
Enrico Ros <enrico.ros@gmail.com>
2025-03-14 23:23:17 -0700

    Azure: instructions on how to add models customizations via code. #774

--- src/modules/llms/server/openai/models/azure.models.ts
+++ src/modules/llms/server/openai/models/azure.models.ts
@@ -1,9 +1,32 @@
 import { z } from 'zod';
 
+// import { LLM_IF_HOTFIX_NoTemperature, LLM_IF_OAI_Chat, LLM_IF_OAI_Fn, LLM_IF_OAI_Json, LLM_IF_OAI_Vision } from '~/common/stores/llms/llms.types';
 import type { ModelDescriptionSchema } from '../../llm.server.types';
 
-import { fromManualMapping } from './models.data';
+import { fromManualMapping, ManualMappings } from './models.data';
 import { _knownOpenAIChatModels } from './openai.models';
+
+
+// [Azure]
+const _knownAzureChatModels: ManualMappings = [
+  // ... if you have your own models, map them here ...
+  //
+  // NOTE: the ManualMapping object is similar to ModelDescriptionSchema,
+  // with a renamed id field and other flags (isPreview, isLegacy, etc.), only used for
+  // a consistent labeling of the model when fromManualMapping is invoked.
+  //
+  // Example:
+  // {
+  //   idPrefix: 'my-deployment-name',
+  //   label: 'This is THE Model (25.12)',
+  //   description: 'Top-tier reasoning.',
+  //   contextWindow: 200 * 1024, // 200K tokens
+  //   maxCompletionTokens: 8192, // 8K tokens
+  //   interfaces: [LLM_IF_OAI_Chat, LLM_IF_OAI_Vision, LLM_IF_OAI_Json, LLM_IF_OAI_Fn, /* <- flags | fixes -> */ LLM_IF_HOTFIX_NoTemperature],
+  //   chatPrice: { input: 2, output: 6 },
+  // },
+  //
+];
 
 
 // parser for Azure models - 2025-03-14: verified
@@ -50,17 +73,17 @@ export function azureDeploymentToModelDescription(deployment: AzureOpenAIDeploym
   } = deployment;
 
   // MAPPING of Deployment -> ModelDescription
-  // ... implement your own here ...
+  const allModels = [..._knownAzureChatModels, ..._knownOpenAIChatModels];
 
   /**
    * Fallback: heuristics to map op OpenAI follow:
    *  1. if the name of the model (set by the user) matches exactly a known OpenAI model, use that
    *  2. otherwise match the 'model' field, which SHOULD be the real OpenAI model id
    */
-  const isNameAKnownOpenAIModel = !!_knownOpenAIChatModels.find(({ idPrefix: id }) => deploymentName == id);
+  const isNameAKnownOpenAIModel = !!allModels.find(({ idPrefix: id }) => deploymentName == id);
 
   const { id: _ignoreThisId, label, hidden: _allVisible, ...restOfModelDescription } = fromManualMapping(
-    _knownOpenAIChatModels,
+    allModels,
     isNameAKnownOpenAIModel ? deploymentName : likelyTheOpenAIModel,
     modelCreated,
     modelUpdated,
