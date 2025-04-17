0e02db3d1025949afc8caad0f44643585420ba51
Enrico Ros <enrico.ros@gmail.com>
2025-04-17 13:35:44 -0700

    Feature Badge

---
+++ src/common/components/FeatureBadge.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+
+import { Badge, BadgeProps } from '@mui/joy';
+
+import { uiSetDismissed, useUIIsDismissed } from '~/common/stores/store-ui';
+
+
+// configuration
+const DEBUG_SHOW_ALL = false;  // set to true to show all badges (for debugging purposes)
+const FEATURE_COLOR = 'color-feature';
+
+
+export function FeatureBadge(props: Omit<BadgeProps, 'size'> & {
+  /** will be prefixed with 'feature-badge-...' */
+  featureKey: string;
+  active: boolean;
+  label?: React.ReactNode;
+  size?: 'sm' | 'md' | 'lg';  // default: 'sm'
+}) {
+
+  const featureKey = 'feature-badge-' + props.featureKey;
+  const { active, label, size = 'sm', color, children, ...badgeProps } = props;
+
+  // external state
+  const isDismissed = useUIIsDismissed(featureKey) ?? false;
+
+
+  // [effect] mark as dismissed when the feature flips to active for the first time
+  const firstFlip = !isDismissed && active;
+  React.useEffect(() => {
+    firstFlip && uiSetDismissed(featureKey);
+  }, [featureKey, firstFlip]);
+
+
+  // NOTE: changes the DOM structure (1 level less)
+  const invisible = isDismissed || active;
+  if (!DEBUG_SHOW_ALL && invisible)
+    return children;
+
+  return (
+    <Badge
+      // invisible={invisible}
+      size={size}
+      color={color ?? FEATURE_COLOR as any}
+      badgeContent={label}
+      {...badgeProps}
+    >
+      {children}
+    </Badge>
+  );
+}
