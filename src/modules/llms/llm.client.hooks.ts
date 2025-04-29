774f14810f6b696884aa1ac9155b7174d162f42c
Enrico Ros <enrico.ros@gmail.com>
2025-04-28 20:53:08 -0700

    LLMs: keep user edits

--- src/modules/llms/llm.client.hooks.ts
+++ src/modules/llms/llm.client.hooks.ts
@@ -14,7 +14,7 @@ import { llmsUpdateModelsForServiceOrThrow } from './llm.client';
 export function useLlmUpdateModels<TServiceSettings extends object>(
   enabled: boolean,
   service: DModelsService<TServiceSettings> | null,
-  discardUserEdits?: boolean,
+  // discardUserEdits?: boolean,
 ): {
   isFetching: boolean,
   refetch: () => void,
@@ -27,7 +27,7 @@ export function useLlmUpdateModels<TServiceSettings extends object>(
     queryFn: async () => {
       if (!service)
         throw new Error('No service provided to fetch models for');
-      return await llmsUpdateModelsForServiceOrThrow(service.id, !discardUserEdits);
+      return await llmsUpdateModelsForServiceOrThrow(service.id, true /*!discardUserEdits*/);
     },
     staleTime: Infinity,
   });
