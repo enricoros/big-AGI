82be58b54ae82125a56dcc2f90be30103125562c
Enrico Ros <enrico.ros@gmail.com>
2025-05-03 13:55:37 -0700

    Optima: cut bar overflow

--- src/common/layout/optima/bar/OptimaBar.tsx
+++ src/common/layout/optima/bar/OptimaBar.tsx
@@ -41,6 +41,8 @@ const centerItemsContainerSx: SxProps = {
   display: 'flex', flexFlow: 'row wrap', justifyContent: 'center', alignItems: 'center',
   my: 'auto',
   gap: { xs: 0, md: 1 },
+  // ensure we can keep the plugged center bars in check
+  overflow: 'hidden',
   // [electron] make the blank part of the bar draggable (and not the contents)
   WebkitAppRegion: 'drag',
   '& > *': { WebkitAppRegion: 'no-drag' },
