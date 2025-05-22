48c4ac18ab8d0efa320348a84c015eacb3e3c6f1
Enrico Ros <enrico.ros@gmail.com>
2025-05-22 13:22:38 -0700

    AIX: emergency fix for a sudden Vercel/tRPC streaming issue
    Suddenly all Vercel builds experienced exceptions and connection terminations.
    
    On 2025-05-22 around 8PM CET, Vercel servers started to log errors on tRPC calls.
    
    This fix waits 1 extra event loop tick. Shall work around the issue until a proper fix is found.

--- src/modules/aix/server/api/aix.router.ts
+++ src/modules/aix/server/api/aix.router.ts
@@ -235,6 +235,26 @@ export const aixRouter = createTRPCRouter({
       // or an error that has already been queued up for this last flush
       yield* chatGenerateTx.flushParticles();
 
+      /**
+       * 2025-05-22: Workaround an issue that appeared in all Vercel deployments.
+       *
+       * This is an emergency fix deployed with priority.
+       *
+       * Analysis:
+       * - the server side saw the following exceptions on Vercel, during the call to this server-side tRPC
+       *   streaming chatGenerateContent function:
+       *
+       *   TypeError: Cannot read properties of undefined (reading 'return')
+       *     at (node_modules/@trpc/server/dist/unstable-core-do-not-import/stream/utils/disposable.mjs:38:0)
+       *     at (node_modules/@trpc/server/dist/unstable-core-do-not-import/stream/jsonl.mjs:204:0)
+       *
+       * - we haven't isolated the cause, but seems that awaiting for the next event loop cycle suppresses
+       *   the issue.
+       *
+       * Ext refs: posted to the tRCP Discord on the streaming channel if anyone else saw this issue.
+       */
+      await new Promise((resolve) => setTimeout(resolve, 0));
+
     }),
 
 });
