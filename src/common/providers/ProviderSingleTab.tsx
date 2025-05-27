f9e14171cabd87992173741220b40e9d6f74dd74
Enrico Ros <enrico.ros@gmail.com>
2025-05-27 09:22:06 -0700

    ProviderSingleTab: add bypass

--- src/common/providers/ProviderSingleTab.tsx
+++ src/common/providers/ProviderSingleTab.tsx
@@ -6,13 +6,13 @@ import { reloadPage } from '../app.routes';
 import { useSingleTabEnforcer } from '../components/useSingleTabEnforcer';
 
 
-export const ProviderSingleTab = (props: { children: React.ReactNode }) => {
+export const ProviderSingleTab = (props: { disabled?: boolean, children: React.ReactNode }) => {
 
   // state
   const isSingleTab = useSingleTabEnforcer('big-agi-tabs');
 
   // pass-through until we know for sure that other tabs are open
-  if (isSingleTab === null || isSingleTab)
+  if (props.disabled || isSingleTab === null || isSingleTab)
     return props.children;
 
 
