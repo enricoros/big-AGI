b07573ec4f7ea98ea37da62545d14e6ddd153ba0
Enrico Ros <enrico.ros@gmail.com>
2025-07-17 10:58:05 -0700

    chat.message: nit

--- src/common/stores/chat/chat.message.ts
+++ src/common/stores/chat/chat.message.ts
@@ -198,7 +198,7 @@ export function duplicateDMessageGenerator(generator: Readonly<DMessageGenerator
 // helpers - status checks
 
 export function messageWasInterruptedAtStart(message: Pick<DMessage, 'generator' | 'fragments'>): boolean {
-  return message.generator?.tokenStopReason === 'client-abort' && message.fragments.length === 0;
+  return message.generator?.tokenStopReason === 'client-abort' && !message.fragments?.length;
 }
 
 // export function messageOnlyContainsPlaceholder(message: Pick<DMessage, 'fragments'>): boolean {
