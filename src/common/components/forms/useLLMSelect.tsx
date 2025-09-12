98bcbba7ca261b1d188b9503921aa1e584d28c34
Enrico Ros <enrico.ros@gmail.com>
2025-09-12 10:35:51 -0700

    useLLMSelect: option to configure models at the bottom, and optimize styles

--- src/common/components/forms/useLLMSelect.tsx
+++ src/common/components/forms/useLLMSelect.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 
 import type { SxProps } from '@mui/joy/styles/types';
-import { Chip, ColorPaletteProp, FormControl, IconButton, ListDivider, ListItemDecorator, Option, optionClasses, Select, SelectSlotsAndSlotProps, SvgIconProps, VariantProp } from '@mui/joy';
+import { Box, Chip, ColorPaletteProp, FormControl, IconButton, ListDivider, ListItemDecorator, Option, optionClasses, Select, SelectSlotsAndSlotProps, SvgIconProps, VariantProp } from '@mui/joy';
+import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
 import AutoModeIcon from '@mui/icons-material/AutoMode';
+import BuildCircleIcon from '@mui/icons-material/BuildCircle';
 
 import type { IModelVendor } from '~/modules/llms/vendors/IModelVendor';
 import { findModelVendor } from '~/modules/llms/vendors/vendors.registry';
@@ -23,6 +25,7 @@ const LLM_SELECT_REDUCE_OPTIONS = 10; // optimization: number of options over wh
 const LLM_SELECT_SHOW_REASONING_ICON = false;
 const LLM_TEXT_PLACEHOLDER = 'Models …';
 const LLM_TEXT_CONFIGURE = 'Add Models …';
+const LLM_SPECIAL_CONFIGURE_ID = '_CONF_' as DLLMId; // special id to open the Models panel
 
 
 /*export function useLLMSelectGlobalState(): [DLLMId | null, (llmId: DLLMId | null) => void] {
@@ -35,17 +38,31 @@ export function useLLMSelectLocalState(initFromGlobal: boolean): [DLLMId | null,
   } : null);
 }
 
-const llmSelectSx: SxProps = {
-  flex: 1,
-  backgroundColor: 'background.popup',
-  // minWidth: '200',
-} as const;
-
-const styleChips: SxProps = {
-  ml: 'auto',
-  backgroundColor: 'background.popup',
-  boxShadow: 'xs',
-} as const;
+const _styles = {
+  select: {
+    flex: 1,
+    backgroundColor: 'background.popup',
+    // minWidth: '200',
+  },
+  chips: {
+    ml: 'auto',
+    backgroundColor: 'background.popup',
+    boxShadow: 'xs',
+  },
+  listVendor: {
+    // see OptimaBarDropdown's _styles.separator
+    fontSize: 'sm',
+    color: 'text.tertiary',
+    textAlign: 'center',
+    my: 0.75,
+  },
+  listConfSep: {
+    mb: 0,
+  },
+  listConfigure: {
+    py: 'calc(2 * var(--ListDivider-gap))',
+  },
+} as const satisfies Record<string, SxProps>;
 
 const _slotProps: SelectSlotsAndSlotProps<false>['slotProps'] = {
   // see the OptimaBarDropdown.listbox for a well made customization (max-height, max-width, etc.)
@@ -58,8 +75,9 @@ const _slotProps: SelectSlotsAndSlotProps<false>['slotProps'] = {
       // No need for larger SVG icons here
       // '--Icon-fontSize': 'var(--joy-fontSize-xl2)',
 
-      // No need to remove the gutter
-      // paddingBlock: 0,
+      // remove the gutter from the bottom, which makes the 'appendConfigureModels' option look
+      // good, but makes the default case a bit too close to the bottom
+      paddingBottom: 0,
 
       // v-size: keep the default
       // maxHeight: 'calc(100dvh - 56px - 24px)',
@@ -102,6 +120,7 @@ interface LLMSelectOptions {
   placeholder?: string;
   isHorizontal?: boolean;
   autoRefreshDomain?: DModelDomainId;
+  appendConfigureModels?: boolean; // appends a bottom option to open the Models panel
 }
 
 /**
@@ -124,7 +143,7 @@ export function useLLMSelect(
   const _filteredLLMs = useVisibleLLMs(llmId);
 
   // derived state
-  const { label, larger = false, disabled = false, placeholder = LLM_TEXT_PLACEHOLDER, isHorizontal = false, autoRefreshDomain } = options;
+  const { label, larger = false, disabled = false, placeholder = LLM_TEXT_PLACEHOLDER, isHorizontal = false, autoRefreshDomain, appendConfigureModels = false } = options;
   const noIcons = false; //smaller;
   const llm = !llmId ? null : _filteredLLMs.find(llm => llm.id === llmId) ?? null;
   const isReasoning = !LLM_SELECT_SHOW_REASONING_ICON ? false : llm?.interfaces?.includes(LLM_IF_OAI_Reasoning) ?? false;
@@ -150,7 +169,7 @@ export function useLLMSelect(
       // add separators if the vendor changed (and more than one vendor)
       const addSeparator = vendorChanged && formerVendor !== null;
       if (addSeparator && !optimizeToSingleVisibleId)
-        acc.push(<ListDivider key={'llm-sep-' + llm.id}>{vendor?.name}</ListDivider>);
+        acc.push(<Box key={'llm-sep-' + llm.id} sx={_styles.listVendor}>{vendor?.name}</Box>);
 
       let features = '';
       const isNotSymlink = !llm.label.startsWith('🔗');
@@ -187,7 +206,7 @@ export function useLLMSelect(
           <div className='agi-ellipsize'>{llm.label}</div>
 
           {/* Features Chips - sync with `ModelsList.tsx` */}
-          {!!features && <Chip size='sm' color={seemsFree ? 'success' : undefined} variant='plain' sx={styleChips}>{features.trim().replace(' ', ' ')}</Chip>}
+          {!!features && <Chip size='sm' color={seemsFree ? 'success' : undefined} variant='plain' sx={_styles.chips}>{features.trim().replace(' ', ' ')}</Chip>}
 
           {/*</Tooltip>*/}
           {/*{llm.gen === 'sdxl' && <Chip size='sm' variant='outlined'>XL</Chip>} {llm.label}*/}
@@ -199,7 +218,13 @@ export function useLLMSelect(
   }, [_filteredLLMs, noIcons, optimizeToSingleVisibleId]);
 
 
-  const onSelectChange = React.useCallback((_event: unknown, value: DLLMId | null) => value && setLlmId(value), [setLlmId]);
+  const onSelectChange = React.useCallback((_event: unknown, value: DLLMId | null) => {
+    // special: open the Models panel
+    if (value === LLM_SPECIAL_CONFIGURE_ID) return optimaOpenModels();
+    // invoke the callback if the selection is non-null
+    value && setLlmId(value);
+  }, [setLlmId]);
+
 
   const hasNoModels = _filteredLLMs.length === 0;
   const showNoOptions = !optionsArray.length;
@@ -227,13 +252,26 @@ export function useLLMSelect(
             </IconButton>
           </TooltipOutlined>
           : isReasoning ? '🧠' : undefined}
-        sx={options.sx ?? llmSelectSx}
+        sx={options.sx ?? _styles.select}
       >
+
+        {/* Model Options */}
         {optionsArray}
+
+        {/* Models Modal Dialog Option */}
+        {appendConfigureModels && !optimizeToSingleVisibleId && !hasNoModels && !showNoOptions && <ListDivider key='cm-sep' sx={_styles.listConfSep} />}
+        {appendConfigureModels && !optimizeToSingleVisibleId && !hasNoModels && (
+          <Option key='cm-option' variant='soft' value={LLM_SPECIAL_CONFIGURE_ID} sx={_styles.listConfigure}>
+            <ListItemDecorator><BuildCircleIcon color='success' /></ListItemDecorator>
+            Models
+            <ArrowForwardRoundedIcon sx={{ ml: 'auto', fontSize: 'xl' }} />
+          </Option>
+        )}
+
       </Select>
       {/*</Box>*/}
     </FormControl>
-  ), [autoRefreshDomain, controlledOpen, disabled, hasNoModels, isHorizontal, isReasoning, label, larger, llmId, onSelectChange, options.color, options.sx, options.variant, optionsArray, placeholder, showNoOptions]);
+  ), [appendConfigureModels, autoRefreshDomain, controlledOpen, disabled, hasNoModels, isHorizontal, isReasoning, label, larger, llmId, onSelectChange, optimizeToSingleVisibleId, options.color, options.sx, options.variant, optionsArray, placeholder, showNoOptions]);
 
   // Memo the vendor icon for the chat LLM
   const chatLLMVendorIconFC = React.useMemo(() => {
