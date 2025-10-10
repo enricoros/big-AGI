5f76200c13aadbf0fc2d02f0f8b59fc6106e94d0
Enrico Ros <enrico.ros@gmail.com>
2025-10-10 09:06:14 -0700

    AIX: Inspector: Quick Toggle

--- src/apps/settings-modal/SettingsModal.tsx
+++ src/apps/settings-modal/SettingsModal.tsx
@@ -199,7 +199,7 @@ export function SettingsModal(props: {
 
   const { setTab } = props;
   const isToolsTab = props.tab === 'tools';
-  const enableAixDebugger = Is.Deployment.Localhost;
+  const enableAixDebuggerDialog = true;
 
   const handleSetTab = React.useCallback((_event: any, value: string | number | null) => {
     setTab((value ?? undefined) as PreferencesTabId);
@@ -221,11 +221,11 @@ export function SettingsModal(props: {
           {!isMobile && !isToolsTab && <Button variant='soft' color='neutral' onClick={props.onOpenShortcuts} startDecorator={<KeyboardCommandKeyOutlinedIcon color='primary' />} sx={darkModeToggleButtonSx}>
             Shortcuts
           </Button>}
+          {isToolsTab && <Button variant='soft' color='neutral' disabled={!enableAixDebuggerDialog} onClick={optimaActions().openAIXDebugger} startDecorator={<TerminalOutlinedIcon color={enableAixDebuggerDialog ? 'primary' : undefined} />} sx={darkModeToggleButtonSx}>
+            AI Inspector
+          </Button>}
           {isToolsTab && <Button variant='soft' color='neutral' onClick={optimaActions().openLogger} startDecorator={<TerminalOutlinedIcon color='primary' />} sx={darkModeToggleButtonSx}>
             Logs Viewer
-          </Button>}
-          {isToolsTab && <Button variant='soft' color='neutral' disabled={!enableAixDebugger} onClick={optimaActions().openAIXDebugger} startDecorator={<TerminalOutlinedIcon color={enableAixDebugger ? 'primary' : undefined} />} sx={darkModeToggleButtonSx}>
-            AIX Debugger
           </Button>}
         </Box>
       }
