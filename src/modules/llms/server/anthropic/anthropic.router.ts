9b32c4b8c5cfaf93b2d989467c942e0f62d69fa3
Enrico Ros <enrico.ros@gmail.com>
2025-10-18 11:17:09 -0700

    AIX: Anthropic: headers improvement

--- src/modules/llms/server/anthropic/anthropic.router.ts
+++ src/modules/llms/server/anthropic/anthropic.router.ts
@@ -78,14 +78,19 @@ const PER_MODEL_BETA_FEATURES: { [modelId: string]: string[] } = {
   ] as const,
 } as const;
 
-function _anthropicHeaders(modelId?: string): HeadersInit {
+type AnthropicHeaderOptions = {
+  modelIdForBetaFeatures?: string;
+  vndAntWebFetch?: boolean;
+};
+
+function _anthropicHeaders(options?: AnthropicHeaderOptions): Record<string, string> {
 
   // accumulate the beta features
   const betaFeatures = [...DEFAULT_ANTHROPIC_BETA_FEATURES];
-  if (modelId) {
+  if (options?.modelIdForBetaFeatures) {
     // string search (.includes) within the keys, to be more resilient to modelId changes/prefixing
     for (const [key, value] of Object.entries(PER_MODEL_BETA_FEATURES))
-      if (key.includes(modelId))
+      if (key.includes(options.modelIdForBetaFeatures))
         betaFeatures.push(...value);
   }
 
@@ -99,7 +104,7 @@ function _anthropicHeaders(modelId?: string): HeadersInit {
 // Mappers
 
 async function anthropicGETOrThrow<TOut extends object>(access: AnthropicAccessSchema, antModelIdForBetaFeatures: undefined | string, apiPath: string /*, signal?: AbortSignal*/): Promise<TOut> {
-  const { headers, url } = anthropicAccess(access, antModelIdForBetaFeatures, apiPath);
+  const { headers, url } = anthropicAccess(access, apiPath, { modelIdForBetaFeatures: antModelIdForBetaFeatures });
   return await fetchJsonOrTRPCThrow<TOut>({ url, headers, name: 'Anthropic' });
 }
 
@@ -108,7 +113,7 @@ async function anthropicGETOrThrow<TOut extends object>(access: AnthropicAccessS
 //   return await fetchJsonOrTRPCThrow<TOut, TPostBody>({ url, method: 'POST', headers, body, name: 'Anthropic' });
 // }
 
-export function anthropicAccess(access: AnthropicAccessSchema, antModelIdForBetaFeatures: undefined | string, apiPath: string): { headers: HeadersInit, url: string } {
+export function anthropicAccess(access: AnthropicAccessSchema, apiPath: string, options?: AnthropicHeaderOptions): { headers: HeadersInit, url: string } {
   // API key
   const anthropicKey = access.anthropicKey || env.ANTHROPIC_API_KEY || '';
 
@@ -135,7 +140,7 @@ export function anthropicAccess(access: AnthropicAccessSchema, antModelIdForBeta
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      ..._anthropicHeaders(antModelIdForBetaFeatures),
+      ..._anthropicHeaders(options),
       'X-API-Key': anthropicKey,
       ...(heliKey && { 'Helicone-Auth': `Bearer ${heliKey}` }),
     },
