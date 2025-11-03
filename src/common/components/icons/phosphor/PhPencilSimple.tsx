285d5b086d77c5aba093b1dc10e286484fba989c
Enrico Ros <enrico.ros@gmail.com>
2025-11-03 09:04:28 -0800

    PhPencilSimple: add

---
+++ src/common/components/icons/phosphor/PhPencilSimple.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import { SvgIcon, SvgIconProps } from '@mui/joy';
+
+/*
+ * Source: 'https://phosphoricons.com/' - pencil-simple
+ */
+export function PhPencilSimple(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox='0 0 256 256' stroke='none' fill='currentColor' width='24' height='24' {...props}>
+      <path d='M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM92.69,208H48V163.31l88-88L180.69,120ZM192,108.68,147.31,64l24-24L216,84.68Z' />
+    </SvgIcon>
+  );
+}
