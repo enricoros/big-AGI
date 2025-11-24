aaa3f5c250077abf3e2a32828f973ab74e839ddc
Enrico Ros <enrico.ros@gmail.com>
2025-11-24 04:09:39 -0800

    2.0.1 Roll AIX, news

--- src/common/app.release.ts
+++ src/common/app.release.ts
@@ -23,8 +23,8 @@ export const Release = {
 
   // this is here to trigger revalidation of data, e.g. models refresh
   Monotonics: {
-    Aix: 40,
-    NewsVersion: 200,
+    Aix: 41,
+    NewsVersion: 201,
   },
 
   // Frontend: pretty features
