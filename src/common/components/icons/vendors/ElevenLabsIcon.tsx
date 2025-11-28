28d911c617aeff406032f00df0fb955660fd68fd
Enrico Ros <enrico.ros@gmail.com>
2025-11-28 05:27:01 -0800

    ElevenLabsIcon: add icon

---
+++ src/common/components/icons/vendors/ElevenLabsIcon.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { SvgIcon, SvgIconProps } from '@mui/joy';
+
+export function ElevenLabsIcon(props: SvgIconProps) {
+  return <SvgIcon viewBox='0 0 24 24' width='24' height='24' fill='currentColor' {...props}>
+    <path d='M7 4h3v16H7V4zm7 0h3v16h-3V4z' />
+  </SvgIcon>;
+}
