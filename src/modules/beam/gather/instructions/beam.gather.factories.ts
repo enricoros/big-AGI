4fa3c4d479aeceb3a7b6853c6550a2750f4129e6
Enrico Ros <enrico.ros@gmail.com>
2025-12-05 19:32:34 -0800

    Remove old material (wrong) path

--- src/modules/beam/gather/instructions/beam.gather.factories.ts
+++ src/modules/beam/gather/instructions/beam.gather.factories.ts
@@ -1,4 +1,4 @@
-import type { SvgIcon } from '@mui/material';
+import type { SvgIcon } from '@mui/joy';
 import BuildRoundedIcon from '@mui/icons-material/BuildRounded';
 import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
 import MediationOutlinedIcon from '@mui/icons-material/MediationOutlined';
@@ -33,7 +33,7 @@ export const FUSION_FACTORIES: FusionFactorySpec[] = [
     shortLabel: 'Fuse',
     addLabel: 'Add Fusion',
     cardTitle: 'Combined Response',
-    Icon: MediationOutlinedIcon,
+    Icon: MediationOutlinedIcon as typeof SvgIcon,
     description: 'AI combines conversation details and ideas into one clear, comprehensive answer.',
     createInstructions: () => [
       {
@@ -59,7 +59,7 @@ Synthesize the perfect cohesive response to my last message that merges the coll
     shortLabel: 'Guided',
     addLabel: 'Add Checklist',
     cardTitle: 'Guided Response',
-    Icon: CheckBoxOutlinedIcon,
+    Icon: CheckBoxOutlinedIcon as typeof SvgIcon,
     description: 'Choose between options extracted by AI from the replies, and the model will combine your selections into a single answer.',
     // description: 'This approach employs a two-stage, interactive process where an AI first generates a checklist of insights from a conversation for user selection, then synthesizes those selections into a tailored, comprehensive response, integrating user preferences with AI analysis and creativity.',
     createInstructions: () => [
@@ -121,7 +121,7 @@ The final output should reflect a deep understanding of the user's preferences a
     shortLabel: 'Compare',
     addLabel: 'Add Breakdown',
     cardTitle: 'Evaluation Table',
-    Icon: TableViewRoundedIcon,
+    Icon: TableViewRoundedIcon as typeof SvgIcon,
     description: 'Analyzes and compares AI responses, offering a structured framework to support your response choice.',
     createInstructions: () => [
       {
@@ -168,7 +168,7 @@ Only work with the provided {{N}} responses. Begin with listing the criteria.`.t
     shortLabel: 'Custom',
     addLabel: 'Add Custom',
     cardTitle: 'User Defined',
-    Icon: BuildRoundedIcon,
+    Icon: BuildRoundedIcon as typeof SvgIcon,
     description: 'Define your own fusion prompt.',
     createInstructions: () => [
       {
