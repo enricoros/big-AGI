51b60598223277e84b30575e70dc6047efadf31a
Enrico Ros <enrico.ros@gmail.com>
2026-03-03 10:09:39 -0800

    Remove setLabsEnhanceCodeLiveFile

--- src/common/livefile/useLiveFileMetadata.tsx
+++ src/common/livefile/useLiveFileMetadata.tsx
@@ -1,6 +1,7 @@
+import { useShallow } from 'zustand/react/shallow';
+
 import type { LiveFileId, LiveFileMetadata } from '~/common/livefile/liveFile.types';
 import { useLiveFileStore } from '~/common/livefile/store-live-file';
-import { useShallow } from 'zustand/react/shallow';
 
 export function useLiveFileMetadata(liveFileId: LiveFileId | undefined): LiveFileMetadata | null {
   return useLiveFileStore(useShallow((store) =>
