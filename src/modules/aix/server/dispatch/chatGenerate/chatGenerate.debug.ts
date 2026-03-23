ca245bf8b8cb9b43d10740b2e48abaf7106acf04
Enrico Ros <enrico.ros@gmail.com>
2026-03-23 15:04:02 -0700

    AIX: Retriers: cleanup name

--- src/modules/aix/server/dispatch/chatGenerate/chatGenerate.debug.ts
+++ src/modules/aix/server/dispatch/chatGenerate/chatGenerate.debug.ts
@@ -11,7 +11,7 @@ export function _createDebugConfig(access: AixAPI_Access, options: undefined | {
   const echoRequest = !!options?.debugDispatchRequest && (AIX_SECURITY_ONLY_IN_DEV_BUILDS || AIX_INSPECTOR_ALLOWED_CONTEXTS.includes(chatGenerateContextName));
   const consoleLogErrors =
     (access.dialect === 'openai' && access.oaiHost) ? false as const // do not server-log OpenAI Custom hosts (often self-hosted and buggy) from server-side console error logging
-      : 'srv-warn' as const; // keeping the highest level of server-side logging for 'fetching' issues (usually however we see the messages of the TRPC retrier `createRetryablePromise` already)
+      : 'srv-warn' as const; // keeping the highest level of server-side logging for 'fetching' issues (usually however we see the messages of the TRPC retrier `fetchWithAbortableConnectionRetry` already)
   return {
     prettyDialect: serverCapitalizeFirstLetter(access.dialect), // string
     echoRequest: echoRequest, // boolean
