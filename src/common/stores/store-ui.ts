d840b0b57a83964e846b96655229e3572cf58af3
Enrico Ros <enrico.ros@gmail.com>
2026-03-24 22:44:10 -0700

    Composer: remove the ctrl+enter hint

--- src/common/stores/store-ui.ts
+++ src/common/stores/store-ui.ts
@@ -251,7 +251,6 @@ type KnownKeys =
   | 'call-wizard'                     // first Call
   | 'composer-shift-enter'            // not used Shift + Enter in the Composer yet
   | 'composer-alt-enter'              // not used Alt + Enter in the Composer yet
-  | 'composer-ctrl-enter'             // not used Ctrl + Enter in the Composer yet
   | 'models-setup-first-visit'        // first visit to the Models Setup
   ;
 
