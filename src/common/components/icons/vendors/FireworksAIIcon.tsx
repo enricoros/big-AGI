08fed36a61fa7b22de29e5b1f23cffb987d20db2
Enrico Ros <enrico.ros@gmail.com>
2026-04-02 22:17:12 -0700

    LLMs: add new icons

---
+++ src/common/components/icons/vendors/FireworksAIIcon.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { SvgIcon, SvgIconProps } from '@mui/joy';
+
+export function FireworksAIIcon(props: SvgIconProps) {
+  return <SvgIcon viewBox='-0.5 -12 48 48' width='24' height='24' fill='currentColor' stroke='none' {...props}>
+    <path fillRule='evenodd' clipRule='evenodd' d='M29 .4L23.5 13.5 18 .4H14.5L20.5 14.8C21 16 22.2 16.7 23.5 16.7 24.8 16.7 26 16 26.5 14.8L32.5.4H29ZM31.4 20.4L41.4 10.3 40 7 29.1 18.2C28.2 19.1 27.9 20.5 28.4 21.7 28.9 22.8 30.1 23.6 31.4 23.6H47.1L45.7 20.4H31.4ZM5.6 10.3L7 7 18 18.1C18.9 19.1 19.2 20.5 18.7 21.6 18.2 22.8 17 23.6 15.7 23.6H0L1.4 20.3H15.7L5.6 10.3Z' />
+  </SvgIcon>;
+}
---
+++ src/common/components/icons/vendors/HeliconeIcon.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import { SvgIcon, SvgIconProps } from '@mui/joy';
+
+export function HeliconeIcon(props: SvgIconProps) {
+  return <SvgIcon viewBox='30 30 252 252' width='24' height='24' fill='none' stroke='currentColor' strokeWidth='9' strokeLinecap='round' strokeLinejoin='round' {...props}>
+    <path d='M36 89L155 37C156 36.6 157 36.6 158 37L276 89M36 89V197M36 89L129 130M276 89L244 103M276 89V117M156 278V238M156 278L183 266M156 278L47 232M156 141L244 103M156 141L129 130M156 141V153M156 238L244 103M156 238V153M183 266L274 228C275 228 276 226 276 224V117M183 266L276 117M36 197V224C36 226 37 228 39 228L47 232M36 197L129 130M47 232L156 153' />
+  </SvgIcon>;
+}
