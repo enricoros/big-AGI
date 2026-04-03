f95b643a5c01be4e2c5c66c329e5711f2eb8496e
Enrico Ros <enrico.ros@gmail.com>
2026-04-02 23:05:21 -0700

    roll Typescript 6.0.2

--- src/common/util/fileSystemUtils.ts
+++ src/common/util/fileSystemUtils.ts
@@ -1,14 +1,6 @@
 import type { FileWithHandle } from 'browser-fs-access';
 
 
-/**
- * Extending the `FileSystemDirectoryHandle` with a `values` method to iterate over the directory contents.
- * This is as defined in https://fs.spec.whatwg.org/#filesystemdirectoryhandle (File System Standard, Last Updated 28 June 2024).
- */
-interface ExplorableFileSystemDirectoryHandle extends FileSystemDirectoryHandle {
-  values?: () => AsyncIterable<FileSystemFileHandle | FileSystemDirectoryHandle | null>;
-}
-
 interface FileWithHandleAndPath {
   fileWithHandle: FileWithHandle;
   relativeName: string;
@@ -22,7 +14,7 @@ export async function getAllFilesFromDirectoryRecursively(directoryHandle: FileS
   const list: FileWithHandleAndPath[] = [];
   const separator = '/';
 
-  async function traverseDirectory(dirHandle: ExplorableFileSystemDirectoryHandle, path: string = '') {
+  async function traverseDirectory(dirHandle: FileSystemDirectoryHandle, path: string = '') {
     if ('values' in dirHandle && typeof dirHandle.values === 'function') {
       for await (const handle of dirHandle.values()) {
         if (!handle) continue;
