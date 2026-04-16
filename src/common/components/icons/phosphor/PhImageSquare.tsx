eea53714cc1a2d817bcfcd4b9a291598cb1f47fb
Enrico Ros <enrico.ros@gmail.com>
2026-04-16 00:52:44 -0700

    Ph: add ImaegeSquare, Wrench

---
+++ src/common/components/icons/phosphor/PhImageSquare.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import { SvgIcon, SvgIconProps } from '@mui/joy';
+
+/*
+ * Source: 'https://phosphoricons.com/' - image-square
+ */
+export function PhImageSquare(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox='0 0 256 256' stroke='none' fill='currentColor' width='24' height='24' {...props}>
+      <path d='M208,32H48A16,16,0,0,0,32,48V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V48A16,16,0,0,0,208,32ZM48,48H208v77.38l-24.69-24.7a16,16,0,0,0-22.62,0L53.37,208H48ZM208,208H76l96-96,36,36v60ZM96,120A24,24,0,1,0,72,96,24,24,0,0,0,96,120Zm0-32a8,8,0,1,1-8,8A8,8,0,0,1,96,88Z' />
+    </SvgIcon>
+  );
+}
